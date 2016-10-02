af8f884f4362ce2ee73f53a3069a8ea2127346ec
kanokara <emptyknk@gmail.com>
2016-10-02 16:14:32 +0900

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 6105,
-	"created_at": "Sun Oct 02 05:14:32 +0000 2016",
+	"tweets": 6107,
+	"created_at": "Sun Oct 02 07:14:32 +0000 2016",
 	"lang": "ko"
 }
--- data/js/tweets/2016_10.js
+++ data/js/tweets/2016_10.js
@@ -1,6 +1,287 @@
 Grailbird.data.tweets_2016_10 = 
 [
 	{
+		"created_at": "Sun Oct 02 07:01:49 +0000 2016",
+		"id": 782475659327090700,
+		"id_str": "782475659327090688",
+		"text": "RT @michsuzu: 川上：ライトノベルのミュージアムについては、サブカルチャーで歴史を持つ会社なので、日本のサブカルチャーの殿堂を作る責任があると考えている。日本のサブカルチャーはアジアを中心として世界中にファンがいるので、インバウンド向けの施設にもなるのではないか　#…",
+		"truncated": false,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "michsuzu",
+					"name": "すずき@マイクラ勉強中",
+					"id": 841723651,
+					"id_str": "841723651",
+					"indices": [
+						3,
+						12
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3255599052,
+			"id_str": "3255599052",
+			"name": "K_K",
+			"screen_name": "EmptyKnK",
+			"location": "",
+			"description": "A Room of One's Own",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": true,
+			"followers_count": 7,
+			"friends_count": 74,
+			"listed_count": 0,
+			"created_at": "Thu Jun 25 13:48:16 +0000 2015",
+			"favourites_count": 1044,
+			"utc_offset": 32400,
+			"time_zone": "Seoul",
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 6103,
+			"lang": "ko",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/764399824590348288/MO_0ThO-_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/764399824590348288/MO_0ThO-_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3255599052/1471081989",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"retweeted_status": {
+			"created_at": "Tue Jun 21 02:08:21 +0000 2016",
+			"id": 745075857757081600,
+			"id_str": "745075857757081600",
+			"text": "川上：ライトノベルのミュージアムについては、サブカルチャーで歴史を持つ会社なので、日本のサブカルチャーの殿堂を作る責任があると考えている。日本のサブカルチャーはアジアを中心として世界中にファンがいるので、インバウンド向けの施設にもなるのではないか　#カドカワ株主総会",
+			"truncated": false,
+			"entities": {
+				"hashtags": [
+					{
+						"text": "カドカワ株主総会",
+						"indices": [
+							124,
+							133
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": []
+			},
+			"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 841723651,
+				"id_str": "841723651",
+				"name": "すずき@マイクラ勉強中",
+				"screen_name": "michsuzu",
+				"location": "Akihabara,Tokyo",
+				"description": "個人発信の可能性を探って、現地現場の情報を流しています。新しいことやゲーム、秋葉原が大好きです！ 毎日夜にゲーム実況生放送（https://t.co/73OhwkSvFb）。 アイコンは浅葱くるみさん作。ヘッダーは新海誠監督『秒速5センチメートル』の壁紙。人間っぽいアカウントのフォローはお返ししています",
+				"url": "https://t.co/5TMk558KDY",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/5TMk558KDY",
+								"expanded_url": "http://michsuzuki.hatenablog.com/",
+								"display_url": "michsuzuki.hatenablog.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": [
+							{
+								"url": "https://t.co/73OhwkSvFb",
+								"expanded_url": "http://bit.ly/2agSNFO",
+								"display_url": "bit.ly/2agSNFO",
+								"indices": [
+									62,
+									85
+								]
+							}
+						]
+					}
+				},
+				"protected": false,
+				"followers_count": 5609,
+				"friends_count": 5659,
+				"listed_count": 57,
+				"created_at": "Sun Sep 23 13:31:28 +0000 2012",
+				"favourites_count": 1993,
+				"utc_offset": 32400,
+				"time_zone": "Tokyo",
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 4354,
+				"lang": "ja",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "C0DEED",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/750993829205929985/7iq0TLcO_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/750993829205929985/7iq0TLcO_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/841723651/1473097263",
+				"profile_link_color": "0084B4",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": true,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 12,
+			"favorite_count": 6,
+			"favorited": false,
+			"retweeted": true,
+			"lang": "ja"
+		},
+		"is_quote_status": false,
+		"retweet_count": 12,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "ja"
+	},
+	{
+		"created_at": "Sun Oct 02 06:53:10 +0000 2016",
+		"id": 782473484152606700,
+		"id_str": "782473484152606721",
+		"text": "「劇場は２．５次元ブームなどがあって１年～１年半前から予約しなければ取れないなど非常に取りにくい状況がある。(…) 劇場で見れない方のために全国の映画館でライブビューイングを行っている。」 https://t.co/tkbR6jqFjH",
+		"truncated": false,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/tkbR6jqFjH",
+					"expanded_url": "http://hanna.hateblo.jp/entry/2016/06/26/232912",
+					"display_url": "hanna.hateblo.jp/entry/2016/06/…",
+					"indices": [
+						95,
+						118
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3255599052,
+			"id_str": "3255599052",
+			"name": "K_K",
+			"screen_name": "EmptyKnK",
+			"location": "",
+			"description": "A Room of One's Own",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": true,
+			"followers_count": 7,
+			"friends_count": 74,
+			"listed_count": 0,
+			"created_at": "Thu Jun 25 13:48:16 +0000 2015",
+			"favourites_count": 1044,
+			"utc_offset": 32400,
+			"time_zone": "Seoul",
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 6103,
+			"lang": "ko",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/764399824590348288/MO_0ThO-_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/764399824590348288/MO_0ThO-_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3255599052/1471081989",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "ja"
+	},
+	{
 		"created_at": "Sun Oct 02 05:06:09 +0000 2016",
 		"id": 782446552186888200,
 		"id_str": "782446552186888192",
