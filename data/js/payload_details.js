90ba2ae77d218d231247a71310b1ebf39d713a83
kanokara <emptyknk@gmail.com>
2016-10-04 20:14:36 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 6194,
-	"created_at": "Tue Oct 04 09:14:33 +0000 2016",
+	"tweets": 6195,
+	"created_at": "Tue Oct 04 11:14:35 +0000 2016",
 	"lang": "ko"
 }
--- data/js/tweets/2016_10.js
+++ data/js/tweets/2016_10.js
@@ -1,6 +1,182 @@
 Grailbird.data.tweets_2016_10 = 
 [
 	{
+		"created_at": "Tue Oct 04 10:29:30 +0000 2016",
+		"id": 783252701081509900,
+		"id_str": "783252701081509888",
+		"text": "RT @BOT_Melancholy: 와! 소리 지르고 싶다. 정신 없이 난리 피우고 싶다. 보이는 걸 다 쥐어뜯고 잡히는 걸 다 던져서 속에 쌓여있는 걸 모조리 털어내고 싶다. 그렇게 깨끗해지면 속에서 썩어 곪을 일도 없을텐데. 가끔 그런 충동이 든…",
+		"truncated": false,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "BOT_Melancholy",
+					"name": "멘헤라 BOT",
+					"id": 2270823505,
+					"id_str": "2270823505",
+					"indices": [
+						3,
+						18
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3255599052,
+			"id_str": "3255599052",
+			"name": "K_K",
+			"screen_name": "EmptyKnK",
+			"location": "",
+			"description": "A Room of One's Own",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": true,
+			"followers_count": 7,
+			"friends_count": 74,
+			"listed_count": 0,
+			"created_at": "Thu Jun 25 13:48:16 +0000 2015",
+			"favourites_count": 1047,
+			"utc_offset": 32400,
+			"time_zone": "Seoul",
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 6174,
+			"lang": "ko",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/764399824590348288/MO_0ThO-_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/764399824590348288/MO_0ThO-_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3255599052/1471081989",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"retweeted_status": {
+			"created_at": "Tue Oct 04 05:15:37 +0000 2016",
+			"id": 783173709003501600,
+			"id_str": "783173709003501568",
+			"text": "와! 소리 지르고 싶다. 정신 없이 난리 피우고 싶다. 보이는 걸 다 쥐어뜯고 잡히는 걸 다 던져서 속에 쌓여있는 걸 모조리 털어내고 싶다. 그렇게 깨끗해지면 속에서 썩어 곪을 일도 없을텐데. 가끔 그런 충동이 든다. 부디 제게 평안과 안식을 주세요.",
+			"truncated": false,
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": []
+			},
+			"source": "<a href=\"http://twittbot.net/\" rel=\"nofollow\">twittbot.net</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 2270823505,
+				"id_str": "2270823505",
+				"name": "멘헤라 BOT",
+				"screen_name": "BOT_Melancholy",
+				"location": "변기에 머리 넣고 물 내려야지!",
+				"description": "지나가세요. 당신에게 해롭습니다.",
+				"url": "https://t.co/r6dPCVhafG",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/r6dPCVhafG",
+								"expanded_url": "http://twpf.jp/BOT_Melancholy",
+								"display_url": "twpf.jp/BOT_Melancholy",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 1276,
+				"friends_count": 2,
+				"listed_count": 26,
+				"created_at": "Tue Dec 31 20:45:10 +0000 2013",
+				"favourites_count": 523,
+				"utc_offset": 28800,
+				"time_zone": "Irkutsk",
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 22485,
+				"lang": "ko",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "C0DEED",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/772762809624997888/2hFo7l4__normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/772762809624997888/2hFo7l4__normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/2270823505/1473075965",
+				"profile_link_color": "0084B4",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": true,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 4,
+			"favorite_count": 3,
+			"favorited": false,
+			"retweeted": true,
+			"lang": "ko"
+		},
+		"is_quote_status": false,
+		"retweet_count": 4,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "ko"
+	},
+	{
 		"created_at": "Tue Oct 04 08:14:56 +0000 2016",
 		"id": 783218834937253900,
 		"id_str": "783218834937253889",
