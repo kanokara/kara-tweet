457e560223ebc6b076a3ace2a3abd972e60b33d2
kanokara <emptyknk@gmail.com>
2016-10-04 13:14:36 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 6191,
-	"created_at": "Mon Oct 03 11:14:32 +0000 2016",
+	"tweets": 6192,
+	"created_at": "Tue Oct 04 04:14:35 +0000 2016",
 	"lang": "ko"
 }
--- data/js/tweets/2016_10.js
+++ data/js/tweets/2016_10.js
@@ -1,6 +1,246 @@
 Grailbird.data.tweets_2016_10 = 
 [
 	{
+		"created_at": "Tue Oct 04 04:08:02 +0000 2016",
+		"id": 783156700073254900,
+		"id_str": "783156700073254912",
+		"text": "RT @chiw00k: 외고 전교 1등이 학교를 중퇴하고 9급공무원에 합격. 이를 어떻게 생각해야 할지… 게다가 토목직이라 응용역학등을 공부해야 했었는데 이해는 못하고 다 외워서 풀었다고… ㅠㅠ https://t.co/QwsmQwSEB4",
+		"truncated": false,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "chiw00k",
+					"name": "박치욱",
+					"id": 18530666,
+					"id_str": "18530666",
+					"indices": [
+						3,
+						11
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/QwsmQwSEB4",
+					"expanded_url": "http://m.blog.naver.com/jobarajob/220824012765",
+					"display_url": "m.blog.naver.com/jobarajob/2208…",
+					"indices": [
+						109,
+						132
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3255599052,
+			"id_str": "3255599052",
+			"name": "K_K",
+			"screen_name": "EmptyKnK",
+			"location": "",
+			"description": "A Room of One's Own",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": true,
+			"followers_count": 7,
+			"friends_count": 74,
+			"listed_count": 0,
+			"created_at": "Thu Jun 25 13:48:16 +0000 2015",
+			"favourites_count": 1046,
+			"utc_offset": 32400,
+			"time_zone": "Seoul",
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 6171,
+			"lang": "ko",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/764399824590348288/MO_0ThO-_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/764399824590348288/MO_0ThO-_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3255599052/1471081989",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"retweeted_status": {
+			"created_at": "Mon Oct 03 16:51:37 +0000 2016",
+			"id": 782986473956139000,
+			"id_str": "782986473956139008",
+			"text": "외고 전교 1등이 학교를 중퇴하고 9급공무원에 합격. 이를 어떻게 생각해야 할지… 게다가 토목직이라 응용역학등을 공부해야 했었는데 이해는 못하고 다 외워서 풀었다고… ㅠㅠ https://t.co/QwsmQwSEB4",
+			"truncated": false,
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/QwsmQwSEB4",
+						"expanded_url": "http://m.blog.naver.com/jobarajob/220824012765",
+						"display_url": "m.blog.naver.com/jobarajob/2208…",
+						"indices": [
+							96,
+							119
+						]
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 18530666,
+				"id_str": "18530666",
+				"name": "박치욱",
+				"screen_name": "chiw00k",
+				"location": "Indiana, USA",
+				"description": "Professor. Biochemist/Biophysicist. Purdue University, Indiana. Protein Folding. Classical Music, Math Puzzles, Cooking. Send Q's to https://t.co/pmebSIHAs3",
+				"url": "https://t.co/DExIsucEka",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/DExIsucEka",
+								"expanded_url": "http://people.pnhs.purdue.edu/~park6/",
+								"display_url": "people.pnhs.purdue.edu/~park6/",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": [
+							{
+								"url": "https://t.co/pmebSIHAs3",
+								"expanded_url": "http://ask.fm/chiw00k",
+								"display_url": "ask.fm/chiw00k",
+								"indices": [
+									133,
+									156
+								]
+							}
+						]
+					}
+				},
+				"protected": false,
+				"followers_count": 5644,
+				"friends_count": 991,
+				"listed_count": 177,
+				"created_at": "Thu Jan 01 16:44:46 +0000 2009",
+				"favourites_count": 1706,
+				"utc_offset": -14400,
+				"time_zone": "Eastern Time (US & Canada)",
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 18830,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "C0DEED",
+				"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/143918519/pollock.jpg",
+				"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/143918519/pollock.jpg",
+				"profile_background_tile": true,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/669634329488830465/81CXDn1N_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/669634329488830465/81CXDn1N_normal.jpg",
+				"profile_link_color": "0084B4",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": {
+				"id": "832ac0728b3e26ac",
+				"url": "https://api.twitter.com/1.1/geo/id/832ac0728b3e26ac.json",
+				"place_type": "city",
+				"name": "West Lafayette",
+				"full_name": "West Lafayette, IN",
+				"country_code": "US",
+				"country": "United States",
+				"contained_within": [],
+				"bounding_box": {
+					"type": "Polygon",
+					"coordinates": [
+						[
+							[
+								-86.999431,
+								40.406761
+							],
+							[
+								-86.86053,
+								40.406761
+							],
+							[
+								-86.86053,
+								40.511402
+							],
+							[
+								-86.999431,
+								40.511402
+							]
+						]
+					]
+				},
+				"attributes": {}
+			},
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 373,
+			"favorite_count": 46,
+			"favorited": false,
+			"retweeted": true,
+			"possibly_sensitive": false,
+			"lang": "ko"
+		},
+		"is_quote_status": false,
+		"retweet_count": 373,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko"
+	},
+	{
 		"created_at": "Mon Oct 03 10:34:08 +0000 2016",
 		"id": 782891479299600400,
 		"id_str": "782891479299600385",
