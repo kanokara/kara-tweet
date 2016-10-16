8b3e0cb8d4308848486edc66367629750f8d669d
kanokara <emptyknk@gmail.com>
2016-10-16 23:14:41 +0900

    Added 5 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 6634,
-	"created_at": "Sun Oct 16 13:14:39 +0000 2016",
+	"tweets": 6639,
+	"created_at": "Sun Oct 16 14:14:40 +0000 2016",
 	"lang": "ko"
 }
--- data/js/tweet_index.js
+++ data/js/tweet_index.js
@@ -3,7 +3,7 @@ var tweet_index = [
 		"file_name": "data/js/tweets/2016_10.js",
 		"year": 2016,
 		"var_name": "tweets_2016_10",
-		"tweet_count": 569,
+		"tweet_count": 574,
 		"month": 10
 	},
 	{
