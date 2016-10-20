2068dc62c80e367065289d2dc62d939860bff086
kanokara <emptyknk@gmail.com>
2016-10-20 22:14:45 +0900

    Added 2 tweets

--- data/js/tweet_index.js
+++ data/js/tweet_index.js
@@ -3,7 +3,7 @@ var tweet_index = [
 		"file_name": "data/js/tweets/2016_10.js",
 		"year": 2016,
 		"var_name": "tweets_2016_10",
-		"tweet_count": 826,
+		"tweet_count": 828,
 		"month": 10
 	},
 	{
