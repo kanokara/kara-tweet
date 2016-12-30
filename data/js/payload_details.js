c1075108cb2583808201b93ec5ef8c081c4a7f8e
kanokara <emptyknk@gmail.com>
2016-12-31 04:14:42 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 8297,
-	"created_at": "Sun Dec 25 17:14:38 +0000 2016",
+	"tweets": 8298,
+	"created_at": "Fri Dec 30 19:14:41 +0000 2016",
 	"lang": "ko"
 }
--- data/js/tweets/2016_12.js
+++ data/js/tweets/2016_12.js
@@ -1,6 +1,184 @@
 Grailbird.data.tweets_2016_12 = 
 [
 	{
+		"created_at": "Fri Dec 30 18:56:00 +0000 2016",
+		"id": 814907910291484700,
+		"id_str": "814907910291484672",
+		"text": "RT @Han_Kyudong: 잘못은 그냥 잘못이라고 하면 그만이다. 하지만 프로 연구자가 아닌- 운운은 그냥 조악한 변명이라고 생각하고 학계 밖에서 연구하는 연구자들의 입지 역시 좁히는 발언임. 그래 너는 못배웠으니까, 학계 소속 안됐으니까, 자료…",
+		"truncated": false,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "Han_Kyudong",
+					"name": "한규동",
+					"id": 181199507,
+					"id_str": "181199507",
+					"indices": [
+						3,
+						15
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3255599052,
+			"id_str": "3255599052",
+			"name": "Solitude [동결:11/20~]",
+			"screen_name": "EmptyKnK",
+			"location": "",
+			"description": "탄식하라,  덧없이 흩어지고 말리라",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": true,
+			"followers_count": 6,
+			"friends_count": 57,
+			"listed_count": 0,
+			"created_at": "Thu Jun 25 13:48:16 +0000 2015",
+			"favourites_count": 1025,
+			"utc_offset": 32400,
+			"time_zone": "Seoul",
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 8098,
+			"lang": "ko",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/798458646283681792/UwzBOgjh_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/798458646283681792/UwzBOgjh_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3255599052/1479202379",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"retweeted_status": {
+			"created_at": "Thu Dec 29 11:23:29 +0000 2016",
+			"id": 814431641254785000,
+			"id_str": "814431641254785024",
+			"text": "잘못은 그냥 잘못이라고 하면 그만이다. 하지만 프로 연구자가 아닌- 운운은 그냥 조악한 변명이라고 생각하고 학계 밖에서 연구하는 연구자들의 입지 역시 좁히는 발언임. 그래 너는 못배웠으니까, 학계 소속 안됐으니까, 자료 다 사서 써야 하니까, 힘들지?",
+			"truncated": false,
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": []
+			},
+			"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+			"in_reply_to_status_id": 814430960192143400,
+			"in_reply_to_status_id_str": "814430960192143360",
+			"in_reply_to_user_id": 181199507,
+			"in_reply_to_user_id_str": "181199507",
+			"in_reply_to_screen_name": "Han_Kyudong",
+			"user": {
+				"id": 181199507,
+				"id_str": "181199507",
+				"name": "한규동",
+				"screen_name": "Han_Kyudong",
+				"location": "",
+				"description": "내 토끼가 너무 예쁘다 | 남자 아니에요",
+				"url": "https://t.co/YkYtFbPFik",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/YkYtFbPFik",
+								"expanded_url": "http://bunny-alex.tumblr.com/",
+								"display_url": "bunny-alex.tumblr.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 42962,
+				"friends_count": 417,
+				"listed_count": 372,
+				"created_at": "Sat Aug 21 15:21:31 +0000 2010",
+				"favourites_count": 23070,
+				"utc_offset": 28800,
+				"time_zone": "Taipei",
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 184419,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "C6E2EE",
+				"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/278737195/ClioTeam1963MagritteLaReconnaiss-1.jpg",
+				"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/278737195/ClioTeam1963MagritteLaReconnaiss-1.jpg",
+				"profile_background_tile": true,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/793291934571782145/0Tzi9pnO_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/793291934571782145/0Tzi9pnO_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/181199507/1452859625",
+				"profile_link_color": "1B95E0",
+				"profile_sidebar_border_color": "C6E2EE",
+				"profile_sidebar_fill_color": "DAECF4",
+				"profile_text_color": "663B12",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 118,
+			"favorite_count": 34,
+			"favorited": false,
+			"retweeted": true,
+			"lang": "ko"
+		},
+		"is_quote_status": false,
+		"retweet_count": 118,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "ko"
+	},
+	{
 		"created_at": "Sun Dec 25 16:26:48 +0000 2016",
 		"id": 813058423965134800,
 		"id_str": "813058423965134848",
