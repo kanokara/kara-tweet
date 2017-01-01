109b82286daeac27aafc1c53c09620a71aabdeab
kanokara <emptyknk@gmail.com>
2017-01-01 13:14:31 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 8317,
-	"created_at": "Sun Jan 01 03:14:34 +0000 2017",
+	"tweets": 8318,
+	"created_at": "Sun Jan 01 04:14:31 +0000 2017",
 	"lang": "ko"
 }
--- data/js/tweets/2017_01.js
+++ data/js/tweets/2017_01.js
@@ -1,6 +1,366 @@
 Grailbird.data.tweets_2017_01 = 
 [
 	{
+		"created_at": "Sun Jan 01 03:34:25 +0000 2017",
+		"id": 815400760829624300,
+		"id_str": "815400760829624320",
+		"text": "RT @_17meisai23: #FGO\n空の境界コラボ復活して欲しい https://t.co/4wCCL7v92T",
+		"truncated": false,
+		"entities": {
+			"hashtags": [
+				{
+					"text": "FGO",
+					"indices": [
+						17,
+						21
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "_17meisai23",
+					"name": "手島nari。",
+					"id": 726122993957503000,
+					"id_str": "726122993957502976",
+					"indices": [
+						3,
+						15
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 814011774160638000,
+					"id_str": "814011774160637952",
+					"indices": [
+						37,
+						60
+					],
+					"media_url": "http://pbs.twimg.com/media/C0vzZVHUkAAsBpb.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/C0vzZVHUkAAsBpb.jpg",
+					"url": "https://t.co/4wCCL7v92T",
+					"display_url": "pic.twitter.com/4wCCL7v92T",
+					"expanded_url": "https://twitter.com/_17meisai23/status/814011784310853632/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1322,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 439,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 775,
+							"h": 1200,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 814011784310853600,
+					"source_status_id_str": "814011784310853632",
+					"source_user_id": 726122993957503000,
+					"source_user_id_str": "726122993957502976"
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 814011774160638000,
+					"id_str": "814011774160637952",
+					"indices": [
+						37,
+						60
+					],
+					"media_url": "http://pbs.twimg.com/media/C0vzZVHUkAAsBpb.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/C0vzZVHUkAAsBpb.jpg",
+					"url": "https://t.co/4wCCL7v92T",
+					"display_url": "pic.twitter.com/4wCCL7v92T",
+					"expanded_url": "https://twitter.com/_17meisai23/status/814011784310853632/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1322,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 439,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 775,
+							"h": 1200,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 814011784310853600,
+					"source_status_id_str": "814011784310853632",
+					"source_user_id": 726122993957503000,
+					"source_user_id_str": "726122993957502976"
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3255599052,
+			"id_str": "3255599052",
+			"name": "Solitude [동결:11/20~]",
+			"screen_name": "EmptyKnK",
+			"location": "",
+			"description": "탄식하라,  덧없이 흩어지고 말리라",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": true,
+			"followers_count": 6,
+			"friends_count": 57,
+			"listed_count": 0,
+			"created_at": "Thu Jun 25 13:48:16 +0000 2015",
+			"favourites_count": 1026,
+			"utc_offset": 32400,
+			"time_zone": "Seoul",
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 8117,
+			"lang": "ko",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/798458646283681792/UwzBOgjh_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/798458646283681792/UwzBOgjh_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3255599052/1479202379",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"retweeted_status": {
+			"created_at": "Wed Dec 28 07:35:07 +0000 2016",
+			"id": 814011784310853600,
+			"id_str": "814011784310853632",
+			"text": "#FGO\n空の境界コラボ復活して欲しい https://t.co/4wCCL7v92T",
+			"truncated": false,
+			"entities": {
+				"hashtags": [
+					{
+						"text": "FGO",
+						"indices": [
+							0,
+							4
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 814011774160638000,
+						"id_str": "814011774160637952",
+						"indices": [
+							20,
+							43
+						],
+						"media_url": "http://pbs.twimg.com/media/C0vzZVHUkAAsBpb.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/C0vzZVHUkAAsBpb.jpg",
+						"url": "https://t.co/4wCCL7v92T",
+						"display_url": "pic.twitter.com/4wCCL7v92T",
+						"expanded_url": "https://twitter.com/_17meisai23/status/814011784310853632/photo/1",
+						"type": "photo",
+						"sizes": {
+							"large": {
+								"w": 1322,
+								"h": 2048,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 439,
+								"h": 680,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 775,
+								"h": 1200,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 814011774160638000,
+						"id_str": "814011774160637952",
+						"indices": [
+							20,
+							43
+						],
+						"media_url": "http://pbs.twimg.com/media/C0vzZVHUkAAsBpb.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/C0vzZVHUkAAsBpb.jpg",
+						"url": "https://t.co/4wCCL7v92T",
+						"display_url": "pic.twitter.com/4wCCL7v92T",
+						"expanded_url": "https://twitter.com/_17meisai23/status/814011784310853632/photo/1",
+						"type": "photo",
+						"sizes": {
+							"large": {
+								"w": 1322,
+								"h": 2048,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 439,
+								"h": 680,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 775,
+								"h": 1200,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 726122993957503000,
+				"id_str": "726122993957502976",
+				"name": "手島nari。",
+				"screen_name": "_17meisai23",
+				"location": "日本",
+				"description": "ちょべりぐ 雑多な落書きが多いです(fgo多め)             御用の方はこちらへ nari.teshima.2721@gmail.com",
+				"url": "https://t.co/49AUmvLO9V",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/49AUmvLO9V",
+								"expanded_url": "http://www.pixiv.net/member.php?id=14993224",
+								"display_url": "pixiv.net/member.php?id=…",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 5051,
+				"friends_count": 609,
+				"listed_count": 102,
+				"created_at": "Fri Apr 29 18:56:27 +0000 2016",
+				"favourites_count": 2677,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 2871,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "F5F8FA",
+				"profile_background_image_url": null,
+				"profile_background_image_url_https": null,
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/814116209218056195/Xl3B6DEe_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/814116209218056195/Xl3B6DEe_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/726122993957502976/1482727720",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": true,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 2496,
+			"favorite_count": 4364,
+			"favorited": true,
+			"retweeted": true,
+			"possibly_sensitive": false,
+			"lang": "ja"
+		},
+		"is_quote_status": false,
+		"retweet_count": 2496,
+		"favorite_count": 0,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ja"
+	},
+	{
 		"created_at": "Sun Jan 01 02:23:57 +0000 2017",
 		"id": 815383027991752700,
 		"id_str": "815383027991752709",
