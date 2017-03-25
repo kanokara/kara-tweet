563b3ee65b3645bb30676daf8518d775706ea05d
kanokara <emptyknk@gmail.com>
2017-03-26 03:14:40 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 9374,
-	"created_at": "Sat Mar 25 17:14:39 +0000 2017",
+	"tweets": 9375,
+	"created_at": "Sat Mar 25 18:14:39 +0000 2017",
 	"lang": "ko"
 }
--- data/js/tweets/2017_03.js
+++ data/js/tweets/2017_03.js
@@ -1,6 +1,172 @@
 Grailbird.data.tweets_2017_03 = 
 [
 	{
+		"created_at": "Sat Mar 25 17:16:40 +0000 2017",
+		"id": 845685881017024500,
+		"id_str": "845685881017024512",
+		"text": "一千億円の市場規模となっている可能性が十分あるだろう。 \nhttps://t.co/2uNaSAuAW4 https://t.co/YVZDtZUqnR",
+		"truncated": false,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/2uNaSAuAW4",
+					"expanded_url": "http://aja.gr.jp/jigyou/chousa/sangyo_toukei",
+					"display_url": "aja.gr.jp/jigyou/chousa/…",
+					"indices": [
+						29,
+						52
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 845685621158920200,
+					"id_str": "845685621158920192",
+					"indices": [
+						53,
+						76
+					],
+					"media_url": "http://pbs.twimg.com/media/C7x6l4DVQAAZkQr.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/C7x6l4DVQAAZkQr.jpg",
+					"url": "https://t.co/YVZDtZUqnR",
+					"display_url": "pic.twitter.com/YVZDtZUqnR",
+					"expanded_url": "https://twitter.com/EmptyKnK/status/845685881017024512/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 342,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 847,
+							"h": 426,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 847,
+							"h": 426,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 845685621158920200,
+					"id_str": "845685621158920192",
+					"indices": [
+						53,
+						76
+					],
+					"media_url": "http://pbs.twimg.com/media/C7x6l4DVQAAZkQr.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/C7x6l4DVQAAZkQr.jpg",
+					"url": "https://t.co/YVZDtZUqnR",
+					"display_url": "pic.twitter.com/YVZDtZUqnR",
+					"expanded_url": "https://twitter.com/EmptyKnK/status/845685881017024512/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 342,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 847,
+							"h": 426,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 847,
+							"h": 426,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": 845685356162822100,
+		"in_reply_to_status_id_str": "845685356162822144",
+		"in_reply_to_user_id": 3255599052,
+		"in_reply_to_user_id_str": "3255599052",
+		"in_reply_to_screen_name": "EmptyKnK",
+		"user": {
+			"id": 3255599052,
+			"id_str": "3255599052",
+			"name": "Solitude [동결:11/20~]",
+			"screen_name": "EmptyKnK",
+			"location": "",
+			"description": "탄식하라,  덧없이 흩어지고 말리라",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": true,
+			"followers_count": 7,
+			"friends_count": 71,
+			"listed_count": 0,
+			"created_at": "Thu Jun 25 13:48:16 +0000 2015",
+			"favourites_count": 1022,
+			"utc_offset": 32400,
+			"time_zone": "Seoul",
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 9090,
+			"lang": "ko",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/798458646283681792/UwzBOgjh_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/798458646283681792/UwzBOgjh_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3255599052/1479202379",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "ja"
+	},
+	{
 		"created_at": "Sat Mar 25 17:14:35 +0000 2017",
 		"id": 845685356162822100,
 		"id_str": "845685356162822144",
