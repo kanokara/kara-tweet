56308b38fe12df282649350023486a0906dd2717
kanokara <emptyknk@gmail.com>
2017-04-12 21:14:36 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 9761,
-	"created_at": "Wed Apr 12 11:14:34 +0000 2017",
+	"tweets": 9762,
+	"created_at": "Wed Apr 12 12:14:35 +0000 2017",
 	"lang": "ko"
 }
--- data/js/tweets/2017_04.js
+++ data/js/tweets/2017_04.js
@@ -1,6 +1,186 @@
 Grailbird.data.tweets_2017_04 = 
 [
 	{
+		"created_at": "Wed Apr 12 11:59:05 +0000 2017",
+		"id": 852128940101558300,
+		"id_str": "852128940101558272",
+		"text": "오토코노코...?\nhttps://t.co/5fUSB4dkyX",
+		"truncated": false,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/5fUSB4dkyX",
+					"expanded_url": "https://twitter.com/cardbana/status/852128231616655360",
+					"display_url": "twitter.com/cardbana/statu…",
+					"indices": [
+						10,
+						33
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3255599052,
+			"id_str": "3255599052",
+			"name": "Solitude [동결:11/20~]",
+			"screen_name": "EmptyKnK",
+			"location": "",
+			"description": "탄식하라,  덧없이 흩어지고 말리라",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": true,
+			"followers_count": 7,
+			"friends_count": 71,
+			"listed_count": 0,
+			"created_at": "Thu Jun 25 13:48:16 +0000 2015",
+			"favourites_count": 1059,
+			"utc_offset": 32400,
+			"time_zone": "Seoul",
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 9434,
+			"lang": "ko",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/798458646283681792/UwzBOgjh_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/798458646283681792/UwzBOgjh_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3255599052/1479202379",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 852128231616655400,
+		"quoted_status_id_str": "852128231616655360",
+		"quoted_status": {
+			"created_at": "Wed Apr 12 11:56:16 +0000 2017",
+			"id": 852128231616655400,
+			"id_str": "852128231616655360",
+			"text": "[이광수][이중어글쓰기]\n한편 이광수는 \"이미 일본인이되 철저한 일본인…'의식적'으로 된 일본인\"(110)이 되어, 香山光郞으로써 비의식적 일본인보다 우위에 선다. 그의 창씨개명은 \"만세일계, 팔굉일우의 주재자 천황이 그 여의주를 보장\"(111)한다.",
+			"truncated": false,
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": []
+			},
+			"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+			"in_reply_to_status_id": 852127092867305500,
+			"in_reply_to_status_id_str": "852127092867305477",
+			"in_reply_to_user_id": 786364996556992500,
+			"in_reply_to_user_id_str": "786364996556992512",
+			"in_reply_to_screen_name": "cardbana",
+			"user": {
+				"id": 786364996556992500,
+				"id_str": "786364996556992512",
+				"name": "타치바나@독서카드",
+				"screen_name": "cardbana",
+				"location": "日本 兵庫",
+				"description": "타치바나... 타치바나 아리스입니다. 타치바나라고 불러주세요. 요즘 시대에 종이로 된 독서 카드라니, 논외입니다.",
+				"url": "https://t.co/4Fdm7wxHnU",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/4Fdm7wxHnU",
+								"expanded_url": "http://twilog.org/cardbana",
+								"display_url": "twilog.org/cardbana",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 21,
+				"friends_count": 0,
+				"listed_count": 0,
+				"created_at": "Thu Oct 13 00:36:40 +0000 2016",
+				"favourites_count": 0,
+				"utc_offset": -25200,
+				"time_zone": "Pacific Time (US & Canada)",
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 258,
+				"lang": "ko",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "F5F8FA",
+				"profile_background_image_url": null,
+				"profile_background_image_url_https": null,
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/786367986928013316/UQQEUuiH_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/786367986928013316/UQQEUuiH_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/786364996556992512/1476319408",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": true,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 0,
+			"favorite_count": 0,
+			"favorited": false,
+			"retweeted": false,
+			"lang": "ko"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "ko"
+	},
+	{
 		"created_at": "Wed Apr 12 10:38:23 +0000 2017",
 		"id": 852108630631489500,
 		"id_str": "852108630631489537",
