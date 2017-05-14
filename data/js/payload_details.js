4895f32e863ec071e8ea2065f035e57e9e12d21a
kanokara <emptyknk@gmail.com>
2017-05-15 01:14:36 +0900

    Added 3 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 10178,
-	"created_at": "Sun May 14 14:14:34 +0000 2017",
+	"tweets": 10181,
+	"created_at": "Sun May 14 16:14:34 +0000 2017",
 	"lang": "ko"
 }
--- data/js/tweets/2017_05.js
+++ data/js/tweets/2017_05.js
@@ -1,6 +1,607 @@
 Grailbird.data.tweets_2017_05 = 
 [
 	{
+		"created_at": "Sun May 14 15:59:44 +0000 2017",
+		"id": 863785912941002800,
+		"id_str": "863785912941002752",
+		"text": "그 점에 있어서 이 기사에서 말하는 정보 통합 이론은 참고할 만해요. 동물에게 의식이 존재하지 않는다는 말에는 동의 못하지만.\n https://t.co/FQUVWU55Dn",
+		"truncated": false,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/FQUVWU55Dn",
+					"expanded_url": "http://sciencebooks.tistory.com/340",
+					"display_url": "sciencebooks.tistory.com/340",
+					"indices": [
+						72,
+						95
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Lite</a>",
+		"in_reply_to_status_id": 863785727678488600,
+		"in_reply_to_status_id_str": "863785727678488576",
+		"in_reply_to_user_id": 3255599052,
+		"in_reply_to_user_id_str": "3255599052",
+		"in_reply_to_screen_name": "EmptyKnK",
+		"user": {
+			"id": 3255599052,
+			"id_str": "3255599052",
+			"name": "Solitude [동결:11/20~]",
+			"screen_name": "EmptyKnK",
+			"location": "",
+			"description": "탄식하라,  덧없이 흩어지고 말리라",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": true,
+			"followers_count": 7,
+			"friends_count": 72,
+			"listed_count": 0,
+			"created_at": "Thu Jun 25 13:48:16 +0000 2015",
+			"favourites_count": 1055,
+			"utc_offset": 32400,
+			"time_zone": "Seoul",
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 9795,
+			"lang": "ko",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/798458646283681792/UwzBOgjh_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/798458646283681792/UwzBOgjh_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3255599052/1479202379",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "ko"
+	},
+	{
+		"created_at": "Sun May 14 15:59:00 +0000 2017",
+		"id": 863785727678488600,
+		"id_str": "863785727678488576",
+		"text": "사실 의식을 모듈의 집합체로써의 뇌의 부산물 정도로 여기는 관점은 기호학과 함께 하지 못하죠. 양자가 서로의 존재를 인정할 수 있는 전제 조건은 의식의 기능을 정당하게 인정하는 것.",
+		"truncated": false,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Lite</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3255599052,
+			"id_str": "3255599052",
+			"name": "Solitude [동결:11/20~]",
+			"screen_name": "EmptyKnK",
+			"location": "",
+			"description": "탄식하라,  덧없이 흩어지고 말리라",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": true,
+			"followers_count": 7,
+			"friends_count": 72,
+			"listed_count": 0,
+			"created_at": "Thu Jun 25 13:48:16 +0000 2015",
+			"favourites_count": 1055,
+			"utc_offset": 32400,
+			"time_zone": "Seoul",
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 9795,
+			"lang": "ko",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/798458646283681792/UwzBOgjh_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/798458646283681792/UwzBOgjh_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3255599052/1479202379",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "ko"
+	},
+	{
+		"created_at": "Sun May 14 15:47:19 +0000 2017",
+		"id": 863782788348100600,
+		"id_str": "863782788348100608",
+		"text": "RT @inuduka_sino: 『空の境界』の両儀式と娘の未那♪ #母の日 https://t.co/oQHTXaVWZK",
+		"truncated": false,
+		"entities": {
+			"hashtags": [
+				{
+					"text": "母の日",
+					"indices": [
+						35,
+						39
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "inuduka_sino",
+					"name": "犬塚志乃",
+					"id": 275920856,
+					"id_str": "275920856",
+					"indices": [
+						3,
+						16
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 863773591908663300,
+					"id_str": "863773591908663298",
+					"indices": [
+						40,
+						63
+					],
+					"media_url": "http://pbs.twimg.com/media/C_y9gWAUQAIwWVm.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/C_y9gWAUQAIwWVm.jpg",
+					"url": "https://t.co/oQHTXaVWZK",
+					"display_url": "pic.twitter.com/oQHTXaVWZK",
+					"expanded_url": "https://twitter.com/inuduka_sino/status/863773740470910976/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 499,
+							"h": 541,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 499,
+							"h": 541,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 499,
+							"h": 541,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 863773740470911000,
+					"source_status_id_str": "863773740470910976",
+					"source_user_id": 275920856,
+					"source_user_id_str": "275920856"
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 863773591908663300,
+					"id_str": "863773591908663298",
+					"indices": [
+						40,
+						63
+					],
+					"media_url": "http://pbs.twimg.com/media/C_y9gWAUQAIwWVm.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/C_y9gWAUQAIwWVm.jpg",
+					"url": "https://t.co/oQHTXaVWZK",
+					"display_url": "pic.twitter.com/oQHTXaVWZK",
+					"expanded_url": "https://twitter.com/inuduka_sino/status/863773740470910976/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 499,
+							"h": 541,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 499,
+							"h": 541,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 499,
+							"h": 541,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 863773740470911000,
+					"source_status_id_str": "863773740470910976",
+					"source_user_id": 275920856,
+					"source_user_id_str": "275920856"
+				},
+				{
+					"id": 863773629955227600,
+					"id_str": "863773629955227649",
+					"indices": [
+						40,
+						63
+					],
+					"media_url": "http://pbs.twimg.com/media/C_y9ijvUwAEVCCZ.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/C_y9ijvUwAEVCCZ.jpg",
+					"url": "https://t.co/oQHTXaVWZK",
+					"display_url": "pic.twitter.com/oQHTXaVWZK",
+					"expanded_url": "https://twitter.com/inuduka_sino/status/863773740470910976/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 265,
+							"h": 384,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 265,
+							"h": 384,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 265,
+							"h": 384,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 863773740470911000,
+					"source_status_id_str": "863773740470910976",
+					"source_user_id": 275920856,
+					"source_user_id_str": "275920856"
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Lite</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3255599052,
+			"id_str": "3255599052",
+			"name": "Solitude [동결:11/20~]",
+			"screen_name": "EmptyKnK",
+			"location": "",
+			"description": "탄식하라,  덧없이 흩어지고 말리라",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": true,
+			"followers_count": 7,
+			"friends_count": 72,
+			"listed_count": 0,
+			"created_at": "Thu Jun 25 13:48:16 +0000 2015",
+			"favourites_count": 1055,
+			"utc_offset": 32400,
+			"time_zone": "Seoul",
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 9795,
+			"lang": "ko",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/798458646283681792/UwzBOgjh_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/798458646283681792/UwzBOgjh_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3255599052/1479202379",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"retweeted_status": {
+			"created_at": "Sun May 14 15:11:22 +0000 2017",
+			"id": 863773740470911000,
+			"id_str": "863773740470910976",
+			"text": "『空の境界』の両儀式と娘の未那♪ #母の日 https://t.co/oQHTXaVWZK",
+			"truncated": false,
+			"entities": {
+				"hashtags": [
+					{
+						"text": "母の日",
+						"indices": [
+							17,
+							21
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 863773591908663300,
+						"id_str": "863773591908663298",
+						"indices": [
+							22,
+							45
+						],
+						"media_url": "http://pbs.twimg.com/media/C_y9gWAUQAIwWVm.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/C_y9gWAUQAIwWVm.jpg",
+						"url": "https://t.co/oQHTXaVWZK",
+						"display_url": "pic.twitter.com/oQHTXaVWZK",
+						"expanded_url": "https://twitter.com/inuduka_sino/status/863773740470910976/photo/1",
+						"type": "photo",
+						"sizes": {
+							"medium": {
+								"w": 499,
+								"h": 541,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 499,
+								"h": 541,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 499,
+								"h": 541,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 863773591908663300,
+						"id_str": "863773591908663298",
+						"indices": [
+							22,
+							45
+						],
+						"media_url": "http://pbs.twimg.com/media/C_y9gWAUQAIwWVm.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/C_y9gWAUQAIwWVm.jpg",
+						"url": "https://t.co/oQHTXaVWZK",
+						"display_url": "pic.twitter.com/oQHTXaVWZK",
+						"expanded_url": "https://twitter.com/inuduka_sino/status/863773740470910976/photo/1",
+						"type": "photo",
+						"sizes": {
+							"medium": {
+								"w": 499,
+								"h": 541,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 499,
+								"h": 541,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 499,
+								"h": 541,
+								"resize": "fit"
+							}
+						}
+					},
+					{
+						"id": 863773629955227600,
+						"id_str": "863773629955227649",
+						"indices": [
+							22,
+							45
+						],
+						"media_url": "http://pbs.twimg.com/media/C_y9ijvUwAEVCCZ.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/C_y9ijvUwAEVCCZ.jpg",
+						"url": "https://t.co/oQHTXaVWZK",
+						"display_url": "pic.twitter.com/oQHTXaVWZK",
+						"expanded_url": "https://twitter.com/inuduka_sino/status/863773740470910976/photo/1",
+						"type": "photo",
+						"sizes": {
+							"large": {
+								"w": 265,
+								"h": 384,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 265,
+								"h": 384,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 265,
+								"h": 384,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 275920856,
+				"id_str": "275920856",
+				"name": "犬塚志乃",
+				"screen_name": "inuduka_sino",
+				"location": "",
+				"description": "　百合やアニメ、ライトノベル、歴史が好きです。\r\n百合とオレンヂ城というサイトを運営しています。\r\n　どうぞ、よろしくお願いいたします。",
+				"url": "http://t.co/PXUnk5g4GC",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "http://t.co/PXUnk5g4GC",
+								"expanded_url": "http://orange.ap.teacup.com/orangejyo/",
+								"display_url": "orange.ap.teacup.com/orangejyo/",
+								"indices": [
+									0,
+									22
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 183,
+				"friends_count": 175,
+				"listed_count": 5,
+				"created_at": "Sat Apr 02 08:15:41 +0000 2011",
+				"favourites_count": 30138,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 9824,
+				"lang": "ja",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "C0DEED",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1296951671/_________normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1296951671/_________normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/275920856/1449986413",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": true,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 7,
+			"favorite_count": 5,
+			"favorited": false,
+			"retweeted": true,
+			"possibly_sensitive": false,
+			"lang": "ja"
+		},
+		"is_quote_status": false,
+		"retweet_count": 7,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ja"
+	},
+	{
 		"created_at": "Sun May 14 13:29:31 +0000 2017",
 		"id": 863748109171544000,
 		"id_str": "863748109171544065",
