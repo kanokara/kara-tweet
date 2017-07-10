2410a1b4ca92838cbe69d51e2eab2a61b5edaa7d
kanokara <emptyknk@gmail.com>
2017-07-10 17:11:00 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 10487,
-	"created_at": "Fri Jul 07 08:10:58 +0000 2017",
+	"tweets": 10488,
+	"created_at": "Mon Jul 10 08:10:59 +0000 2017",
 	"lang": "ko"
 }
--- data/js/tweets/2017_07.js
+++ data/js/tweets/2017_07.js
@@ -1,6 +1,337 @@
 Grailbird.data.tweets_2017_07 = 
 [
 	{
+		"created_at": "Mon Jul 10 08:06:32 +0000 2017",
+		"id": 884322936974262300,
+		"id_str": "884322936974262272",
+		"text": "RT @Blem_775: 오늘의 무대는 제가 이뤄낸 것, 그 누구의 공도 아닙니다. 그 더러운 손 치우시죠 프로듀서. 악수는 높으신 분들과 잔뜩 하시기 바랍니다. https://t.co/b7gGPFKnpv",
+		"truncated": false,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "Blem_775",
+					"name": "MelB",
+					"id": 821565908,
+					"id_str": "821565908",
+					"indices": [
+						3,
+						12
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 883636497114480600,
+					"id_str": "883636497114480640",
+					"indices": [
+						91,
+						114
+					],
+					"media_url": "http://pbs.twimg.com/media/DENOtmEUQAAHggL.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DENOtmEUQAAHggL.jpg",
+					"url": "https://t.co/b7gGPFKnpv",
+					"display_url": "pic.twitter.com/b7gGPFKnpv",
+					"expanded_url": "https://twitter.com/Blem_775/status/883636525837058048/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1920,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					},
+					"source_status_id": 883636525837058000,
+					"source_status_id_str": "883636525837058048",
+					"source_user_id": 821565908,
+					"source_user_id_str": "821565908"
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 883636497114480600,
+					"id_str": "883636497114480640",
+					"indices": [
+						91,
+						114
+					],
+					"media_url": "http://pbs.twimg.com/media/DENOtmEUQAAHggL.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DENOtmEUQAAHggL.jpg",
+					"url": "https://t.co/b7gGPFKnpv",
+					"display_url": "pic.twitter.com/b7gGPFKnpv",
+					"expanded_url": "https://twitter.com/Blem_775/status/883636525837058048/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1920,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					},
+					"source_status_id": 883636525837058000,
+					"source_status_id_str": "883636525837058048",
+					"source_user_id": 821565908,
+					"source_user_id_str": "821565908"
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Lite</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3255599052,
+			"id_str": "3255599052",
+			"name": "Solitude [동결:11/20~]",
+			"screen_name": "EmptyKnK",
+			"location": "",
+			"description": "탄식하라,  덧없이 흩어지고 말리라",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": true,
+			"followers_count": 6,
+			"friends_count": 71,
+			"listed_count": 0,
+			"created_at": "Thu Jun 25 13:48:16 +0000 2015",
+			"favourites_count": 1057,
+			"utc_offset": 32400,
+			"time_zone": "Seoul",
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 10038,
+			"lang": "ko",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/883004104825421824/OpqJfYKo_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/883004104825421824/OpqJfYKo_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3255599052/1499359511",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"retweeted_status": {
+			"created_at": "Sat Jul 08 10:38:59 +0000 2017",
+			"id": 883636525837058000,
+			"id_str": "883636525837058048",
+			"text": "오늘의 무대는 제가 이뤄낸 것, 그 누구의 공도 아닙니다. 그 더러운 손 치우시죠 프로듀서. 악수는 높으신 분들과 잔뜩 하시기 바랍니다. https://t.co/b7gGPFKnpv",
+			"truncated": false,
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 883636497114480600,
+						"id_str": "883636497114480640",
+						"indices": [
+							77,
+							100
+						],
+						"media_url": "http://pbs.twimg.com/media/DENOtmEUQAAHggL.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/DENOtmEUQAAHggL.jpg",
+						"url": "https://t.co/b7gGPFKnpv",
+						"display_url": "pic.twitter.com/b7gGPFKnpv",
+						"expanded_url": "https://twitter.com/Blem_775/status/883636525837058048/photo/1",
+						"type": "photo",
+						"sizes": {
+							"medium": {
+								"w": 1200,
+								"h": 675,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1920,
+								"h": 1080,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 383,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 883636497114480600,
+						"id_str": "883636497114480640",
+						"indices": [
+							77,
+							100
+						],
+						"media_url": "http://pbs.twimg.com/media/DENOtmEUQAAHggL.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/DENOtmEUQAAHggL.jpg",
+						"url": "https://t.co/b7gGPFKnpv",
+						"display_url": "pic.twitter.com/b7gGPFKnpv",
+						"expanded_url": "https://twitter.com/Blem_775/status/883636525837058048/photo/1",
+						"type": "photo",
+						"sizes": {
+							"medium": {
+								"w": 1200,
+								"h": 675,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1920,
+								"h": 1080,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 383,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 821565908,
+				"id_str": "821565908",
+				"name": "MelB",
+				"screen_name": "Blem_775",
+				"location": "로코랜드",
+				"description": "iM@S 如月千早, 七尾百合子, 星輝子P / 伊藤美来, Pyxis / Mili / バンドリ / Maplestory",
+				"url": null,
+				"entities": {
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 923,
+				"friends_count": 570,
+				"listed_count": 0,
+				"created_at": "Thu Sep 13 14:00:43 +0000 2012",
+				"favourites_count": 503,
+				"utc_offset": 32400,
+				"time_zone": "Seoul",
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 102191,
+				"lang": "ko",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "000000",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme4/bg.gif",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme4/bg.gif",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/884100777387544576/8E7tWq4g_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/884100777387544576/8E7tWq4g_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/821565908/1499157146",
+				"profile_link_color": "1B95E0",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "000000",
+				"profile_text_color": "000000",
+				"profile_use_background_image": false,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 366,
+			"favorite_count": 108,
+			"favorited": false,
+			"retweeted": true,
+			"possibly_sensitive": false,
+			"lang": "ko"
+		},
+		"is_quote_status": false,
+		"retweet_count": 366,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko"
+	},
+	{
 		"created_at": "Fri Jul 07 07:26:11 +0000 2017",
 		"id": 883225617835020300,
 		"id_str": "883225617835020289",
