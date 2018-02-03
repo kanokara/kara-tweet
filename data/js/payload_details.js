1d6e96f14d654f0895e6ed2b9db47d96097372bd
kanokara <emptyknk@gmail.com>
2018-02-04 05:10:59 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 10733,
-	"created_at": "Tue Jan 23 15:10:58 +0000 2018",
+	"tweets": 10734,
+	"created_at": "Sat Feb 03 20:10:58 +0000 2018",
 	"lang": "ko"
 }
---
+++ data/js/tweets/2018_02.js
@@ -0,0 +1,159 @@
+Grailbird.data.tweets_2018_02 = 
+[
+	{
+		"created_at": "Sat Feb 03 20:02:27 +0000 2018",
+		"id": 959879778374307800,
+		"id_str": "959879778374307840",
+		"text": "넋 놓고 있었는데, 지난 12월에 도바시 신지로 신작 《콜로세움》이 정발됐었네요. 믿고 보는 몇 안 되는 작가. https://t.co/LoOp388dNw",
+		"truncated": false,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 959879776029720600,
+					"id_str": "959879776029720581",
+					"indices": [
+						63,
+						86
+					],
+					"media_url": "http://pbs.twimg.com/media/DVItkhVVQAU-39y.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DVItkhVVQAU-39y.jpg",
+					"url": "https://t.co/LoOp388dNw",
+					"display_url": "pic.twitter.com/LoOp388dNw",
+					"expanded_url": "https://twitter.com/EmptyKnK/status/959879778374307840/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 420,
+							"h": 320,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 420,
+							"h": 320,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 420,
+							"h": 320,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 959879776029720600,
+					"id_str": "959879776029720581",
+					"indices": [
+						63,
+						86
+					],
+					"media_url": "http://pbs.twimg.com/media/DVItkhVVQAU-39y.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DVItkhVVQAU-39y.jpg",
+					"url": "https://t.co/LoOp388dNw",
+					"display_url": "pic.twitter.com/LoOp388dNw",
+					"expanded_url": "https://twitter.com/EmptyKnK/status/959879778374307840/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 420,
+							"h": 320,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 420,
+							"h": 320,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 420,
+							"h": 320,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://www.twitter.com\" rel=\"nofollow\">Twitter for Windows</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3255599052,
+			"id_str": "3255599052",
+			"name": "Solitude [동결:11/20~]",
+			"screen_name": "EmptyKnK",
+			"location": "",
+			"description": "탄식하라,  덧없이 흩어지고 말리라",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": true,
+			"followers_count": 6,
+			"friends_count": 70,
+			"listed_count": 0,
+			"created_at": "Thu Jun 25 13:48:16 +0000 2015",
+			"favourites_count": 1010,
+			"utc_offset": 32400,
+			"time_zone": "Seoul",
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 10095,
+			"lang": "ko",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/902611212990464001/uFpl92QO_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/902611212990464001/uFpl92QO_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3255599052/1504034318",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "ko"
+	}
+]
