689b552003fd6a97e5eb63b514903423a9f83b6d
kanokara <emptyknk@gmail.com>
2018-05-02 00:10:58 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 11401,
-	"created_at": "Tue May 01 09:11:02 +0000 2018",
+	"tweets": 11402,
+	"created_at": "Tue May 01 15:10:57 +0000 2018",
 	"lang": "ko"
 }
--- data/js/tweets/2018_05.js
+++ data/js/tweets/2018_05.js
@@ -1,6 +1,182 @@
 Grailbird.data.tweets_2018_05 = 
 [
 	{
+		"created_at": "Tue May 01 14:59:04 +0000 2018",
+		"id": 991331175208599600,
+		"id_str": "991331175208599555",
+		"text": "RT @Denying_Access: '우울증은 아주 안정적인 상태다. 다시 말해 뇌는 계속해서 우울한 상태를 유지하는 방향으로 생각하고 행동하는 경향이 있다.' 이 문장 너무 와닿아서 본문을 가져왔다. 우울증에 걸리면 사실 원래 느껴야 할 감정을 못…",
+		"truncated": false,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "Denying_Access",
+					"name": "홍익둘리",
+					"id": 909362628123693000,
+					"id_str": "909362628123693056",
+					"indices": [
+						3,
+						18
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3255599052,
+			"id_str": "3255599052",
+			"name": "Solitude [동결:11/20~]",
+			"screen_name": "EmptyKnK",
+			"location": "",
+			"description": "탄식하라,  덧없이 흩어지고 말리라",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": true,
+			"followers_count": 8,
+			"friends_count": 77,
+			"listed_count": 0,
+			"created_at": "Thu Jun 25 13:48:16 +0000 2015",
+			"favourites_count": 1091,
+			"utc_offset": 32400,
+			"time_zone": "Seoul",
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 10641,
+			"lang": "ko",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/902611212990464001/uFpl92QO_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/902611212990464001/uFpl92QO_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3255599052/1504034318",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"retweeted_status": {
+			"created_at": "Tue May 01 09:15:37 +0000 2018",
+			"id": 991244740761825300,
+			"id_str": "991244740761825280",
+			"text": "'우울증은 아주 안정적인 상태다. 다시 말해 뇌는 계속해서 우울한 상태를 유지하는 방향으로 생각하고 행동하는 경향이 있다.' 이 문장 너무 와닿아서 본문을 가져왔다. 우울증에 걸리면 사실 원래 느껴야 할 감… https://t.co/DvW48Qii2L",
+			"truncated": true,
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/DvW48Qii2L",
+						"expanded_url": "https://twitter.com/i/web/status/991244740761825280",
+						"display_url": "twitter.com/i/web/status/9…",
+						"indices": [
+							117,
+							140
+						]
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 909362628123693000,
+				"id_str": "909362628123693056",
+				"name": "홍익둘리",
+				"screen_name": "Denying_Access",
+				"location": "FUMB FREE",
+				"description": "퀴어 / 천천히 면역력 기르는 중인 사람 / ※트친 정리 했는데 실수로 된 분도 있을 수 있음 그런 경우 연락 주시면 감사하겠음",
+				"url": null,
+				"entities": {
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 194,
+				"friends_count": 69,
+				"listed_count": 0,
+				"created_at": "Sun Sep 17 10:25:20 +0000 2017",
+				"favourites_count": 9253,
+				"utc_offset": 32400,
+				"time_zone": "Seoul",
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 564,
+				"lang": "ko",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "000000",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/990115615837843456/3SbnqGLZ_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/990115615837843456/3SbnqGLZ_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/909362628123693056/1525000946",
+				"profile_link_color": "9E9478",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "000000",
+				"profile_text_color": "000000",
+				"profile_use_background_image": false,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 5171,
+			"favorite_count": 1858,
+			"favorited": false,
+			"retweeted": true,
+			"possibly_sensitive": false,
+			"lang": "ko"
+		},
+		"is_quote_status": false,
+		"retweet_count": 5171,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "ko"
+	},
+	{
 		"created_at": "Tue May 01 08:14:24 +0000 2018",
 		"id": 991229335750131700,
 		"id_str": "991229335750131712",
