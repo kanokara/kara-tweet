2732590de5c27a67e8d60297c3ca2f98e903e975
kanokara <emptyknk@gmail.com>
2018-10-17 04:11:05 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 11725,
-	"created_at": "Tue Oct 16 14:10:58 +0000 2018",
+	"tweets": 11726,
+	"created_at": "Tue Oct 16 19:11:04 +0000 2018",
 	"lang": "ko"
 }
--- data/js/tweets/2018_10.js
+++ data/js/tweets/2018_10.js
@@ -1,6 +1,99 @@
 Grailbird.data.tweets_2018_10 = 
 [
 	{
+		"created_at": "Tue Oct 16 18:37:05 +0000 2018",
+		"id": 1052267199350497300,
+		"id_str": "1052267199350497282",
+		"full_text": "역대 권력과 기독교(개신교)\nhttps://t.co/Ap6eMJqCcR\n\n와, 보수 정권과 결탁하여 급격히 성장한 것은 알았지만(현재도 기독교 비중이 낮은 일본과 다른 길을 가게 된, 종교 지형의 분기점이 되기도 했고) 이렇게 일목요연하게 정리한 글은 처음 보네요.",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			148
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/Ap6eMJqCcR",
+					"expanded_url": "https://m.blog.naver.com/woogy68/220866154152",
+					"display_url": "m.blog.naver.com/woogy68/220866…",
+					"indices": [
+						16,
+						39
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3255599052,
+			"id_str": "3255599052",
+			"name": "Solitude [동결:11/20~]",
+			"screen_name": "EmptyKnK",
+			"location": "",
+			"description": "탄식하라,  덧없이 흩어지고 말리라",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": true,
+			"followers_count": 8,
+			"friends_count": 77,
+			"listed_count": 0,
+			"created_at": "Thu Jun 25 13:48:16 +0000 2015",
+			"favourites_count": 1111,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 10856,
+			"lang": "ko",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/902611212990464001/uFpl92QO_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/902611212990464001/uFpl92QO_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3255599052/1504034318",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "역대 권력과 기독교(개신교)\nhttps://t.co/Ap6eMJqCcR\n\n와, 보수 정권과 결탁하여 급격히 성장한 것은 알았지만(현재도 기독교 비중이 낮은 일본과 다른 길을 가게 된, 종교 지형의 분기점이 되기도 했고) 이렇게 일목요연하게 정리한 글은 처음 보네요."
+	},
+	{
 		"created_at": "Tue Oct 16 13:42:57 +0000 2018",
 		"id": 1052193178470629400,
 		"id_str": "1052193178470629382",
