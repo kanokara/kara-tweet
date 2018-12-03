ad7bb42ac0eb148575b1f99d367744c9aeb82e28
kanokara <emptyknk@gmail.com>
2018-12-04 00:11:03 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 13422,
-	"created_at": "Mon Dec 03 13:11:02 +0000 2018",
+	"tweets": 13423,
+	"created_at": "Mon Dec 03 15:11:02 +0000 2018",
 	"lang": "ko"
 }
--- data/js/tweets/2018_12.js
+++ data/js/tweets/2018_12.js
@@ -1,6 +1,203 @@
 Grailbird.data.tweets_2018_12 = 
 [
 	{
+		"created_at": "Mon Dec 03 14:42:24 +0000 2018",
+		"id": 1069602757080838100,
+		"id_str": "1069602757080838144",
+		"full_text": "저는 예수가 이 발언을 한 번만 한 줄 알았는데, 투쟁하면서 여러 차례 꺼낸 신념이었네요. https://t.co/CtcI3ur2wX",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			50
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1069602483222147100,
+					"id_str": "1069602483222147072",
+					"indices": [
+						51,
+						74
+					],
+					"media_url": "http://pbs.twimg.com/media/Dtf9yp9U8AAdLsg.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Dtf9yp9U8AAdLsg.jpg",
+					"url": "https://t.co/CtcI3ur2wX",
+					"display_url": "pic.twitter.com/CtcI3ur2wX",
+					"expanded_url": "https://twitter.com/EmptyKnK/status/1069602757080838144/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 469,
+							"h": 455,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 469,
+							"h": 455,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 469,
+							"h": 455,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1069602483222147100,
+					"id_str": "1069602483222147072",
+					"indices": [
+						51,
+						74
+					],
+					"media_url": "http://pbs.twimg.com/media/Dtf9yp9U8AAdLsg.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Dtf9yp9U8AAdLsg.jpg",
+					"url": "https://t.co/CtcI3ur2wX",
+					"display_url": "pic.twitter.com/CtcI3ur2wX",
+					"expanded_url": "https://twitter.com/EmptyKnK/status/1069602757080838144/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 469,
+							"h": 455,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 469,
+							"h": 455,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 469,
+							"h": 455,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1069602508744454100,
+					"id_str": "1069602508744454144",
+					"indices": [
+						51,
+						74
+					],
+					"media_url": "http://pbs.twimg.com/media/Dtf90JCUcAA6I_H.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Dtf90JCUcAA6I_H.jpg",
+					"url": "https://t.co/CtcI3ur2wX",
+					"display_url": "pic.twitter.com/CtcI3ur2wX",
+					"expanded_url": "https://twitter.com/EmptyKnK/status/1069602757080838144/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 467,
+							"h": 622,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 467,
+							"h": 622,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 467,
+							"h": 622,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3255599052,
+			"id_str": "3255599052",
+			"name": "Solitude [동결:11/20~]",
+			"screen_name": "EmptyKnK",
+			"location": "",
+			"description": "탄식하라,  덧없이 흩어지고 말리라",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": true,
+			"followers_count": 8,
+			"friends_count": 77,
+			"listed_count": 0,
+			"created_at": "Thu Jun 25 13:48:16 +0000 2015",
+			"favourites_count": 1127,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 11000,
+			"lang": "ko",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/902611212990464001/uFpl92QO_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/902611212990464001/uFpl92QO_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3255599052/1504034318",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "저는 예수가 이 발언을 한 번만 한 줄 알았는데, 투쟁하면서 여러 차례 꺼낸 신념이었네요. https://t.co/CtcI3ur2wX"
+	},
+	{
 		"created_at": "Mon Dec 03 12:57:17 +0000 2018",
 		"id": 1069576306138898400,
 		"id_str": "1069576306138898433",
