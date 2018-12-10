31c7d9b419eec9e97e76a14372bb90bb18114688
kanokara <emptyknk@gmail.com>
2018-12-10 23:11:04 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 13489,
-	"created_at": "Mon Dec 10 13:11:03 +0000 2018",
+	"tweets": 13490,
+	"created_at": "Mon Dec 10 14:11:02 +0000 2018",
 	"lang": "ko"
 }
--- data/js/tweet_index.js
+++ data/js/tweet_index.js
@@ -3,7 +3,7 @@ var tweet_index = [
 		"file_name": "data/js/tweets/2018_12.js",
 		"year": 2018,
 		"var_name": "tweets_2018_12",
-		"tweet_count": 553,
+		"tweet_count": 554,
 		"month": 12
 	},
 	{
