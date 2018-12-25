bfe11dc378b3f761496b1d4037127c10e7bf4693
kanokara <emptyknk@gmail.com>
2018-12-25 09:11:01 +0900

    Added 1 tweets

--- data/js/tweet_index.js
+++ data/js/tweet_index.js
@@ -3,7 +3,7 @@ var tweet_index = [
 		"file_name": "data/js/tweets/2018_12.js",
 		"year": 2018,
 		"var_name": "tweets_2018_12",
-		"tweet_count": 346,
+		"tweet_count": 347,
 		"month": 12
 	},
 	{
