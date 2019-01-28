6cab2cdd01a6e4826b21f76424a1c094ada97381
kanokara <emptyknk@gmail.com>
2019-01-29 01:11:00 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 19282,
-	"created_at": "Sat Jan 26 06:10:57 +0000 2019",
+	"tweets": 19283,
+	"created_at": "Mon Jan 28 16:10:59 +0000 2019",
 	"lang": "ko"
 }
--- data/js/tweets/2019_01.js
+++ data/js/tweets/2019_01.js
@@ -1,6 +1,108 @@
 Grailbird.data.tweets_2019_01 = 
 [
 	{
+		"created_at": "Mon Jan 28 15:54:23 +0000 2019",
+		"id": 1089914594833780700,
+		"id_str": "1089914594833780736",
+		"full_text": "팬메이드 케모노 프렌즈 2기 1화 (바다 편).manhwa\nhttps://t.co/3u0NZsTfRR\nhttps://t.co/Dw1pczoeCx\n\n타노시!",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			86
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/3u0NZsTfRR",
+					"expanded_url": "http://web.humoruniv.com/board/humor/read.html?table=thema2&number=883569",
+					"display_url": "web.humoruniv.com/board/humor/re…",
+					"indices": [
+						33,
+						56
+					]
+				},
+				{
+					"url": "https://t.co/Dw1pczoeCx",
+					"expanded_url": "https://www.pixiv.net/member.php?id=321750",
+					"display_url": "pixiv.net/member.php?id=…",
+					"indices": [
+						57,
+						80
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3255599052,
+			"id_str": "3255599052",
+			"name": "Solitude [동결:11/20~]",
+			"screen_name": "EmptyKnK",
+			"location": "",
+			"description": "탄식하라,  덧없이 흩어지고 말리라",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": true,
+			"followers_count": 8,
+			"friends_count": 78,
+			"listed_count": 0,
+			"created_at": "Thu Jun 25 13:48:16 +0000 2015",
+			"favourites_count": 1139,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 11188,
+			"lang": "ko",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/902611212990464001/uFpl92QO_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/902611212990464001/uFpl92QO_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3255599052/1504034318",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "팬메이드 케모노 프렌즈 2기 1화 (바다 편).manhwa\nhttps://t.co/3u0NZsTfRR\nhttps://t.co/Dw1pczoeCx\n\n타노시!"
+	},
+	{
 		"created_at": "Sat Jan 26 05:27:07 +0000 2019",
 		"id": 1089031960536608800,
 		"id_str": "1089031960536608770",
