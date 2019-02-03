9de8aea10f3a77bc21630d2f24db6779b2d4fd38
kanokara <emptyknk@gmail.com>
2019-02-04 08:10:58 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 19289,
-	"created_at": "Sun Feb 03 22:10:57 +0000 2019",
+	"tweets": 19290,
+	"created_at": "Sun Feb 03 23:10:57 +0000 2019",
 	"lang": "ko"
 }
--- data/js/tweets/2019_02.js
+++ data/js/tweets/2019_02.js
@@ -1,6 +1,99 @@
 Grailbird.data.tweets_2019_02 = 
 [
 	{
+		"created_at": "Sun Feb 03 22:22:41 +0000 2019",
+		"id": 1092186639948902400,
+		"id_str": "1092186639948902401",
+		"full_text": "Artist - 사카모토 마아야 / 坂本真綾(さかもと まあや)\nAlbum - Grapefruit (グレープフルーツ) / [1997.04.23]\nTitle - Track 07.青い瞳(Aoi Hitomi) Remix / Blue eyes\nhttps://t.co/ClqJh7GSHz\n\n아! 이 노래를 누가 불렀나 이제야 알았네요.",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			181
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/ClqJh7GSHz",
+					"expanded_url": "https://www.youtube.com/watch?v=Q5dHTtBvuo8",
+					"display_url": "youtube.com/watch?v=Q5dHTt…",
+					"indices": [
+						131,
+						154
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": 1092178674936250400,
+		"in_reply_to_status_id_str": "1092178674936250368",
+		"in_reply_to_user_id": 3255599052,
+		"in_reply_to_user_id_str": "3255599052",
+		"in_reply_to_screen_name": "EmptyKnK",
+		"user": {
+			"id": 3255599052,
+			"id_str": "3255599052",
+			"name": "Solitude [동결:11/20~]",
+			"screen_name": "EmptyKnK",
+			"location": "",
+			"description": "탄식하라,  덧없이 흩어지고 말리라",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": true,
+			"followers_count": 8,
+			"friends_count": 78,
+			"listed_count": 0,
+			"created_at": "Thu Jun 25 13:48:16 +0000 2015",
+			"favourites_count": 1138,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 11186,
+			"lang": "ko",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/902611212990464001/uFpl92QO_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/902611212990464001/uFpl92QO_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3255599052/1504034318",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "ja",
+		"text": "Artist - 사카모토 마아야 / 坂本真綾(さかもと まあや)\nAlbum - Grapefruit (グレープフルーツ) / [1997.04.23]\nTitle - Track 07.青い瞳(Aoi Hitomi) Remix / Blue eyes\nhttps://t.co/ClqJh7GSHz\n\n아! 이 노래를 누가 불렀나 이제야 알았네요."
+	},
+	{
 		"created_at": "Sun Feb 03 21:51:02 +0000 2019",
 		"id": 1092178674936250400,
 		"id_str": "1092178674936250368",
