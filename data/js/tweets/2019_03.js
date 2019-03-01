690ef295aad4f369f786bd9a183d983da690fe23
kanokara <emptyknk@gmail.com>
2019-03-01 13:10:58 +0900

    Added 1 tweets

---
+++ data/js/tweets/2019_03.js
@@ -0,0 +1,98 @@
+Grailbird.data.tweets_2019_03 = 
+[
+	{
+		"created_at": "Fri Mar 01 03:20:53 +0000 2019",
+		"id": 1101321381139034100,
+		"id_str": "1101331579790487553",
+		"full_text": "광복절 행사 보는데 '아리랑' 너무 한국적 정서에 잘 맞는 거 같다. 옛날에는 '한의 정서' 어쩌고는 한국 일반의 정서와는 관계 없지 않나 싶었는데 가사를 몇 번이고 되새겨보니 배신감에 남 좆되길 바라는 감수성을 '한' 단 한 글자로 대변하고 있었던 것.",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			142
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1627227828,
+			"id_str": "1627227828",
+			"name": "위래",
+			"screen_name": "N91211",
+			"location": "n91211.postype.com",
+			"description": "소설가",
+			"url": "https://t.co/OfoZqj1u83",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/OfoZqj1u83",
+							"expanded_url": "https://britg.kr/user/wirae",
+							"display_url": "britg.kr/user/wirae",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 856,
+			"friends_count": 541,
+			"listed_count": 16,
+			"created_at": "Sun Jul 28 06:48:52 +0000 2013",
+			"favourites_count": 31049,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 40975,
+			"lang": "ko",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "131516",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/442418193559064576/DHEUycKh_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/442418193559064576/DHEUycKh_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1627227828/1547107030",
+			"profile_link_color": "000000",
+			"profile_sidebar_border_color": "EEEEEE",
+			"profile_sidebar_fill_color": "EFEFEF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 111,
+		"favorite_count": 31,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "ko",
+		"text": "광복절 행사 보는데 '아리랑' 너무 한국적 정서에 잘 맞는 거 같다. 옛날에는 '한의 정서' 어쩌고는 한국 일반의 정서와는 관계 없지 않나 싶었는데 가사를 몇 번이고 되새겨보니 배신감에 남 좆되길 바라는 감수성을 '한' 단 한 글자로 대변하고 있었던 것."
+	}
+]
