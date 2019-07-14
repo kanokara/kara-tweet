b9f238ad57ed7f3d534c0ef23798817456d6cbb2
kanokara <emptyknk@gmail.com>
2019-07-14 09:10:58 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 19373,
-	"created_at": "Thu Jun 27 07:10:57 +0000 2019",
+	"tweets": 19374,
+	"created_at": "Sun Jul 14 00:10:57 +0000 2019",
 	"lang": "ko"
 }
---
+++ data/js/tweets/2019_07.js
@@ -0,0 +1,85 @@
+Grailbird.data.tweets_2019_07 = 
+[
+	{
+		"created_at": "Sat Jul 13 23:17:40 +0000 2019",
+		"id": 1150182533566308400,
+		"id_str": "1150182533566308353",
+		"full_text": "사이코패스와 같은 주인공상을 비판한 《전지적 독자 시점》의 예기치 못한 여성향의 인기에서 보듯이, 남성이나 여성이나 무정한 사이코패스를 좋아하는 걸 보면 넌더리가 나요. 로맨스 소설의 남주 중 가장 흔한 인물상이기도 하고. 권력지향적 남성의 이상적 체현이라고 할까.",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			148
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3255599052,
+			"id_str": "3255599052",
+			"name": "Solitude [동결:11/20~]",
+			"screen_name": "EmptyKnK",
+			"location": "",
+			"description": "탄식하라,  덧없이 흩어지고 말리라",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": true,
+			"followers_count": 9,
+			"friends_count": 79,
+			"listed_count": 0,
+			"created_at": "Thu Jun 25 13:48:16 +0000 2015",
+			"favourites_count": 1151,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 11214,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/902611212990464001/uFpl92QO_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/902611212990464001/uFpl92QO_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3255599052/1504034318",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "ko",
+		"text": "사이코패스와 같은 주인공상을 비판한 《전지적 독자 시점》의 예기치 못한 여성향의 인기에서 보듯이, 남성이나 여성이나 무정한 사이코패스를 좋아하는 걸 보면 넌더리가 나요. 로맨스 소설의 남주 중 가장 흔한 인물상이기도 하고. 권력지향적 남성의 이상적 체현이라고 할까."
+	}
+]
