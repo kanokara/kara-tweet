fff3297a3a6e7171c958b66a2ad95a073f628d56
kanokara <emptyknk@gmail.com>
2019-08-26 00:10:58 +0900

    Added 1 tweets

--- data/js/tweets/2019_08.js
+++ data/js/tweets/2019_08.js
@@ -1,6 +1,99 @@
 Grailbird.data.tweets_2019_08 = 
 [
 	{
+		"created_at": "Sun Aug 25 14:51:03 +0000 2019",
+		"id": 1165637721379016700,
+		"id_str": "1165637721379016707",
+		"full_text": "“자민당은 미래입니다. 민주당은 과거입니다. 그들이 주목하는 눈높이 말입니다. 각론(各論)으로 들어가면 양당 모두 과거 현재 미래가 투영돼 있겠지요? 그러나 대체적으로 보면 자민당이 미래, 민주당은 과거에 연연해 방향을 맞춘다는 것을 알 수 있습니다.”\nhttps://t.co/DlGlqhAj5I",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			165
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/DlGlqhAj5I",
+					"expanded_url": "http://m.monthly.chosun.com/client/news/viw.asp?ctcd=D&nNewsNumb=201502100023&page=7",
+					"display_url": "m.monthly.chosun.com/client/news/vi…",
+					"indices": [
+						142,
+						165
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3255599052,
+			"id_str": "3255599052",
+			"name": "Solitude [동결:11/20~]",
+			"screen_name": "EmptyKnK",
+			"location": "",
+			"description": "탄식하라,  덧없이 흩어지고 말리라",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": true,
+			"followers_count": 9,
+			"friends_count": 79,
+			"listed_count": 0,
+			"created_at": "Thu Jun 25 13:48:16 +0000 2015",
+			"favourites_count": 1166,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 11206,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/902611212990464001/uFpl92QO_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/902611212990464001/uFpl92QO_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3255599052/1504034318",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "“자민당은 미래입니다. 민주당은 과거입니다. 그들이 주목하는 눈높이 말입니다. 각론(各論)으로 들어가면 양당 모두 과거 현재 미래가 투영돼 있겠지요? 그러나 대체적으로 보면 자민당이 미래, 민주당은 과거에 연연해 방향을 맞춘다는 것을 알 수 있습니다.”\nhttps://t.co/DlGlqhAj5I"
+	},
+	{
 		"created_at": "Tue Aug 20 13:58:16 +0000 2019",
 		"id": 1163812497377247200,
 		"id_str": "1163812497377247233",
