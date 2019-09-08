013a99fd0e664de59b195fa9ca693237d1445493
kanokara <emptyknk@gmail.com>
2019-09-09 01:10:58 +0900

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 19399,
-	"created_at": "Sat Sep 07 19:10:57 +0000 2019",
+	"tweets": 19401,
+	"created_at": "Sun Sep 08 16:10:58 +0000 2019",
 	"lang": "ko"
 }
--- data/js/tweets/2019_09.js
+++ data/js/tweets/2019_09.js
@@ -1,6 +1,498 @@
 Grailbird.data.tweets_2019_09 = 
 [
 	{
+		"created_at": "Sun Sep 08 15:39:17 +0000 2019",
+		"id": 1170354289887236000,
+		"id_str": "1170723288521170944",
+		"full_text": "유행\n1946~1965년. https://t.co/WDUIl4nHgM",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			14
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1170354232148410400,
+					"id_str": "1170354232148410369",
+					"indices": [
+						15,
+						38
+					],
+					"media_url": "http://pbs.twimg.com/media/ED3u-SyUwAEoRzk.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ED3u-SyUwAEoRzk.jpg",
+					"url": "https://t.co/WDUIl4nHgM",
+					"display_url": "pic.twitter.com/WDUIl4nHgM",
+					"expanded_url": "https://twitter.com/cityscape_360/status/1170354289887236097/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 690,
+							"h": 1024,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 690,
+							"h": 1024,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 458,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1170354232148410400,
+					"id_str": "1170354232148410369",
+					"indices": [
+						15,
+						38
+					],
+					"media_url": "http://pbs.twimg.com/media/ED3u-SyUwAEoRzk.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ED3u-SyUwAEoRzk.jpg",
+					"url": "https://t.co/WDUIl4nHgM",
+					"display_url": "pic.twitter.com/WDUIl4nHgM",
+					"expanded_url": "https://twitter.com/cityscape_360/status/1170354289887236097/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 690,
+							"h": 1024,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 690,
+							"h": 1024,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 458,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1170354243963773000,
+					"id_str": "1170354243963772928",
+					"indices": [
+						15,
+						38
+					],
+					"media_url": "http://pbs.twimg.com/media/ED3u--zU4AASSyn.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ED3u--zU4AASSyn.jpg",
+					"url": "https://t.co/WDUIl4nHgM",
+					"display_url": "pic.twitter.com/WDUIl4nHgM",
+					"expanded_url": "https://twitter.com/cityscape_360/status/1170354289887236097/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 371,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 559,
+							"h": 1024,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 559,
+							"h": 1024,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1170354263282729000,
+					"id_str": "1170354263282728960",
+					"indices": [
+						15,
+						38
+					],
+					"media_url": "http://pbs.twimg.com/media/ED3vAGxUwAAwdn2.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ED3vAGxUwAAwdn2.jpg",
+					"url": "https://t.co/WDUIl4nHgM",
+					"display_url": "pic.twitter.com/WDUIl4nHgM",
+					"expanded_url": "https://twitter.com/cityscape_360/status/1170354289887236097/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 358,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 539,
+							"h": 1024,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 539,
+							"h": 1024,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1170354272069775400,
+					"id_str": "1170354272069775360",
+					"indices": [
+						15,
+						38
+					],
+					"media_url": "http://pbs.twimg.com/media/ED3vAngUcAA9VHn.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ED3vAngUcAA9VHn.jpg",
+					"url": "https://t.co/WDUIl4nHgM",
+					"display_url": "pic.twitter.com/WDUIl4nHgM",
+					"expanded_url": "https://twitter.com/cityscape_360/status/1170354289887236097/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 394,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 593,
+							"h": 1024,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 593,
+							"h": 1024,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1128584802083303400,
+			"id_str": "1128584802083303424",
+			"name": "시티스케이프",
+			"screen_name": "cityscape_360",
+			"location": "서울 여기저기",
+			"description": "걷기, 달리기, 버스와 지도.",
+			"url": "https://t.co/0RSnzmnldK",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/0RSnzmnldK",
+							"expanded_url": "http://peing.net/ko/cityscape",
+							"display_url": "peing.net/ko/cityscape",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 687,
+			"friends_count": 35,
+			"listed_count": 11,
+			"created_at": "Wed May 15 08:55:59 +0000 2019",
+			"favourites_count": 276,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 829,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1148212612036870144/_NgcHq-5_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1148212612036870144/_NgcHq-5_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1128584802083303424/1562590194",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1314,
+		"favorite_count": 1113,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "유행\n1946~1965년. https://t.co/WDUIl4nHgM"
+	},
+	{
+		"created_at": "Sun Sep 08 15:39:11 +0000 2019",
+		"id": 1170354775776321500,
+		"id_str": "1170723262835253249",
+		"full_text": "유행\n1966년~1974년.\n출처 사진으로 보는 광복 30년사 (1975) https://t.co/0wGJivvqax",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			41
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1170354758772637700,
+					"id_str": "1170354758772637697",
+					"indices": [
+						42,
+						65
+					],
+					"media_url": "http://pbs.twimg.com/media/ED3vc8nUwAE9_rc.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ED3vc8nUwAE9_rc.jpg",
+					"url": "https://t.co/0wGJivvqax",
+					"display_url": "pic.twitter.com/0wGJivvqax",
+					"expanded_url": "https://twitter.com/cityscape_360/status/1170354775776321540/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 586,
+							"h": 1024,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 389,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 586,
+							"h": 1024,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1170354758772637700,
+					"id_str": "1170354758772637697",
+					"indices": [
+						42,
+						65
+					],
+					"media_url": "http://pbs.twimg.com/media/ED3vc8nUwAE9_rc.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ED3vc8nUwAE9_rc.jpg",
+					"url": "https://t.co/0wGJivvqax",
+					"display_url": "pic.twitter.com/0wGJivvqax",
+					"expanded_url": "https://twitter.com/cityscape_360/status/1170354775776321540/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 586,
+							"h": 1024,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 389,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 586,
+							"h": 1024,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1170354766993453000,
+					"id_str": "1170354766993453056",
+					"indices": [
+						42,
+						65
+					],
+					"media_url": "http://pbs.twimg.com/media/ED3vdbPUcAAs9Ki.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ED3vdbPUcAAs9Ki.jpg",
+					"url": "https://t.co/0wGJivvqax",
+					"display_url": "pic.twitter.com/0wGJivvqax",
+					"expanded_url": "https://twitter.com/cityscape_360/status/1170354775776321540/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 362,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 545,
+							"h": 1024,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 545,
+							"h": 1024,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": 1170354289887236000,
+		"in_reply_to_status_id_str": "1170354289887236097",
+		"in_reply_to_user_id": 1128584802083303400,
+		"in_reply_to_user_id_str": "1128584802083303424",
+		"in_reply_to_screen_name": "cityscape_360",
+		"user": {
+			"id": 1128584802083303400,
+			"id_str": "1128584802083303424",
+			"name": "시티스케이프",
+			"screen_name": "cityscape_360",
+			"location": "서울 여기저기",
+			"description": "걷기, 달리기, 버스와 지도.",
+			"url": "https://t.co/0RSnzmnldK",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/0RSnzmnldK",
+							"expanded_url": "http://peing.net/ko/cityscape",
+							"display_url": "peing.net/ko/cityscape",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 687,
+			"friends_count": 35,
+			"listed_count": 11,
+			"created_at": "Wed May 15 08:55:59 +0000 2019",
+			"favourites_count": 276,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 829,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1148212612036870144/_NgcHq-5_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1148212612036870144/_NgcHq-5_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1128584802083303424/1562590194",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 274,
+		"favorite_count": 221,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "유행\n1966년~1974년.\n출처 사진으로 보는 광복 30년사 (1975) https://t.co/0wGJivvqax"
+	},
+	{
 		"created_at": "Sat Sep 07 18:19:46 +0000 2019",
 		"id": 1170401286371467300,
 		"id_str": "1170401286371467264",
