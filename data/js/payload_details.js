7b2bd18ddef0f495ad1cf33e76f509099190b07a
kanokara <emptyknk@gmail.com>
2019-09-13 14:10:59 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 19420,
-	"created_at": "Fri Sep 13 04:10:58 +0000 2019",
+	"tweets": 19421,
+	"created_at": "Fri Sep 13 05:10:58 +0000 2019",
 	"lang": "ko"
 }
--- data/js/tweets/2019_09.js
+++ data/js/tweets/2019_09.js
@@ -1,6 +1,100 @@
 Grailbird.data.tweets_2019_09 = 
 [
 	{
+		"created_at": "Fri Sep 13 04:14:07 +0000 2019",
+		"id": 1169903812615659500,
+		"id_str": "1172362800255000576",
+		"full_text": "아니 왜 여성애자 아니어도 여성연대자라는 뜻으로 레즈비언 쓰면 안되냐니 여자를 사랑하는 남자를 페미니스트라고 부르는거랑 똑같아서 좆같다고요",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			77
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1049840014463393800,
+			"id_str": "1049840014463393794",
+			"name": "초ㅑ",
+			"screen_name": "choyaTR",
+			"location": "탐사자 양심무관 인성무관 시나리오 찾습니다.",
+			"description": "*젠더학을 젠더론이라 일컬으며 지지하지않는다 주장하는 사람은 받지않습니다.\n초ㅑ 티알피지 계정. \n*오타많음\n☂️",
+			"url": "https://t.co/xVROelDmGs",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/xVROelDmGs",
+							"expanded_url": "http://chokzaryosil.tistory.com",
+							"display_url": "chokzaryosil.tistory.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 106,
+			"friends_count": 65,
+			"listed_count": 3,
+			"created_at": "Wed Oct 10 01:52:19 +0000 2018",
+			"favourites_count": 412,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 5773,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1171022894547095558/17oEyBQt_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1171022894547095558/17oEyBQt_normal.jpg",
+			"profile_link_color": "91D2FA",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 3577,
+		"favorite_count": 776,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "ko",
+		"text": "아니 왜 여성애자 아니어도 여성연대자라는 뜻으로 레즈비언 쓰면 안되냐니 여자를 사랑하는 남자를 페미니스트라고 부르는거랑 똑같아서 좆같다고요"
+	},
+	{
 		"created_at": "Fri Sep 13 04:08:54 +0000 2019",
 		"id": 1172320306058412000,
 		"id_str": "1172361488486371328",
