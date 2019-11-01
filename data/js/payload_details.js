8dde102930e9ba6fbe5d69bd63583404d7a00d7d
kanokara <emptyknk@gmail.com>
2019-11-02 07:10:58 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 19544,
-	"created_at": "Fri Nov 01 21:10:58 +0000 2019",
+	"tweets": 19545,
+	"created_at": "Fri Nov 01 22:10:57 +0000 2019",
 	"lang": "ko"
 }
--- data/js/tweets/2019_11.js
+++ data/js/tweets/2019_11.js
@@ -1,6 +1,103 @@
 Grailbird.data.tweets_2019_11 = 
 [
 	{
+		"created_at": "Fri Nov 01 21:11:46 +0000 2019",
+		"id": 1190069662685417500,
+		"id_str": "1190375903181725696",
+		"full_text": "옛날에는 내 트위터 아카이브를 다운로드하면 웹으로 내 트윗을 죽 볼 수 있었는데 지금 트위터 데이터(?)라는 놈을 다운로드하면 걍 JSON파일만 죽 떨궈준다?",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			88
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 50653800,
+			"id_str": "50653800",
+			"name": "Raoul Kim / 김형진",
+			"screen_name": "Raoulian",
+			"location": "37.483573,127.087094",
+			"description": "Game Developer / Enuma, inc.",
+			"url": "https://t.co/ALa8XtEw71",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/ALa8XtEw71",
+							"expanded_url": "http://enuma.com/",
+							"display_url": "enuma.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2976,
+			"friends_count": 2864,
+			"listed_count": 103,
+			"created_at": "Thu Jun 25 14:05:05 +0000 2009",
+			"favourites_count": 62149,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 62290,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1109509360/n1077128533_3873_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1109509360/n1077128533_3873_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/50653800/1476615743",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"can_media_tag": false,
+			"followed_by": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 3,
+		"favorite_count": 4,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "ko",
+		"text": "옛날에는 내 트위터 아카이브를 다운로드하면 웹으로 내 트윗을 죽 볼 수 있었는데 지금 트위터 데이터(?)라는 놈을 다운로드하면 걍 JSON파일만 죽 떨궈준다?"
+	},
+	{
 		"created_at": "Fri Nov 01 20:59:20 +0000 2019",
 		"id": 1189036564992577500,
 		"id_str": "1190372775786995712",
