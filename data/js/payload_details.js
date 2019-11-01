748a0dd6acc1fc2d29306841c27404de1cd8e7d1
kanokara <emptyknk@gmail.com>
2019-11-02 06:10:58 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 19543,
-	"created_at": "Sun Oct 27 02:10:58 +0000 2019",
+	"tweets": 19544,
+	"created_at": "Fri Nov 01 21:10:58 +0000 2019",
 	"lang": "ko"
 }
---
+++ data/js/tweets/2019_11.js
@@ -0,0 +1,87 @@
+Grailbird.data.tweets_2019_11 = 
+[
+	{
+		"created_at": "Fri Nov 01 20:59:20 +0000 2019",
+		"id": 1189036564992577500,
+		"id_str": "1190372775786995712",
+		"full_text": "아들이 어릴적 소중히하던 인형을 여행지에서 잃어버려서 「그러니까 들고다니지 말랬지?」라고 화냈더니 「슬프니까 화내지말아줘」라고 말해 깜짝놀랐다.\n요전, 내가 겉옷을 잃어버렸을 때 「괜찮아? 슬프지 않아?」라고 말해줬던 아들.\n화내는건 자신을 위해서지, 상대를 위해서가 아님을 깨달았다.",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			159
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1182177784161943600,
+			"id_str": "1182177784161943552",
+			"name": "으랏차 일본트윗 번역맨",
+			"screen_name": "Honyakuuman",
+			"location": "",
+			"description": "日本語ツイート翻訳マン 일본어가 서투른 편",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 20345,
+			"friends_count": 4,
+			"listed_count": 72,
+			"created_at": "Thu Oct 10 06:15:33 +0000 2019",
+			"favourites_count": 279,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 183,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1182178663619387393/9BJzIYnx_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1182178663619387393/9BJzIYnx_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1182177784161943552/1570939508",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"can_media_tag": false,
+			"followed_by": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 19235,
+		"favorite_count": 10251,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "ko",
+		"text": "아들이 어릴적 소중히하던 인형을 여행지에서 잃어버려서 「그러니까 들고다니지 말랬지?」라고 화냈더니 「슬프니까 화내지말아줘」라고 말해 깜짝놀랐다.\n요전, 내가 겉옷을 잃어버렸을 때 「괜찮아? 슬프지 않아?」라고 말해줬던 아들.\n화내는건 자신을 위해서지, 상대를 위해서가 아님을 깨달았다."
+	}
+]
