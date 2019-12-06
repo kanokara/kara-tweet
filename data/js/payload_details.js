a886845edaa959c77acac20b41b0d4053de4e82a
kanokara <emptyknk@gmail.com>
2019-12-06 23:10:58 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 19567,
-	"created_at": "Wed Nov 20 19:10:58 +0000 2019",
+	"tweets": 19568,
+	"created_at": "Fri Dec 06 14:10:57 +0000 2019",
 	"lang": "ko"
 }
---
+++ data/js/tweets/2019_12.js
@@ -0,0 +1,98 @@
+Grailbird.data.tweets_2019_12 = 
+[
+	{
+		"created_at": "Fri Dec 06 13:19:17 +0000 2019",
+		"id": 1202940574875013000,
+		"id_str": "1202940574875013120",
+		"full_text": "\"Bluntschli의 이같은 설명은 현재 한국에서 받아들여지고 있는 민족, 국민 개념과 완벽하게 일치한다. 그의 용어에서 nation은 한국의 민족과 동의어라고 할 수 있고, volk는 한국의 국민과 동의어라고 할 수 있다. 문제는 이같은 용어 정의를 영미권 용어로 대입할 때 발생\"\nhttps://t.co/0Q1GYJSb5y",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			183
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/0Q1GYJSb5y",
+					"expanded_url": "http://lyuen.egloos.com/m/4871928",
+					"display_url": "lyuen.egloos.com/m/4871928",
+					"indices": [
+						160,
+						183
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3255599052,
+			"id_str": "3255599052",
+			"name": "Solitude [동결:11/20~]",
+			"screen_name": "EmptyKnK",
+			"location": "",
+			"description": "탄식하라,  덧없이 흩어지고 말리라",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": true,
+			"followers_count": 9,
+			"friends_count": 79,
+			"listed_count": 0,
+			"created_at": "Thu Jun 25 13:48:16 +0000 2015",
+			"favourites_count": 1310,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 11406,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/902611212990464001/uFpl92QO_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/902611212990464001/uFpl92QO_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3255599052/1504034318",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"can_media_tag": false,
+			"followed_by": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "\"Bluntschli의 이같은 설명은 현재 한국에서 받아들여지고 있는 민족, 국민 개념과 완벽하게 일치한다. 그의 용어에서 nation은 한국의 민족과 동의어라고 할 수 있고, volk는 한국의 국민과 동의어라고 할 수 있다. 문제는 이같은 용어 정의를 영미권 용어로 대입할 때 발생\"\nhttps://t.co/0Q1GYJSb5y"
+	}
+]
