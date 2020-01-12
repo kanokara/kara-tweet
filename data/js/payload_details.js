ed0936f12863f74acdba0a319e3a0c110fc25bc8
kanokara <emptyknk@gmail.com>
2020-01-12 17:10:58 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 19569,
-	"created_at": "Wed Dec 18 03:10:57 +0000 2019",
+	"tweets": 19570,
+	"created_at": "Sun Jan 12 08:10:57 +0000 2020",
 	"lang": "ko"
 }
---
+++ data/js/tweets/2020_01.js
@@ -0,0 +1,87 @@
+Grailbird.data.tweets_2020_01 = 
+[
+	{
+		"created_at": "Sun Jan 12 07:54:21 +0000 2020",
+		"id": 1216267154376417300,
+		"id_str": "1216267154376417280",
+		"full_text": "반면 조선에서는 당대 어느 국가보다 중앙의 행정력을 하부까지 투사하였고, 백성들이 억울함의 호소를 통한 공적 구제에 기댔기에 '한'에 치우친 문화가 형성된 거고요. 한국에서 무협이 성립할 수 없는 이유죠.",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			114
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": 1216266409182818300,
+		"in_reply_to_status_id_str": "1216266409182818305",
+		"in_reply_to_user_id": 3255599052,
+		"in_reply_to_user_id_str": "3255599052",
+		"in_reply_to_screen_name": "EmptyKnK",
+		"user": {
+			"id": 3255599052,
+			"id_str": "3255599052",
+			"name": "Solitude [동결:11/20~]",
+			"screen_name": "EmptyKnK",
+			"location": "",
+			"description": "탄식하라,  덧없이 흩어지고 말리라",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": true,
+			"followers_count": 9,
+			"friends_count": 79,
+			"listed_count": 0,
+			"created_at": "Thu Jun 25 13:48:16 +0000 2015",
+			"favourites_count": 1305,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 11408,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/902611212990464001/uFpl92QO_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/902611212990464001/uFpl92QO_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3255599052/1504034318",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"can_media_tag": false,
+			"followed_by": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "ko",
+		"text": "반면 조선에서는 당대 어느 국가보다 중앙의 행정력을 하부까지 투사하였고, 백성들이 억울함의 호소를 통한 공적 구제에 기댔기에 '한'에 치우친 문화가 형성된 거고요. 한국에서 무협이 성립할 수 없는 이유죠."
+	}
+]
