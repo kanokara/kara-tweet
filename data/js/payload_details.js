70ce080e8afd8c47ce61dd41625aefcda49e0c3e
kanokara <emptyknk@gmail.com>
2020-02-02 09:10:58 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 19584,
-	"created_at": "Sat Feb 01 00:10:57 +0000 2020",
+	"tweets": 19585,
+	"created_at": "Sun Feb 02 00:10:57 +0000 2020",
 	"lang": "ko"
 }
---
+++ data/js/tweets/2020_02.js
@@ -0,0 +1,206 @@
+Grailbird.data.tweets_2020_02 = 
+[
+	{
+		"created_at": "Sat Feb 01 23:39:46 +0000 2020",
+		"id": 1223752834362966000,
+		"id_str": "1223752834362966018",
+		"full_text": "한국 특유의 피해자 정서, 자신을 피해자의 입장에 두고서 도덕적 우위에 서려는 경향은 일제의 압제에서 원인을 찾기엔 그 유교적 뿌리가 깊죠.\nhttps://t.co/5VvLGw4Uql",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			102
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/5VvLGw4Uql",
+					"expanded_url": "https://twitter.com/cityknight2000/status/1223414091697418240?s=19",
+					"display_url": "twitter.com/cityknight2000…",
+					"indices": [
+						79,
+						102
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": 1216267154376417300,
+		"in_reply_to_status_id_str": "1216267154376417280",
+		"in_reply_to_user_id": 3255599052,
+		"in_reply_to_user_id_str": "3255599052",
+		"in_reply_to_screen_name": "EmptyKnK",
+		"user": {
+			"id": 3255599052,
+			"id_str": "3255599052",
+			"name": "Solitude [동결:11/20~]",
+			"screen_name": "EmptyKnK",
+			"location": "",
+			"description": "탄식하라,  덧없이 흩어지고 말리라",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": true,
+			"followers_count": 9,
+			"friends_count": 80,
+			"listed_count": 0,
+			"created_at": "Thu Jun 25 13:48:16 +0000 2015",
+			"favourites_count": 1343,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 11419,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/902611212990464001/uFpl92QO_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/902611212990464001/uFpl92QO_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3255599052/1504034318",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"can_media_tag": false,
+			"followed_by": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1223414091697418200,
+		"quoted_status_id_str": "1223414091697418240",
+		"quoted_status_permalink": {
+			"url": "https://t.co/5VvLGw4Uql",
+			"expanded": "https://twitter.com/cityknight2000/status/1223414091697418240?s=19",
+			"display": "twitter.com/cityknight2000…"
+		},
+		"quoted_status": {
+			"created_at": "Sat Feb 01 01:13:44 +0000 2020",
+			"id": 1223414091697418200,
+			"id_str": "1223414091697418240",
+			"full_text": "일본인 학자 중에서 한일관계와 한국인이 갖는 “한”에 대해서 잉글랜드-아일랜드 관계로 설명하시는 분이 계셨습니다. 이미 은퇴하신 국제관계론 학자셨음. https://t.co/McF7SH8IAL",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				83
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/McF7SH8IAL",
+						"expanded_url": "https://twitter.com/kkendd/status/1223102913314115584",
+						"display_url": "twitter.com/kkendd/status/…",
+						"indices": [
+							84,
+							107
+						]
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 113556467,
+				"id_str": "113556467",
+				"name": "Mr.Japaneses Studies Ph.D.【2020 Goal=Write a book】",
+				"screen_name": "cityknight2000",
+				"location": "兵庫 神戸市",
+				"description": "Political Science(Ph.D.), Korean, Korean-Japanese Simultaneous Interpreter&Social Coordinator, Author. My motto → 「Better than the Best」「Ancora lmparo」",
+				"url": null,
+				"entities": {
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 1229,
+				"friends_count": 887,
+				"listed_count": 31,
+				"created_at": "Fri Feb 12 05:58:11 +0000 2010",
+				"favourites_count": 16227,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 10285,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "642D8B",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme10/bg.gif",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme10/bg.gif",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1089060224063795200/5CmZnADF_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1089060224063795200/5CmZnADF_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/113556467/1559745741",
+				"profile_link_color": "981CEB",
+				"profile_sidebar_border_color": "65B0DA",
+				"profile_sidebar_fill_color": "7AC3EE",
+				"profile_text_color": "3D1957",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"can_media_tag": false,
+				"followed_by": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": true,
+			"quoted_status_id": 1223102913314115600,
+			"quoted_status_id_str": "1223102913314115584",
+			"quoted_status_permalink": {
+				"url": "https://t.co/McF7SH8IAL",
+				"expanded": "https://twitter.com/kkendd/status/1223102913314115584",
+				"display": "twitter.com/kkendd/status/…"
+			},
+			"retweet_count": 269,
+			"favorite_count": 121,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "ko"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "한국 특유의 피해자 정서, 자신을 피해자의 입장에 두고서 도덕적 우위에 서려는 경향은 일제의 압제에서 원인을 찾기엔 그 유교적 뿌리가 깊죠.\nhttps://t.co/5VvLGw4Uql"
+	}
+]
