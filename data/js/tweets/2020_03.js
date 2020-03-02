6e56d47d6e2f211ca122872f082184e1f85913ce
kanokara <emptyknk@gmail.com>
2020-03-02 13:10:58 +0900

    Added 1 tweets

---
+++ data/js/tweets/2020_03.js
@@ -0,0 +1,85 @@
+Grailbird.data.tweets_2020_03 = 
+[
+	{
+		"created_at": "Mon Mar 02 03:44:37 +0000 2020",
+		"id": 786406549841322000,
+		"id_str": "1234323698846318592",
+		"full_text": "신문이라는 매체의 공식 트위터계정이 보다 정제된 단어와 10년 뒤에 봐도 창피하지 않을 표현으로 구성되어 있기를 바라는 것은 과도한 소망인가? 아사히신문 한국어계정이 그립다",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			96
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"http://mvilla.it/fenix\" rel=\"nofollow\">Fenix for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 715273915,
+			"id_str": "715273915",
+			"name": "Aisha",
+			"screen_name": "windycityexo",
+			"location": "나는 놀려고 일해",
+			"description": "세상이 세훈이에게 늘 상냥하기를",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 144,
+			"friends_count": 77,
+			"listed_count": 7,
+			"created_at": "Wed Jul 25 01:50:30 +0000 2012",
+			"favourites_count": 1814,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 104406,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1153156648770273280/RwmOlA7E_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1153156648770273280/RwmOlA7E_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/715273915/1574852501",
+			"profile_link_color": "3B94D9",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "ko",
+		"text": "신문이라는 매체의 공식 트위터계정이 보다 정제된 단어와 10년 뒤에 봐도 창피하지 않을 표현으로 구성되어 있기를 바라는 것은 과도한 소망인가? 아사히신문 한국어계정이 그립다"
+	}
+]
