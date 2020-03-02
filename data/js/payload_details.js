b4a50e052be75c8013ce996f6df83ac49579c966
kanokara <emptyknk@gmail.com>
2020-03-02 21:10:58 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 19616,
-	"created_at": "Mon Mar 02 04:10:57 +0000 2020",
+	"tweets": 19617,
+	"created_at": "Mon Mar 02 12:10:57 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_03.js
+++ data/js/tweets/2020_03.js
@@ -1,6 +1,167 @@
 Grailbird.data.tweets_2020_03 = 
 [
 	{
+		"created_at": "Mon Mar 02 11:22:28 +0000 2020",
+		"id": 1234341789823197200,
+		"id_str": "1234438923880845312",
+		"full_text": "이시국 안즈레 ㅅㅂㅋㅋㄱㄱㅋ https://t.co/a2bkRgxyRz",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			15
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1234341778163060700,
+					"id_str": "1234341778163060736",
+					"indices": [
+						16,
+						39
+					],
+					"media_url": "http://pbs.twimg.com/media/ESFDTrLVUAAjEfZ.png",
+					"media_url_https": "https://pbs.twimg.com/media/ESFDTrLVUAAjEfZ.png",
+					"url": "https://t.co/a2bkRgxyRz",
+					"display_url": "pic.twitter.com/a2bkRgxyRz",
+					"expanded_url": "https://twitter.com/SH1FT_K3Y/status/1234341789823197184/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 488,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 721,
+							"h": 1004,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 721,
+							"h": 1004,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1234341778163060700,
+					"id_str": "1234341778163060736",
+					"indices": [
+						16,
+						39
+					],
+					"media_url": "http://pbs.twimg.com/media/ESFDTrLVUAAjEfZ.png",
+					"media_url_https": "https://pbs.twimg.com/media/ESFDTrLVUAAjEfZ.png",
+					"url": "https://t.co/a2bkRgxyRz",
+					"display_url": "pic.twitter.com/a2bkRgxyRz",
+					"expanded_url": "https://twitter.com/SH1FT_K3Y/status/1234341789823197184/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 488,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 721,
+							"h": 1004,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 721,
+							"h": 1004,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 844968110964670500,
+			"id_str": "844968110964670464",
+			"name": "시프트P/シフトP @민초사랑단",
+			"screen_name": "SH1FT_K3Y",
+			"location": "본진*서현 게임파크",
+			"description": "디댤 주력/DRS 10LV FC Masta/일도리/EZ2AC 5k only 15/アナスタシアP/LOL/SDVX/Muse dash/항상 피곤한 머학생...../이별은 블언블,언팔은 오류임 ㅇㅇ/커플인장 w/ @DemonsTRA_tion/#アーニャは天使",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1036,
+			"friends_count": 926,
+			"listed_count": 46,
+			"created_at": "Thu Mar 23 17:44:31 +0000 2017",
+			"favourites_count": 18611,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 53321,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1234340293891088385/MGchvuLJ_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1234340293891088385/MGchvuLJ_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/844968110964670464/1577629626",
+			"profile_link_color": "F58EA8",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 324,
+		"favorite_count": 179,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "이시국 안즈레 ㅅㅂㅋㅋㄱㄱㅋ https://t.co/a2bkRgxyRz"
+	},
+	{
 		"created_at": "Mon Mar 02 03:44:37 +0000 2020",
 		"id": 786406549841322000,
 		"id_str": "1234323698846318592",
