f795d5bcb8b161c2e6c7e9c8ed7d8ff1a73e5c3f
kanokara <emptyknk@gmail.com>
2020-03-29 13:10:59 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 19714,
-	"created_at": "Sat Mar 28 23:10:58 +0000 2020",
+	"tweets": 19715,
+	"created_at": "Sun Mar 29 04:10:58 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_03.js
+++ data/js/tweets/2020_03.js
@@ -1,6 +1,387 @@
 Grailbird.data.tweets_2020_03 = 
 [
 	{
+		"created_at": "Sun Mar 29 03:28:50 +0000 2020",
+		"id": 1244104202437030000,
+		"id_str": "1244104202437029890",
+		"full_text": "해당 작품의 특수성에 국한한 것이 아니라 일반화한 논지의 거증이라면, 우노 츠네히로 유의 상식을 가장하여 세간의 선입관에 부응하고 강화할 뿐인 속류 비평 같네요. 메타 게임이란 게 처음 본 소리도 아니고.\nhttps://t.co/YV110T4EBd",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			138
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/YV110T4EBd",
+					"expanded_url": "https://twitter.com/ho_nol_lulu/status/1243944664656277505",
+					"display_url": "twitter.com/ho_nol_lulu/st…",
+					"indices": [
+						115,
+						138
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3255599052,
+			"id_str": "3255599052",
+			"name": "Solitude [동결:11/20~]",
+			"screen_name": "EmptyKnK",
+			"location": "",
+			"description": "탄식하라,  덧없이 흩어지고 말리라",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": true,
+			"followers_count": 9,
+			"friends_count": 85,
+			"listed_count": 0,
+			"created_at": "Thu Jun 25 13:48:16 +0000 2015",
+			"favourites_count": 1600,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 11560,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/902611212990464001/uFpl92QO_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/902611212990464001/uFpl92QO_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3255599052/1504034318",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1243944664656277500,
+		"quoted_status_id_str": "1243944664656277505",
+		"quoted_status_permalink": {
+			"url": "https://t.co/YV110T4EBd",
+			"expanded": "https://twitter.com/ho_nol_lulu/status/1243944664656277505",
+			"display": "twitter.com/ho_nol_lulu/st…"
+		},
+		"quoted_status": {
+			"created_at": "Sat Mar 28 16:54:54 +0000 2020",
+			"id": 1243944664656277500,
+			"id_str": "1243944664656277505",
+			"full_text": "타인을 부감하는게 되게보편적인 상위포지션쟁탈방법이엇구나... (또 현상을 바깥에서 부감하면서 자기만쏙빠지는것도) 안그래도 트위터에서 되도않는분석하는애들 생각낫는데 인용문에 트위터얘기나와서뿜넛음ㅋㅋㅋㅋ https://t.co/vEcnPgKo5k",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				111
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 1243944631647129600,
+						"id_str": "1243944631647129601",
+						"indices": [
+							112,
+							135
+						],
+						"media_url": "http://pbs.twimg.com/media/EUNhDUGU0AE9vYf.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/EUNhDUGU0AE9vYf.jpg",
+						"url": "https://t.co/vEcnPgKo5k",
+						"display_url": "pic.twitter.com/vEcnPgKo5k",
+						"expanded_url": "https://twitter.com/ho_nol_lulu/status/1243944664656277505/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 615,
+								"h": 1024,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 408,
+								"h": 680,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 615,
+								"h": 1024,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1243944631647129600,
+						"id_str": "1243944631647129601",
+						"indices": [
+							112,
+							135
+						],
+						"media_url": "http://pbs.twimg.com/media/EUNhDUGU0AE9vYf.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/EUNhDUGU0AE9vYf.jpg",
+						"url": "https://t.co/vEcnPgKo5k",
+						"display_url": "pic.twitter.com/vEcnPgKo5k",
+						"expanded_url": "https://twitter.com/ho_nol_lulu/status/1243944664656277505/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 615,
+								"h": 1024,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 408,
+								"h": 680,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 615,
+								"h": 1024,
+								"resize": "fit"
+							}
+						}
+					},
+					{
+						"id": 1243944641000398800,
+						"id_str": "1243944641000398850",
+						"indices": [
+							112,
+							135
+						],
+						"media_url": "http://pbs.twimg.com/media/EUNhD28UYAI5ycO.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/EUNhD28UYAI5ycO.jpg",
+						"url": "https://t.co/vEcnPgKo5k",
+						"display_url": "pic.twitter.com/vEcnPgKo5k",
+						"expanded_url": "https://twitter.com/ho_nol_lulu/status/1243944664656277505/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 579,
+								"h": 1024,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 384,
+								"h": 680,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 579,
+								"h": 1024,
+								"resize": "fit"
+							}
+						}
+					},
+					{
+						"id": 1243944649019945000,
+						"id_str": "1243944649019944961",
+						"indices": [
+							112,
+							135
+						],
+						"media_url": "http://pbs.twimg.com/media/EUNhEU0U8AEubNy.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/EUNhEU0U8AEubNy.jpg",
+						"url": "https://t.co/vEcnPgKo5k",
+						"display_url": "pic.twitter.com/vEcnPgKo5k",
+						"expanded_url": "https://twitter.com/ho_nol_lulu/status/1243944664656277505/photo/1",
+						"type": "photo",
+						"sizes": {
+							"large": {
+								"w": 690,
+								"h": 1024,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 690,
+								"h": 1024,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 458,
+								"h": 680,
+								"resize": "fit"
+							}
+						}
+					},
+					{
+						"id": 1243944657362374700,
+						"id_str": "1243944657362374658",
+						"indices": [
+							112,
+							135
+						],
+						"media_url": "http://pbs.twimg.com/media/EUNhEz5UUAIFvrV.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/EUNhEz5UUAIFvrV.jpg",
+						"url": "https://t.co/vEcnPgKo5k",
+						"display_url": "pic.twitter.com/vEcnPgKo5k",
+						"expanded_url": "https://twitter.com/ho_nol_lulu/status/1243944664656277505/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 398,
+								"h": 680,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 599,
+								"h": 1024,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 599,
+								"h": 1024,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 930044012660002800,
+				"id_str": "930044012660002816",
+				"name": "에스비",
+				"screen_name": "ho_nol_lulu",
+				"location": "",
+				"description": "다욕함",
+				"url": "https://t.co/yTYhqgNRpR",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/yTYhqgNRpR",
+								"expanded_url": "http://brwiorfnkfdsfjkfdsddftt.blogspot.com/",
+								"display_url": "brwiorfnkfdsfjkfdsddftt.blogspot.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 593,
+				"friends_count": 57,
+				"listed_count": 16,
+				"created_at": "Mon Nov 13 12:05:46 +0000 2017",
+				"favourites_count": 8565,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 25015,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "F5F8FA",
+				"profile_background_image_url": null,
+				"profile_background_image_url_https": null,
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/999481765461807105/nqehxoxT_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/999481765461807105/nqehxoxT_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/930044012660002816/1515428626",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": true,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 176,
+			"favorite_count": 137,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "ko"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "해당 작품의 특수성에 국한한 것이 아니라 일반화한 논지의 거증이라면, 우노 츠네히로 유의 상식을 가장하여 세간의 선입관에 부응하고 강화할 뿐인 속류 비평 같네요. 메타 게임이란 게 처음 본 소리도 아니고.\nhttps://t.co/YV110T4EBd"
+	},
+	{
 		"created_at": "Sat Mar 28 22:36:11 +0000 2020",
 		"id": 1244030551935213600,
 		"id_str": "1244030551935213569",
