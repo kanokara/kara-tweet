40198177bce687f0a22362c44ab5c8d62f45a50a
kanokara <emptyknk@gmail.com>
2020-10-18 09:11:00 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 20449,
-	"created_at": "Sat Oct 17 16:10:59 +0000 2020",
+	"tweets": 20450,
+	"created_at": "Sun Oct 18 00:10:59 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_10.js
+++ data/js/tweets/2020_10.js
@@ -1,6 +1,99 @@
 Grailbird.data.tweets_2020_10 = 
 [
 	{
+		"created_at": "Sat Oct 17 23:56:00 +0000 2020",
+		"id": 1317615375018647600,
+		"id_str": "1317615375018647552",
+		"full_text": "\"경영자 시선\"과 관련된 세 가지 관점\nhttps://t.co/v3in3TK4iG\n\n기업에서 '인사'가 아니라 'HR(인적 자원)'이라고 표현하는 걸 보면 역겨울 때가 있죠. 프로이센 군참모부에서 비롯한 시각을 여과 없이 보여주는. 한국에선 90년대 이후 대중화된 '몸값'이 그 예.",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			158
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/v3in3TK4iG",
+					"expanded_url": "http://sonnet.egloos.com/7502406",
+					"display_url": "sonnet.egloos.com/7502406",
+					"indices": [
+						22,
+						45
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3255599052,
+			"id_str": "3255599052",
+			"name": "Solitude [동결:11/20~]",
+			"screen_name": "EmptyKnK",
+			"location": "",
+			"description": "탄식하라,  덧없이 흩어지고 말리라",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": true,
+			"followers_count": 9,
+			"friends_count": 84,
+			"listed_count": 0,
+			"created_at": "Thu Jun 25 13:48:16 +0000 2015",
+			"favourites_count": 2396,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 12335,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/902611212990464001/uFpl92QO_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/902611212990464001/uFpl92QO_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3255599052/1504034318",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "\"경영자 시선\"과 관련된 세 가지 관점\nhttps://t.co/v3in3TK4iG\n\n기업에서 '인사'가 아니라 'HR(인적 자원)'이라고 표현하는 걸 보면 역겨울 때가 있죠. 프로이센 군참모부에서 비롯한 시각을 여과 없이 보여주는. 한국에선 90년대 이후 대중화된 '몸값'이 그 예."
+	},
+	{
 		"created_at": "Sat Oct 17 15:57:54 +0000 2020",
 		"id": 1317495058031992800,
 		"id_str": "1317495058031992832",
