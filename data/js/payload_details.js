4b30a21bcfa71ab82c7841b5bba48eebd3cb58a3
kanokara <emptyknk@gmail.com>
2020-11-02 22:10:59 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 20478,
-	"created_at": "Sat Oct 31 09:10:59 +0000 2020",
+	"tweets": 20479,
+	"created_at": "Mon Nov 02 13:10:58 +0000 2020",
 	"lang": "ko"
 }
---
+++ data/js/tweets/2020_11.js
@@ -0,0 +1,98 @@
+Grailbird.data.tweets_2020_11 = 
+[
+	{
+		"created_at": "Mon Nov 02 13:08:41 +0000 2020",
+		"id": 1323250043499896800,
+		"id_str": "1323250679951994881",
+		"full_text": "기후위기로 위협을 받는 사람, 위험을 느끼는 사람들조차 활동가를 좇는다. 화재가 났는데도 불난 곳이 아니라 경보음을 향하는 격이다. 트럼프가 내 얘기를 할 때도 사람들은 기후 자체가 아니라 (두 사람의) 갈등에 관해 이야기한다.",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			127
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1323250040911978500,
+		"in_reply_to_status_id_str": "1323250040911978497",
+		"in_reply_to_user_id": 27725896,
+		"in_reply_to_user_id_str": "27725896",
+		"in_reply_to_screen_name": "atmostbeautiful",
+		"user": {
+			"id": 27725896,
+			"id_str": "27725896",
+			"name": "Journey",
+			"screen_name": "atmostbeautiful",
+			"location": "Seoul",
+			"description": "writer & curator @ book club origin / music book movie yoga piano travel wise human & wild nature / Life is a wonder / REM, at my most beautiful",
+			"url": "https://t.co/MzklD4fR8k",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/MzklD4fR8k",
+							"expanded_url": "http://1boon.kakao.com/bookclub",
+							"display_url": "1boon.kakao.com/bookclub",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 101373,
+			"friends_count": 2385,
+			"listed_count": 314,
+			"created_at": "Mon Mar 30 20:52:06 +0000 2009",
+			"favourites_count": 49,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 16034,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1149094584674242560/wv78dMIl_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1149094584674242560/wv78dMIl_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/27725896/1575690071",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 7,
+		"favorite_count": 10,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "ko",
+		"text": "기후위기로 위협을 받는 사람, 위험을 느끼는 사람들조차 활동가를 좇는다. 화재가 났는데도 불난 곳이 아니라 경보음을 향하는 격이다. 트럼프가 내 얘기를 할 때도 사람들은 기후 자체가 아니라 (두 사람의) 갈등에 관해 이야기한다."
+	}
+]
