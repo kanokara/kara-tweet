276c2d2344209ca05858368a57b556938289ce7c
kanokara <emptyknk@gmail.com>
2020-11-10 09:10:59 +0900

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 20528,
-	"created_at": "Mon Nov 09 13:10:58 +0000 2020",
+	"tweets": 20530,
+	"created_at": "Tue Nov 10 00:10:58 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_11.js
+++ data/js/tweets/2020_11.js
@@ -1,6 +1,364 @@
 Grailbird.data.tweets_2020_11 = 
 [
 	{
+		"created_at": "Mon Nov 09 23:54:05 +0000 2020",
+		"id": 1325949815927132200,
+		"id_str": "1325949815927132160",
+		"full_text": "\"무능한 여캐 나오면 여혐, 사악한 여캐 나오면 여혐, 찌질한 여캐 나오면 여혐. 여적여는 여혐! 이러니 진짜 뭐 어쩌라는 건지. 혐오라는 단어 자체가 진저리가 나. 온갖 유형의 남자만 잔뜩 나오는 남적남은 아무래도 좋은 모양이지.\" \"\n여자를 인간이 아닌 여자로만 파악하려니 그럼\" https://t.co/SGdcEDxX7a",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			158
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1325949419343110100,
+					"id_str": "1325949419343110145",
+					"indices": [
+						159,
+						182
+					],
+					"media_url": "http://pbs.twimg.com/media/Ema3-MIU4AE_hGQ.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Ema3-MIU4AE_hGQ.jpg",
+					"url": "https://t.co/SGdcEDxX7a",
+					"display_url": "pic.twitter.com/SGdcEDxX7a",
+					"expanded_url": "https://twitter.com/EmptyKnK/status/1325949815927132160/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 735,
+							"h": 348,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 735,
+							"h": 348,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 322,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1325949419343110100,
+					"id_str": "1325949419343110145",
+					"indices": [
+						159,
+						182
+					],
+					"media_url": "http://pbs.twimg.com/media/Ema3-MIU4AE_hGQ.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Ema3-MIU4AE_hGQ.jpg",
+					"url": "https://t.co/SGdcEDxX7a",
+					"display_url": "pic.twitter.com/SGdcEDxX7a",
+					"expanded_url": "https://twitter.com/EmptyKnK/status/1325949815927132160/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 735,
+							"h": 348,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 735,
+							"h": 348,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 322,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3255599052,
+			"id_str": "3255599052",
+			"name": "Solitude [동결:11/20~]",
+			"screen_name": "EmptyKnK",
+			"location": "",
+			"description": "탄식하라,  덧없이 흩어지고 말리라",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": true,
+			"followers_count": 9,
+			"friends_count": 86,
+			"listed_count": 0,
+			"created_at": "Thu Jun 25 13:48:16 +0000 2015",
+			"favourites_count": 2582,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 12431,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/902611212990464001/uFpl92QO_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/902611212990464001/uFpl92QO_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3255599052/1504034318",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "\"무능한 여캐 나오면 여혐, 사악한 여캐 나오면 여혐, 찌질한 여캐 나오면 여혐. 여적여는 여혐! 이러니 진짜 뭐 어쩌라는 건지. 혐오라는 단어 자체가 진저리가 나. 온갖 유형의 남자만 잔뜩 나오는 남적남은 아무래도 좋은 모양이지.\" \"\n여자를 인간이 아닌 여자로만 파악하려니 그럼\" https://t.co/SGdcEDxX7a"
+	},
+	{
+		"created_at": "Mon Nov 09 23:48:04 +0000 2020",
+		"id": 1325948301980528600,
+		"id_str": "1325948301980528640",
+		"full_text": "상대적으로 소수의견이지만 차라리 하렘물을 본다는 여초 의견. https://t.co/QaW3rlBWX1",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			33
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1325948228420817000,
+					"id_str": "1325948228420816897",
+					"indices": [
+						34,
+						57
+					],
+					"media_url": "http://pbs.twimg.com/media/Ema243mU0AEKcw7.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Ema243mU0AEKcw7.jpg",
+					"url": "https://t.co/QaW3rlBWX1",
+					"display_url": "pic.twitter.com/QaW3rlBWX1",
+					"expanded_url": "https://twitter.com/EmptyKnK/status/1325948301980528640/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 671,
+							"h": 309,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 671,
+							"h": 309,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 671,
+							"h": 309,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1325948228420817000,
+					"id_str": "1325948228420816897",
+					"indices": [
+						34,
+						57
+					],
+					"media_url": "http://pbs.twimg.com/media/Ema243mU0AEKcw7.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Ema243mU0AEKcw7.jpg",
+					"url": "https://t.co/QaW3rlBWX1",
+					"display_url": "pic.twitter.com/QaW3rlBWX1",
+					"expanded_url": "https://twitter.com/EmptyKnK/status/1325948301980528640/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 671,
+							"h": 309,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 671,
+							"h": 309,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 671,
+							"h": 309,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1325948240664039400,
+					"id_str": "1325948240664039424",
+					"indices": [
+						34,
+						57
+					],
+					"media_url": "http://pbs.twimg.com/media/Ema25lNVkAAPYuP.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Ema25lNVkAAPYuP.jpg",
+					"url": "https://t.co/QaW3rlBWX1",
+					"display_url": "pic.twitter.com/QaW3rlBWX1",
+					"expanded_url": "https://twitter.com/EmptyKnK/status/1325948301980528640/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 242,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1123,
+							"h": 400,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1123,
+							"h": 400,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3255599052,
+			"id_str": "3255599052",
+			"name": "Solitude [동결:11/20~]",
+			"screen_name": "EmptyKnK",
+			"location": "",
+			"description": "탄식하라,  덧없이 흩어지고 말리라",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": true,
+			"followers_count": 9,
+			"friends_count": 86,
+			"listed_count": 0,
+			"created_at": "Thu Jun 25 13:48:16 +0000 2015",
+			"favourites_count": 2582,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 12431,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/902611212990464001/uFpl92QO_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/902611212990464001/uFpl92QO_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3255599052/1504034318",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "상대적으로 소수의견이지만 차라리 하렘물을 본다는 여초 의견. https://t.co/QaW3rlBWX1"
+	},
+	{
 		"created_at": "Mon Nov 09 12:25:17 +0000 2020",
 		"id": 1325776474129928200,
 		"id_str": "1325776474129928193",
