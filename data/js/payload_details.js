85a5788bce4a5e239e42290b62da6a66b2c10e26
kanokara <emptyknk@gmail.com>
2021-01-07 21:10:59 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 21000,
-	"created_at": "Thu Jan 07 00:10:58 +0000 2021",
+	"tweets": 21001,
+	"created_at": "Thu Jan 07 12:10:58 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_01.js
+++ data/js/tweets/2021_01.js
@@ -1,6 +1,108 @@
 Grailbird.data.tweets_2021_01 = 
 [
 	{
+		"created_at": "Thu Jan 07 11:57:56 +0000 2021",
+		"id": 1347150475074814000,
+		"id_str": "1347150475074813953",
+		"full_text": "무거운 쇳덩어리 비행기가 하늘에 뜨는 이유는 무엇일까(1)\nhttps://t.co/h581BHjKiX\n\n하늘을 나는 쇳덩어리에 숨겨진 과학적 원리 - 고난을 거쳐 경외의 순간으로\nhttps://t.co/QaAJ4QoYvc",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			123
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/h581BHjKiX",
+					"expanded_url": "https://www.mk.co.kr/premium/life/view/2020/09/29060/",
+					"display_url": "mk.co.kr/premium/life/v…",
+					"indices": [
+						33,
+						56
+					]
+				},
+				{
+					"url": "https://t.co/QaAJ4QoYvc",
+					"expanded_url": "http://mbnmoney.mbn.co.kr/news/view?news_no=MM1004139920",
+					"display_url": "mbnmoney.mbn.co.kr/news/view?news…",
+					"indices": [
+						100,
+						123
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3255599052,
+			"id_str": "3255599052",
+			"name": "Solitude [동결:11/20~]",
+			"screen_name": "EmptyKnK",
+			"location": "",
+			"description": "탄식하라,  덧없이 흩어지고 말리라",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": true,
+			"followers_count": 8,
+			"friends_count": 94,
+			"listed_count": 0,
+			"created_at": "Thu Jun 25 13:48:16 +0000 2015",
+			"favourites_count": 2980,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 12991,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1344647229387071494/FeA3dkeA_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1344647229387071494/FeA3dkeA_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3255599052/1606110388",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "무거운 쇳덩어리 비행기가 하늘에 뜨는 이유는 무엇일까(1)\nhttps://t.co/h581BHjKiX\n\n하늘을 나는 쇳덩어리에 숨겨진 과학적 원리 - 고난을 거쳐 경외의 순간으로\nhttps://t.co/QaAJ4QoYvc"
+	},
+	{
 		"created_at": "Wed Jan 06 23:23:03 +0000 2021",
 		"id": 1346960499963949000,
 		"id_str": "1346960499963949061",
