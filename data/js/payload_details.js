82383b357a6f1b22926eb1937b2f2b06f7290e0a
kanokara <emptyknk@gmail.com>
2021-01-24 00:11:00 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 21113,
-	"created_at": "Sat Jan 23 14:10:59 +0000 2021",
+	"tweets": 21114,
+	"created_at": "Sat Jan 23 15:10:59 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_01.js
+++ data/js/tweets/2021_01.js
@@ -1,6 +1,216 @@
 Grailbird.data.tweets_2021_01 = 
 [
 	{
+		"created_at": "Sat Jan 23 15:02:03 +0000 2021",
+		"id": 1346675941632745500,
+		"id_str": "1352995016231194629",
+		"full_text": "한녀가 한녀 생각을 말하면 몰려와서 시애미질 하는거 100퍼 확률로 한녀들이라 이거 너무 생각나게 함 https://t.co/dtfXlS4QdO",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			56
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1346675932396834800,
+					"id_str": "1346675932396834817",
+					"indices": [
+						57,
+						80
+					],
+					"media_url": "http://pbs.twimg.com/media/ErBaoCIU0AEvpPP.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ErBaoCIU0AEvpPP.jpg",
+					"url": "https://t.co/dtfXlS4QdO",
+					"display_url": "pic.twitter.com/dtfXlS4QdO",
+					"expanded_url": "https://twitter.com/k_girl_daily/status/1346675941632745473/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 329,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 952,
+							"h": 1970,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 580,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1346675932396834800,
+					"id_str": "1346675932396834817",
+					"indices": [
+						57,
+						80
+					],
+					"media_url": "http://pbs.twimg.com/media/ErBaoCIU0AEvpPP.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ErBaoCIU0AEvpPP.jpg",
+					"url": "https://t.co/dtfXlS4QdO",
+					"display_url": "pic.twitter.com/dtfXlS4QdO",
+					"expanded_url": "https://twitter.com/k_girl_daily/status/1346675941632745473/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 329,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 952,
+							"h": 1970,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 580,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1346675938612760600,
+					"id_str": "1346675938612760576",
+					"indices": [
+						57,
+						80
+					],
+					"media_url": "http://pbs.twimg.com/media/ErBaoZSUUAAu_9t.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ErBaoZSUUAAu_9t.jpg",
+					"url": "https://t.co/dtfXlS4QdO",
+					"display_url": "pic.twitter.com/dtfXlS4QdO",
+					"expanded_url": "https://twitter.com/k_girl_daily/status/1346675941632745473/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1059,
+							"h": 1488,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 854,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 484,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1309052888409419800,
+			"id_str": "1309052888409419777",
+			"name": "일상",
+			"screen_name": "k_girl_daily",
+			"location": "",
+			"description": "개인주의 한녀 살려",
+			"url": "https://t.co/IAfkCtbob8",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/IAfkCtbob8",
+							"expanded_url": "https://peing.net/ko/k_girl_daily",
+							"display_url": "peing.net/ko/k_girl_daily",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 221,
+			"friends_count": 103,
+			"listed_count": 0,
+			"created_at": "Thu Sep 24 08:51:52 +0000 2020",
+			"favourites_count": 668,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 2406,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1322613302027984897/syfL595K_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1322613302027984897/syfL595K_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1309052888409419777/1604169986",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 14,
+		"favorite_count": 17,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "한녀가 한녀 생각을 말하면 몰려와서 시애미질 하는거 100퍼 확률로 한녀들이라 이거 너무 생각나게 함 https://t.co/dtfXlS4QdO"
+	},
+	{
 		"created_at": "Sat Jan 23 13:29:46 +0000 2021",
 		"id": 1351892168953139200,
 		"id_str": "1352971790960611328",
