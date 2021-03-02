3943901e92ada2790c2a1a748820ed3ebdc75e43
kanokara <emptyknk@gmail.com>
2021-03-02 18:10:58 +0900

    Added 1 tweets

--- data/js/tweets/2021_03.js
+++ data/js/tweets/2021_03.js
@@ -1,6 +1,197 @@
 Grailbird.data.tweets_2021_03 = 
 [
 	{
+		"created_at": "Tue Mar 02 08:58:41 +0000 2021",
+		"id": 1366032816497041400,
+		"id_str": "1366674310354493442",
+		"full_text": "아씨발 영화드라마검사특징 ㅇㅈㄹ존나웃겨아ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ 개똑같애소름돋아ㅠ https://t.co/mPqWO0JEe8",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			60
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1366032536665645000,
+					"id_str": "1366032536665645056",
+					"indices": [
+						61,
+						84
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1366032536665645056/pu/img/O_1EgPxeylTIzgIA.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1366032536665645056/pu/img/O_1EgPxeylTIzgIA.jpg",
+					"url": "https://t.co/mPqWO0JEe8",
+					"display_url": "pic.twitter.com/mPqWO0JEe8",
+					"expanded_url": "https://twitter.com/thumpoo/status/1366032816497041408/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1280,
+							"h": 720,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1366032536665645000,
+					"id_str": "1366032536665645056",
+					"indices": [
+						61,
+						84
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1366032536665645056/pu/img/O_1EgPxeylTIzgIA.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1366032536665645056/pu/img/O_1EgPxeylTIzgIA.jpg",
+					"url": "https://t.co/mPqWO0JEe8",
+					"display_url": "pic.twitter.com/mPqWO0JEe8",
+					"expanded_url": "https://twitter.com/thumpoo/status/1366032816497041408/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1280,
+							"h": 720,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							16,
+							9
+						],
+						"duration_millis": 108127,
+						"variants": [
+							{
+								"bitrate": 256000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1366032536665645056/pu/vid/480x270/k_UnHco2zL6noAPB.mp4?tag=10"
+							},
+							{
+								"bitrate": 2176000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1366032536665645056/pu/vid/1280x720/I-GigB5emUd7He_G.mp4?tag=10"
+							},
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/ext_tw_video/1366032536665645056/pu/pl/pf4EWWKztneLg9tN.m3u8?tag=10"
+							},
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1366032536665645056/pu/vid/640x360/vsraYJKGLps-Vg8a.mp4?tag=10"
+							}
+						]
+					},
+					"additional_media_info": {
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1358717414859366400,
+			"id_str": "1358717414859366403",
+			"name": "꿍..",
+			"screen_name": "thumpoo",
+			"location": "",
+			"description": "감성곰신잡덕계..",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 23,
+			"friends_count": 24,
+			"listed_count": 0,
+			"created_at": "Mon Feb 08 10:00:58 +0000 2021",
+			"favourites_count": 61,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 651,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1358725389707665409/YFQcXStb_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1358725389707665409/YFQcXStb_normal.jpg",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 24802,
+		"favorite_count": 10557,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "아씨발 영화드라마검사특징 ㅇㅈㄹ존나웃겨아ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ 개똑같애소름돋아ㅠ https://t.co/mPqWO0JEe8"
+	},
+	{
 		"created_at": "Mon Mar 01 07:22:53 +0000 2021",
 		"id": 1366287810857078800,
 		"id_str": "1366287810857078787",
