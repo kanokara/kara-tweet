1fae0c2608b6e206be56ba5889b9ec41a9668ad7
kanokara <emptyknk@gmail.com>
2021-04-21 02:10:58 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 21515,
-	"created_at": "Wed Apr 14 13:10:58 +0000 2021",
+	"tweets": 21516,
+	"created_at": "Tue Apr 20 17:10:57 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_04.js
+++ data/js/tweets/2021_04.js
@@ -1,6 +1,109 @@
 Grailbird.data.tweets_2021_04 = 
 [
 	{
+		"created_at": "Tue Apr 20 16:11:06 +0000 2021",
+		"id": 1384540133853077500,
+		"id_str": "1384540133853077508",
+		"full_text": "\"하지만 네가 보였다\"\n\"손을 뻗자. 닿을 테니까\"\nhttps://t.co/L4IhjUZnG9\n\n\"청춘이 없는 것도 청춘이다\"\nhttps://t.co/qR4c7CoMO5",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			94
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/L4IhjUZnG9",
+					"expanded_url": "https://blog.naver.com/stussy9505/222314152444",
+					"display_url": "blog.naver.com/stussy9505/222…",
+					"indices": [
+						29,
+						52
+					]
+				},
+				{
+					"url": "https://t.co/qR4c7CoMO5",
+					"expanded_url": "https://blog.naver.com/stussy9505/220348479817",
+					"display_url": "blog.naver.com/stussy9505/220…",
+					"indices": [
+						71,
+						94
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1380904167527436300,
+		"in_reply_to_status_id_str": "1380904167527436299",
+		"in_reply_to_user_id": 3255599052,
+		"in_reply_to_user_id_str": "3255599052",
+		"in_reply_to_screen_name": "EmptyKnK",
+		"user": {
+			"id": 3255599052,
+			"id_str": "3255599052",
+			"name": "Solitude [동결:11/20~]",
+			"screen_name": "EmptyKnK",
+			"location": "",
+			"description": "탄식하라,  덧없이 흩어지고 말리라",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": true,
+			"followers_count": 7,
+			"friends_count": 98,
+			"listed_count": 0,
+			"created_at": "Thu Jun 25 13:48:16 +0000 2015",
+			"favourites_count": 3578,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 13555,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1344647229387071494/FeA3dkeA_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1344647229387071494/FeA3dkeA_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3255599052/1606110388",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "\"하지만 네가 보였다\"\n\"손을 뻗자. 닿을 테니까\"\nhttps://t.co/L4IhjUZnG9\n\n\"청춘이 없는 것도 청춘이다\"\nhttps://t.co/qR4c7CoMO5"
+	},
+	{
 		"created_at": "Wed Apr 14 12:49:41 +0000 2021",
 		"id": 1382301586324553700,
 		"id_str": "1382315120047493125",
