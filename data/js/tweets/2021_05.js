3437ecf5784c96b53da09c0c643fe035212cc91d
kanokara <emptyknk@gmail.com>
2021-05-02 06:10:58 +0900

    Added 1 tweets

---
+++ data/js/tweets/2021_05.js
@@ -0,0 +1,86 @@
+Grailbird.data.tweets_2021_05 = 
+[
+	{
+		"created_at": "Sat May 01 21:05:00 +0000 2021",
+		"id": 1388600366002958300,
+		"id_str": "1388600366002958336",
+		"full_text": "\"이처럼 예측이 불가능한 상황에 직면하면 모두가 그렇듯 당황하면서 방향성을 찾지 못한다. 살면서 한번도 겪어보지 못한  전염병으로 인한 경제 초붕괴(코로나 전에도 침몰 중이긴 했으나)로 세상이 변했고 사람들이 변했고 국민성에도 큰 변화가 생긴 듯 하다.\"\n\n정권이 뉴딜에 실패해서...",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			159
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3255599052,
+			"id_str": "3255599052",
+			"name": "Solitude [동결:11/20~]",
+			"screen_name": "EmptyKnK",
+			"location": "",
+			"description": "탄식하라,  덧없이 흩어지고 말리라",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": true,
+			"followers_count": 7,
+			"friends_count": 98,
+			"listed_count": 0,
+			"created_at": "Thu Jun 25 13:48:16 +0000 2015",
+			"favourites_count": 3583,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 13590,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1344647229387071494/FeA3dkeA_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1344647229387071494/FeA3dkeA_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3255599052/1606110388",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "ko",
+		"text": "\"이처럼 예측이 불가능한 상황에 직면하면 모두가 그렇듯 당황하면서 방향성을 찾지 못한다. 살면서 한번도 겪어보지 못한  전염병으로 인한 경제 초붕괴(코로나 전에도 침몰 중이긴 했으나)로 세상이 변했고 사람들이 변했고 국민성에도 큰 변화가 생긴 듯 하다.\"\n\n정권이 뉴딜에 실패해서..."
+	}
+]
