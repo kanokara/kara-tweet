0febd244831c48be04237c24ee6c96a28c01be0d
kanokara <emptyknk@gmail.com>
2021-05-15 02:10:59 +0900

    Added 1 tweets

--- data/js/tweets/2021_05.js
+++ data/js/tweets/2021_05.js
@@ -1,6 +1,196 @@
 Grailbird.data.tweets_2021_05 = 
 [
 	{
+		"created_at": "Fri May 14 16:17:09 +0000 2021",
+		"id": 1393155655117066200,
+		"id_str": "1393238966585860102",
+		"full_text": "お嬢様のご許可も頂きましたゆえ、\nこれから わたくしめ作のお嬢様以外のイラストもまれにアップ致しますぞ！\n下手の横好きではありますが、ご覧くださると嬉しいですぞ！(再\n#両儀式 #空の境界 https://t.co/K8mH87jMYI",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			94
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "両儀式",
+					"indices": [
+						84,
+						88
+					]
+				},
+				{
+					"text": "空の境界",
+					"indices": [
+						89,
+						94
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1393155397293203500,
+					"id_str": "1393155397293203456",
+					"indices": [
+						95,
+						118
+					],
+					"media_url": "http://pbs.twimg.com/media/E1V7c9yUYAAbF3t.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E1V7c9yUYAAbF3t.jpg",
+					"url": "https://t.co/K8mH87jMYI",
+					"display_url": "pic.twitter.com/K8mH87jMYI",
+					"expanded_url": "https://twitter.com/Todoroki_situji/status/1393155655117066242/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 912,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1050,
+							"h": 1382,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 517,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1393155397293203500,
+					"id_str": "1393155397293203456",
+					"indices": [
+						95,
+						118
+					],
+					"media_url": "http://pbs.twimg.com/media/E1V7c9yUYAAbF3t.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E1V7c9yUYAAbF3t.jpg",
+					"url": "https://t.co/K8mH87jMYI",
+					"display_url": "pic.twitter.com/K8mH87jMYI",
+					"expanded_url": "https://twitter.com/Todoroki_situji/status/1393155655117066242/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 912,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1050,
+							"h": 1382,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 517,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1390582671181373400,
+			"id_str": "1390582671181373442",
+			"name": "轟 源拾郎",
+			"screen_name": "Todoroki_situji",
+			"location": "",
+			"description": "わたくしトルテ家に仕えさせて頂いております、執事の轟 源拾郎と申す者。最近当家のお嬢様がVtuberなるものにご挑戦されているようで。\nどうぞ皆様、お転婆なザッハお嬢様ではありますが、何卒よろしくお願いいたしますぞ。仕え先⇒@Zahhatorute_V",
+			"url": "https://t.co/UDqAthXc6y",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/UDqAthXc6y",
+							"expanded_url": "https://www.youtube.com/channel/UCgRZl0jwfbnc_5Je1duMvbw",
+							"display_url": "youtube.com/channel/UCgRZl…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 353,
+			"friends_count": 301,
+			"listed_count": 6,
+			"created_at": "Fri May 07 08:23:32 +0000 2021",
+			"favourites_count": 363,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 126,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1390585869480775688/-X-LmjoO_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1390585869480775688/-X-LmjoO_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1390582671181373442/1620376939",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 170,
+		"favorite_count": 495,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ja",
+		"text": "お嬢様のご許可も頂きましたゆえ、\nこれから わたくしめ作のお嬢様以外のイラストもまれにアップ致しますぞ！\n下手の横好きではありますが、ご覧くださると嬉しいですぞ！(再\n#両儀式 #空の境界 https://t.co/K8mH87jMYI"
+	},
+	{
 		"created_at": "Fri May 14 15:17:03 +0000 2021",
 		"id": 1393223841116168200,
 		"id_str": "1393223841116168193",
