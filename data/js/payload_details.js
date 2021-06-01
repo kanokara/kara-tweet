5a4a8f9bf81f7f7dc94bdced8a816f6576eb676a
kanokara <emptyknk@gmail.com>
2021-06-01 14:11:00 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 21823,
-	"created_at": "Mon May 31 20:11:00 +0000 2021",
+	"tweets": 21824,
+	"created_at": "Tue Jun 01 05:10:59 +0000 2021",
 	"lang": "ko"
 }
---
+++ data/js/tweets/2021_06.js
@@ -0,0 +1,97 @@
+Grailbird.data.tweets_2021_06 = 
+[
+	{
+		"created_at": "Tue Jun 01 04:32:44 +0000 2021",
+		"id": 1399584677317419000,
+		"id_str": "1399584677317419010",
+		"full_text": "\"우리네 풍습과 인생관을 담았던 뜻깊은 말들이 다 법률 용어와 제도를 정의하는 말로 바뀌었다. ‘식구’가 너무 그립다.\" https://t.co/tQEQT3LTjV",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			91
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/tQEQT3LTjV",
+					"expanded_url": "http://aladin.kr/p/hPpvF",
+					"display_url": "aladin.kr/p/hPpvF",
+					"indices": [
+						68,
+						91
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1399584612834156500,
+		"in_reply_to_status_id_str": "1399584612834156544",
+		"in_reply_to_user_id": 3255599052,
+		"in_reply_to_user_id_str": "3255599052",
+		"in_reply_to_screen_name": "EmptyKnK",
+		"user": {
+			"id": 3255599052,
+			"id_str": "3255599052",
+			"name": "Solitude [동결:11/20~]",
+			"screen_name": "EmptyKnK",
+			"location": "",
+			"description": "탄식하라,  덧없이 흩어지고 말리라",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": true,
+			"followers_count": 7,
+			"friends_count": 98,
+			"listed_count": 0,
+			"created_at": "Thu Jun 25 13:48:16 +0000 2015",
+			"favourites_count": 3742,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 13876,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1344647229387071494/FeA3dkeA_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1344647229387071494/FeA3dkeA_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3255599052/1606110388",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "\"우리네 풍습과 인생관을 담았던 뜻깊은 말들이 다 법률 용어와 제도를 정의하는 말로 바뀌었다. ‘식구’가 너무 그립다.\" https://t.co/tQEQT3LTjV"
+	}
+]
