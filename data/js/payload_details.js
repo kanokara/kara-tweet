a7bc62ea7e9ec568538f63205d53539a63b7c95b
kanokara <emptyknk@gmail.com>
2021-06-16 15:10:59 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 21978,
-	"created_at": "Wed Jun 16 04:10:58 +0000 2021",
+	"tweets": 21979,
+	"created_at": "Wed Jun 16 06:10:58 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_06.js
+++ data/js/tweets/2021_06.js
@@ -1,6 +1,123 @@
 Grailbird.data.tweets_2021_06 = 
 [
 	{
+		"created_at": "Wed Jun 16 05:43:54 +0000 2021",
+		"id": 1405037003805134800,
+		"id_str": "1405038402127339520",
+		"full_text": "보수층의 기후인식 높이려면 비디오를 틀어라? https://t.co/Zip1YXTPv4",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			48
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/Zip1YXTPv4",
+					"expanded_url": "https://www.hani.co.kr/arti/science/science_general/999605.html",
+					"display_url": "hani.co.kr/arti/science/s…",
+					"indices": [
+						25,
+						48
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 113273977,
+			"id_str": "113273977",
+			"name": "한겨레 미래&과학",
+			"screen_name": "SciON_hani",
+			"location": "",
+			"description": "한겨레의 미래/과학/기술/환경의 뉴스와 비평 플랫폼 \"미래&과학\"의 공식 트위터입니다. (과학웹진 사이언스온(https://t.co/RLpWyFfbM6)의 옛 트위터를 사용합니다)",
+			"url": "https://t.co/NKjJm7i5gO",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/NKjJm7i5gO",
+							"expanded_url": "http://www.hani.co.kr/arti/science/home01.html",
+							"display_url": "hani.co.kr/arti/science/h…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/RLpWyFfbM6",
+							"expanded_url": "http://scienceon.hani.co.kr",
+							"display_url": "scienceon.hani.co.kr",
+							"indices": [
+								61,
+								84
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 7043,
+			"friends_count": 24,
+			"listed_count": 452,
+			"created_at": "Thu Feb 11 07:19:42 +0000 2010",
+			"favourites_count": 1,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 4262,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/940129723883036674/xbncUGi8_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/940129723883036674/xbncUGi8_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/113273977/1512546106",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "보수층의 기후인식 높이려면 비디오를 틀어라? https://t.co/Zip1YXTPv4"
+	},
+	{
 		"created_at": "Wed Jun 16 03:57:00 +0000 2021",
 		"id": 1404834623062872000,
 		"id_str": "1405011500461301763",
