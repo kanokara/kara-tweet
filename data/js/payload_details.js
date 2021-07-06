b1adb9a83b93ad504c531829362579c78f6f5daf
kanokara <emptyknk@gmail.com>
2021-07-06 22:10:58 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 22046,
-	"created_at": "Sun Jul 04 02:10:56 +0000 2021",
+	"tweets": 22047,
+	"created_at": "Tue Jul 06 13:10:57 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_07.js
+++ data/js/tweets/2021_07.js
@@ -1,6 +1,113 @@
 Grailbird.data.tweets_2021_07 = 
 [
 	{
+		"created_at": "Tue Jul 06 13:10:14 +0000 2021",
+		"id": 1412373180203761700,
+		"id_str": "1412398485496680448",
+		"full_text": "출판사 라이크노벨,\n\n2021년 라이트노벨 출판합니다. \n\n공식 웹사이트 공지를 봐주세요. \n\nhttps://t.co/eBnOJz6s63",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			76
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/eBnOJz6s63",
+					"expanded_url": "https://likenovel.net/notices?#collapse11",
+					"display_url": "likenovel.net/notices?#colla…",
+					"indices": [
+						53,
+						76
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1354734771675435000,
+			"id_str": "1354734771675435009",
+			"name": "LIKENOVEL_Official",
+			"screen_name": "welikenovel",
+			"location": "대한민국 성남시",
+			"description": "라이트노벨 Web연재 플랫폼 LIKENOVEL 공식 twitter 계정\n\n《Web小説投稿サイト\nLIKENOVEL Official アカウント》\n\n서비스문의: admin@likenovel.net\n작품평가시트 신청:moeking@likenovel.net",
+			"url": "https://t.co/w9fII95NJ0",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/w9fII95NJ0",
+							"expanded_url": "https://likenovel.net",
+							"display_url": "likenovel.net",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 84,
+			"friends_count": 187,
+			"listed_count": 1,
+			"created_at": "Thu Jan 28 10:15:31 +0000 2021",
+			"favourites_count": 156,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 104,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1354735128937779200/3r_8dCs__normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1354735128937779200/3r_8dCs__normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1354734771675435009/1625205524",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 16,
+		"favorite_count": 12,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "출판사 라이크노벨,\n\n2021년 라이트노벨 출판합니다. \n\n공식 웹사이트 공지를 봐주세요. \n\nhttps://t.co/eBnOJz6s63"
+	},
+	{
 		"created_at": "Sun Jul 04 01:30:14 +0000 2021",
 		"id": 1411249792542404600,
 		"id_str": "1411497546455781382",
