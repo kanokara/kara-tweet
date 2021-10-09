e34d44a84abd179edfe9b4d701a953bdf85f425d
kanokara <emptyknk@gmail.com>
2021-10-10 02:10:57 +0900

    Added 2 tweets

--- data/js/tweets/2021_10.js
+++ data/js/tweets/2021_10.js
@@ -1,6 +1,436 @@
 Grailbird.data.tweets_2021_10 = 
 [
 	{
+		"created_at": "Sat Oct 09 16:28:46 +0000 2021",
+		"id": 1446875296045539300,
+		"id_str": "1446875296045539335",
+		"full_text": "(화산귀환을 추천하는 건 아니다.)",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			19
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": 1446875141405773800,
+		"in_reply_to_status_id_str": "1446875141405773824",
+		"in_reply_to_user_id": 3255599052,
+		"in_reply_to_user_id_str": "3255599052",
+		"in_reply_to_screen_name": "EmptyKnK",
+		"user": {
+			"id": 3255599052,
+			"id_str": "3255599052",
+			"name": "Solitude [동결:11/20~]",
+			"screen_name": "EmptyKnK",
+			"location": "",
+			"description": "탄식하라,  덧없이 흩어지고 말리라",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": true,
+			"followers_count": 7,
+			"friends_count": 93,
+			"listed_count": 0,
+			"created_at": "Thu Jun 25 13:48:16 +0000 2015",
+			"favourites_count": 3842,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 14153,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1344647229387071494/FeA3dkeA_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1344647229387071494/FeA3dkeA_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3255599052/1606110388",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "ko",
+		"text": "(화산귀환을 추천하는 건 아니다.)"
+	},
+	{
+		"created_at": "Sat Oct 09 16:28:10 +0000 2021",
+		"id": 1446875141405773800,
+		"id_str": "1446875141405773824",
+		"full_text": "네이버쇼핑-멤버십을 연동하면 한 달에 두 권은 무료로 볼 수 있어요.\nhttps://t.co/g96HT1vJKU",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			62
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/g96HT1vJKU",
+					"expanded_url": "https://twitter.com/NAVER_SERIES/status/1446860504215928832?t=ifwNLQ4HT_wyHKtrT5-HYg&s=19",
+					"display_url": "twitter.com/NAVER_SERIES/s…",
+					"indices": [
+						39,
+						62
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": 1446874727327277000,
+		"in_reply_to_status_id_str": "1446874727327277060",
+		"in_reply_to_user_id": 3255599052,
+		"in_reply_to_user_id_str": "3255599052",
+		"in_reply_to_screen_name": "EmptyKnK",
+		"user": {
+			"id": 3255599052,
+			"id_str": "3255599052",
+			"name": "Solitude [동결:11/20~]",
+			"screen_name": "EmptyKnK",
+			"location": "",
+			"description": "탄식하라,  덧없이 흩어지고 말리라",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": true,
+			"followers_count": 7,
+			"friends_count": 93,
+			"listed_count": 0,
+			"created_at": "Thu Jun 25 13:48:16 +0000 2015",
+			"favourites_count": 3842,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 14153,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1344647229387071494/FeA3dkeA_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1344647229387071494/FeA3dkeA_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3255599052/1606110388",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1446860504215928800,
+		"quoted_status_id_str": "1446860504215928832",
+		"quoted_status_permalink": {
+			"url": "https://t.co/g96HT1vJKU",
+			"expanded": "https://twitter.com/NAVER_SERIES/status/1446860504215928832?t=ifwNLQ4HT_wyHKtrT5-HYg&s=19",
+			"display": "twitter.com/NAVER_SERIES/s…"
+		},
+		"quoted_status": {
+			"created_at": "Sat Oct 09 15:30:00 +0000 2021",
+			"id": 1446860504215928800,
+			"id_str": "1446860504215928832",
+			"full_text": "#EVENT\n우리 청명이 생일인 거 \n모르는 트친 없게 해주세요...🙏\n\n📢 지금 시리즈에서 청명이 생일 축하하면\n쿠키 5개🍪 즉시 지급! (~10/11)\n*본 이벤트는 AOS 유저만 참여 가능합니다\n\n#화산귀환 #부동의_1위무협 \n#매화검존탄신일축하 #많관부 https://t.co/IL3O6xAL3J",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				145
+			],
+			"entities": {
+				"hashtags": [
+					{
+						"text": "EVENT",
+						"indices": [
+							0,
+							6
+						]
+					},
+					{
+						"text": "화산귀환",
+						"indices": [
+							113,
+							118
+						]
+					},
+					{
+						"text": "부동의_1위무협",
+						"indices": [
+							119,
+							128
+						]
+					},
+					{
+						"text": "매화검존탄신일축하",
+						"indices": [
+							130,
+							140
+						]
+					},
+					{
+						"text": "많관부",
+						"indices": [
+							141,
+							145
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 1446413744368599000,
+						"id_str": "1446413744368599041",
+						"indices": [
+							146,
+							169
+						],
+						"media_url": "http://pbs.twimg.com/media/FBKxoxdVQAEtJgD.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/FBKxoxdVQAEtJgD.jpg",
+						"url": "https://t.co/IL3O6xAL3J",
+						"display_url": "pic.twitter.com/IL3O6xAL3J",
+						"expanded_url": "https://twitter.com/NAVER_SERIES/status/1446860504215928832/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 800,
+								"h": 800,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 800,
+								"h": 800,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 680,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1446413744368599000,
+						"id_str": "1446413744368599041",
+						"indices": [
+							146,
+							169
+						],
+						"media_url": "http://pbs.twimg.com/media/FBKxoxdVQAEtJgD.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/FBKxoxdVQAEtJgD.jpg",
+						"url": "https://t.co/IL3O6xAL3J",
+						"display_url": "pic.twitter.com/IL3O6xAL3J",
+						"expanded_url": "https://twitter.com/NAVER_SERIES/status/1446860504215928832/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 800,
+								"h": 800,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 800,
+								"h": 800,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 680,
+								"resize": "fit"
+							}
+						}
+					},
+					{
+						"id": 1446414010044215300,
+						"id_str": "1446414010044215308",
+						"indices": [
+							146,
+							169
+						],
+						"media_url": "http://pbs.twimg.com/media/FBKx4PLVcAw-zKz.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/FBKx4PLVcAw-zKz.jpg",
+						"url": "https://t.co/IL3O6xAL3J",
+						"display_url": "pic.twitter.com/IL3O6xAL3J",
+						"expanded_url": "https://twitter.com/NAVER_SERIES/status/1446860504215928832/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 680,
+								"h": 680,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 800,
+								"h": 800,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 800,
+								"h": 800,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"https://twitter.com\" rel=\"nofollow\">Twitter for Advertisers</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 1040065090144284700,
+				"id_str": "1040065090144284673",
+				"name": "SERIES(시리즈)",
+				"screen_name": "NAVER_SERIES",
+				"location": "대한민국",
+				"description": "네이버 시리즈 공식 트위터 입니다.  웹툰, 웹소설 좋아해요!",
+				"url": "https://t.co/smwqPKw24u",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/smwqPKw24u",
+								"expanded_url": "https://bit.ly/2AWGFZP",
+								"display_url": "bit.ly/2AWGFZP",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 5732,
+				"friends_count": 87,
+				"listed_count": 20,
+				"created_at": "Thu Sep 13 02:30:15 +0000 2018",
+				"favourites_count": 1082,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": true,
+				"statuses_count": 255,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "000000",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1040479399760388096/_oS62cmf_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1040479399760388096/_oS62cmf_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/1040065090144284673/1622785125",
+				"profile_link_color": "19CF86",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "000000",
+				"profile_text_color": "000000",
+				"profile_use_background_image": false,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none",
+				"withheld_in_countries": []
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 769,
+			"favorite_count": 238,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "ko"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "네이버쇼핑-멤버십을 연동하면 한 달에 두 권은 무료로 볼 수 있어요.\nhttps://t.co/g96HT1vJKU"
+	},
+	{
 		"created_at": "Sun Oct 03 18:58:02 +0000 2021",
 		"id": 1444738532967288800,
 		"id_str": "1444738532967288832",
