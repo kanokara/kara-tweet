d8b2e695ee3a5ea270f640f527850d19adfc8dfd
kanokara <emptyknk@gmail.com>
2021-10-21 21:10:58 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 22107,
-	"created_at": "Thu Oct 21 11:10:57 +0000 2021",
+	"tweets": 22108,
+	"created_at": "Thu Oct 21 12:10:57 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_10.js
+++ data/js/tweets/2021_10.js
@@ -1,6 +1,102 @@
 Grailbird.data.tweets_2021_10 = 
 [
 	{
+		"created_at": "Thu Oct 21 11:15:38 +0000 2021",
+		"id": 1450724038792204300,
+		"id_str": "1451145145634594818",
+		"full_text": "얘들아! 멀쩡한 관계에선 좋아하는 사람한테 오뎅 한사발 조지고 아이스크림 때리러가자고 하지 살아가라고 말하지 않는단다! 살라고 말할 땐 뭔가 문제가 있는 상황임을 깨닫길 바란다! 이상!",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			103
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 777697064,
+			"id_str": "777697064",
+			"name": "렌므",
+			"screen_name": "lenmue0",
+			"location": "모든창작물,작업물의 무단도용및 불펌을 금합니다.",
+			"description": "이것저것 좋아하는 거 많음! \n최근엔 세포신곡(細胞神曲)▶: 스포일러 주의!",
+			"url": "https://t.co/lHntcG6GJZ",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/lHntcG6GJZ",
+							"expanded_url": "https://lenmue-etc.postype.com/",
+							"display_url": "lenmue-etc.postype.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 13848,
+			"friends_count": 128,
+			"listed_count": 76,
+			"created_at": "Fri Aug 24 08:18:07 +0000 2012",
+			"favourites_count": 2718,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 199482,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "1A1B1F",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1384834936306233350/P_V7vylM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1384834936306233350/P_V7vylM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/777697064/1619005354",
+			"profile_link_color": "2FC2EF",
+			"profile_sidebar_border_color": "181A1E",
+			"profile_sidebar_fill_color": "252429",
+			"profile_text_color": "666666",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1863,
+		"favorite_count": 570,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "ko",
+		"text": "얘들아! 멀쩡한 관계에선 좋아하는 사람한테 오뎅 한사발 조지고 아이스크림 때리러가자고 하지 살아가라고 말하지 않는단다! 살라고 말할 땐 뭔가 문제가 있는 상황임을 깨닫길 바란다! 이상!"
+	},
+	{
 		"created_at": "Thu Oct 21 11:08:54 +0000 2021",
 		"id": 1451143451341328400,
 		"id_str": "1451143451341328390",
