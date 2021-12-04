d0a05e7672e1e23783b027928b8e20f046050f8c
kanokara <emptyknk@gmail.com>
2021-12-04 10:10:57 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 22225,
-	"created_at": "Tue Nov 30 00:10:58 +0000 2021",
+	"tweets": 22226,
+	"created_at": "Sat Dec 04 01:10:57 +0000 2021",
 	"lang": "ko"
 }
---
+++ data/js/tweets/2021_12.js
@@ -0,0 +1,86 @@
+Grailbird.data.tweets_2021_12 = 
+[
+	{
+		"created_at": "Sat Dec 04 00:26:07 +0000 2021",
+		"id": 1466884852280873000,
+		"id_str": "1466926754292183043",
+		"full_text": "Frozen chateau - of northern sea\nCitadel of beast - in southern jungle\n\n못할게뭐여",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			77
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": 1466881360162062300,
+		"in_reply_to_status_id_str": "1466881360162062336",
+		"in_reply_to_user_id": 1466482887411179500,
+		"in_reply_to_user_id_str": "1466482887411179521",
+		"in_reply_to_screen_name": "Dubalmun_A",
+		"user": {
+			"id": 1466482887411179500,
+			"id_str": "1466482887411179521",
+			"name": "두발문어",
+			"screen_name": "Dubalmun_A",
+			"location": "",
+			"description": "알피지쟁이 글마",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 8,
+			"friends_count": 14,
+			"listed_count": 0,
+			"created_at": "Thu Dec 02 19:02:28 +0000 2021",
+			"favourites_count": 23,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 47,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1466483651198267392/7pVzYDJo_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1466483651198267392/7pVzYDJo_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1466482887411179521/1638471973",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "en",
+		"text": "Frozen chateau - of northern sea\nCitadel of beast - in southern jungle\n\n못할게뭐여"
+	}
+]
