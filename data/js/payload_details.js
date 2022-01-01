53fa0c86d39c702307a2c3a364d46d052a98481f
kanokara <emptyknk@gmail.com>
2022-01-01 22:10:58 +0900

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 22276,
-	"created_at": "Thu Dec 30 14:10:58 +0000 2021",
+	"tweets": 22278,
+	"created_at": "Sat Jan 01 13:10:57 +0000 2022",
 	"lang": "ko"
 }
---
+++ data/js/tweets/2022_01.js
@@ -0,0 +1,284 @@
+Grailbird.data.tweets_2022_01 = 
+[
+	{
+		"created_at": "Sat Jan 01 12:56:09 +0000 2022",
+		"id": 1477187439957577700,
+		"id_str": "1477262370183741442",
+		"full_text": "ㅋㅋㅋㅋ일단 전쟁의 참상을 논하려면 슈퍼 에이스 파일럿 얘기의 비중부터 좀 줄이셔야(《주머니 속의 전쟁》은 비교적 성공적인 변종이었다)... 단언컨대 철혈의 오펀스는 실패작이지만 시리즈의 에센스를 놓친 게 아니라 시리즈의 에센스가 무엇이었는지를 폭로한 것이다",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			144
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1222931828727844900,
+			"id_str": "1222931828727844864",
+			"name": "그런거보는계정",
+			"screen_name": "willnotshipyuri",
+			"location": "",
+			"description": "으아악 아니야",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 429,
+			"friends_count": 287,
+			"listed_count": 2,
+			"created_at": "Thu Jan 30 17:17:38 +0000 2020",
+			"favourites_count": 9241,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 50874,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1421039155711205378/rQRcnX8K_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1421039155711205378/rQRcnX8K_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1222931828727844864/1580405000",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 7,
+		"favorite_count": 4,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "ko",
+		"text": "ㅋㅋㅋㅋ일단 전쟁의 참상을 논하려면 슈퍼 에이스 파일럿 얘기의 비중부터 좀 줄이셔야(《주머니 속의 전쟁》은 비교적 성공적인 변종이었다)... 단언컨대 철혈의 오펀스는 실패작이지만 시리즈의 에센스를 놓친 게 아니라 시리즈의 에센스가 무엇이었는지를 폭로한 것이다"
+	},
+	{
+		"created_at": "Sat Jan 01 12:32:12 +0000 2022",
+		"id": 1476021949084373000,
+		"id_str": "1477256341802852352",
+		"full_text": "아 이거 우리나라 영화에도 예시 있다. 소녀의 세계ㅋㅋㅋㅋ https://t.co/nkkiuHz3Mp",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			32
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1476021815005040600,
+					"id_str": "1476021815005040648",
+					"indices": [
+						33,
+						56
+					],
+					"media_url": "http://pbs.twimg.com/media/FHviBTwaUAgEdDl.png",
+					"media_url_https": "https://pbs.twimg.com/media/FHviBTwaUAgEdDl.png",
+					"url": "https://t.co/nkkiuHz3Mp",
+					"display_url": "pic.twitter.com/nkkiuHz3Mp",
+					"expanded_url": "https://twitter.com/not_unot_u/status/1476021949084372994/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 420,
+							"h": 600,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 420,
+							"h": 600,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 420,
+							"h": 600,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1476021815005040600,
+					"id_str": "1476021815005040648",
+					"indices": [
+						33,
+						56
+					],
+					"media_url": "http://pbs.twimg.com/media/FHviBTwaUAgEdDl.png",
+					"media_url_https": "https://pbs.twimg.com/media/FHviBTwaUAgEdDl.png",
+					"url": "https://t.co/nkkiuHz3Mp",
+					"display_url": "pic.twitter.com/nkkiuHz3Mp",
+					"expanded_url": "https://twitter.com/not_unot_u/status/1476021949084372994/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 420,
+							"h": 600,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 420,
+							"h": 600,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 420,
+							"h": 600,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1476021842947481600,
+					"id_str": "1476021842947481600",
+					"indices": [
+						33,
+						56
+					],
+					"media_url": "http://pbs.twimg.com/media/FHviC72aIAAhIeh.png",
+					"media_url_https": "https://pbs.twimg.com/media/FHviC72aIAAhIeh.png",
+					"url": "https://t.co/nkkiuHz3Mp",
+					"display_url": "pic.twitter.com/nkkiuHz3Mp",
+					"expanded_url": "https://twitter.com/not_unot_u/status/1476021949084372994/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 580,
+							"h": 502,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 580,
+							"h": 502,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 580,
+							"h": 502,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1476013287330676700,
+		"in_reply_to_status_id_str": "1476013287330676736",
+		"in_reply_to_user_id": 778352745732513800,
+		"in_reply_to_user_id_str": "778352745732513792",
+		"in_reply_to_screen_name": "not_unot_u",
+		"user": {
+			"id": 778352745732513800,
+			"id_str": "778352745732513792",
+			"name": "브리라순자",
+			"screen_name": "not_unot_u",
+			"location": "사르디나",
+			"description": "모순모순맨🏳️‍🌈 / 비팔알림끔",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 11937,
+			"friends_count": 467,
+			"listed_count": 51,
+			"created_at": "Tue Sep 20 21:58:50 +0000 2016",
+			"favourites_count": 34890,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 57600,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1207551747062763520/vjsKLMWc_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1207551747062763520/vjsKLMWc_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/778352745732513792/1530265100",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 63,
+		"favorite_count": 86,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "아 이거 우리나라 영화에도 예시 있다. 소녀의 세계ㅋㅋㅋㅋ https://t.co/nkkiuHz3Mp"
+	}
+]
