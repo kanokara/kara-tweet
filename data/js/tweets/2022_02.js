1854c894c1e38077af495fbed641cc06894ad088
kanokara <emptyknk@gmail.com>
2022-02-02 09:10:58 +0900

    Added 1 tweets

---
+++ data/js/tweets/2022_02.js
@@ -0,0 +1,178 @@
+Grailbird.data.tweets_2022_02 = 
+[
+	{
+		"created_at": "Wed Feb 02 00:04:21 +0000 2022",
+		"id": 1488369470611460000,
+		"id_str": "1488664550904057856",
+		"full_text": "장르와 무관하게 모든 내러티브가 자전적으로 읽히는 전쟁의 산문 시대. 이 시대에서 자전적으로 읽히지 않으려면 어떻게 써야 하는가. https://t.co/AlbYqwKJtP",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			72
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1488369460109254700,
+					"id_str": "1488369460109254657",
+					"indices": [
+						73,
+						96
+					],
+					"media_url": "http://pbs.twimg.com/media/FKfAI2RaIAEHUO9.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FKfAI2RaIAEHUO9.jpg",
+					"url": "https://t.co/AlbYqwKJtP",
+					"display_url": "pic.twitter.com/AlbYqwKJtP",
+					"expanded_url": "https://twitter.com/chamsaebak/status/1488369470611460103/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 308,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 927,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 543,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1488369460109254700,
+					"id_str": "1488369460109254657",
+					"indices": [
+						73,
+						96
+					],
+					"media_url": "http://pbs.twimg.com/media/FKfAI2RaIAEHUO9.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FKfAI2RaIAEHUO9.jpg",
+					"url": "https://t.co/AlbYqwKJtP",
+					"display_url": "pic.twitter.com/AlbYqwKJtP",
+					"expanded_url": "https://twitter.com/chamsaebak/status/1488369470611460103/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 308,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 927,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 543,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1445324509494407200,
+			"id_str": "1445324509494407170",
+			"name": "박참새",
+			"screen_name": "chamsaebak",
+			"location": "",
+			"description": "본업 : 알티하기 ⠀⠀부업 : 책 팔이",
+			"url": "https://t.co/irBRy2UMmB",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/irBRy2UMmB",
+							"expanded_url": "http://www.shop-moi.kr",
+							"display_url": "shop-moi.kr",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 175,
+			"friends_count": 25,
+			"listed_count": 0,
+			"created_at": "Tue Oct 05 09:46:47 +0000 2021",
+			"favourites_count": 193,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 208,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1445324582248869891/Wk_VFPmk_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1445324582248869891/Wk_VFPmk_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1445324509494407170/1634046230",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 42,
+		"favorite_count": 88,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "장르와 무관하게 모든 내러티브가 자전적으로 읽히는 전쟁의 산문 시대. 이 시대에서 자전적으로 읽히지 않으려면 어떻게 써야 하는가. https://t.co/AlbYqwKJtP"
+	}
+]
