8388a86cae814fafc11b9b30979a13972aef58c4
kanokara <emptyknk@gmail.com>
2022-02-07 21:10:58 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 22361,
-	"created_at": "Mon Feb 07 11:10:57 +0000 2022",
+	"tweets": 22362,
+	"created_at": "Mon Feb 07 12:10:57 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_02.js
+++ data/js/tweets/2022_02.js
@@ -1,6 +1,168 @@
 Grailbird.data.tweets_2022_02 = 
 [
 	{
+		"created_at": "Mon Feb 07 11:33:35 +0000 2022",
+		"id": 1490649942943101000,
+		"id_str": "1490649942943100932",
+		"full_text": "노출수 백만 ㅋㅋㅋㅋ https://t.co/wARKf54s4r",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			11
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1490649940447887400,
+					"id_str": "1490649940447887365",
+					"indices": [
+						12,
+						35
+					],
+					"media_url": "http://pbs.twimg.com/media/FK_aORjagAUPwlU.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FK_aORjagAUPwlU.jpg",
+					"url": "https://t.co/wARKf54s4r",
+					"display_url": "pic.twitter.com/wARKf54s4r",
+					"expanded_url": "https://twitter.com/EmptyKnK/status/1490649942943100932/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 317,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 955,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 560,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1490649940447887400,
+					"id_str": "1490649940447887365",
+					"indices": [
+						12,
+						35
+					],
+					"media_url": "http://pbs.twimg.com/media/FK_aORjagAUPwlU.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FK_aORjagAUPwlU.jpg",
+					"url": "https://t.co/wARKf54s4r",
+					"display_url": "pic.twitter.com/wARKf54s4r",
+					"expanded_url": "https://twitter.com/EmptyKnK/status/1490649942943100932/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 317,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 955,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 560,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3255599052,
+			"id_str": "3255599052",
+			"name": "Solitude [동결:11/20~]",
+			"screen_name": "EmptyKnK",
+			"location": "",
+			"description": "탄식하라,  덧없이 흩어지고 말리라",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": true,
+			"followers_count": 6,
+			"friends_count": 91,
+			"listed_count": 0,
+			"created_at": "Thu Jun 25 13:48:16 +0000 2015",
+			"favourites_count": 3852,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 14149,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1344647229387071494/FeA3dkeA_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1344647229387071494/FeA3dkeA_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3255599052/1606110388",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "노출수 백만 ㅋㅋㅋㅋ https://t.co/wARKf54s4r"
+	},
+	{
 		"created_at": "Mon Feb 07 10:23:25 +0000 2022",
 		"id": 1490513329705554000,
 		"id_str": "1490632284017688578",
