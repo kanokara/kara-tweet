c075c67387b23e510d495ad4d59420cd2d666bae
kanokara <emptyknk@gmail.com>
2022-02-15 03:10:59 +0900

    Added 1 tweets

--- data/js/tweets/2022_02.js
+++ data/js/tweets/2022_02.js
@@ -1,6 +1,168 @@
 Grailbird.data.tweets_2022_02 = 
 [
 	{
+		"created_at": "Mon Feb 14 17:49:17 +0000 2022",
+		"id": 1493281202811269000,
+		"id_str": "1493281202811269120",
+		"full_text": "다시 읽어도 아리다. https://t.co/pefVFoqntD",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			11
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1493281196994080800,
+					"id_str": "1493281196994080768",
+					"indices": [
+						12,
+						35
+					],
+					"media_url": "http://pbs.twimg.com/media/FLkzVkCaMAAuqYP.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FLkzVkCaMAAuqYP.jpg",
+					"url": "https://t.co/pefVFoqntD",
+					"display_url": "pic.twitter.com/pefVFoqntD",
+					"expanded_url": "https://twitter.com/EmptyKnK/status/1493281202811269120/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 290,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 413,
+							"h": 968,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 413,
+							"h": 968,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1493281196994080800,
+					"id_str": "1493281196994080768",
+					"indices": [
+						12,
+						35
+					],
+					"media_url": "http://pbs.twimg.com/media/FLkzVkCaMAAuqYP.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FLkzVkCaMAAuqYP.jpg",
+					"url": "https://t.co/pefVFoqntD",
+					"display_url": "pic.twitter.com/pefVFoqntD",
+					"expanded_url": "https://twitter.com/EmptyKnK/status/1493281202811269120/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 290,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 413,
+							"h": 968,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 413,
+							"h": 968,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": 1481733324213465000,
+		"in_reply_to_status_id_str": "1481733324213465093",
+		"in_reply_to_user_id": 3255599052,
+		"in_reply_to_user_id_str": "3255599052",
+		"in_reply_to_screen_name": "EmptyKnK",
+		"user": {
+			"id": 3255599052,
+			"id_str": "3255599052",
+			"name": "Solitude [동결:11/20~]",
+			"screen_name": "EmptyKnK",
+			"location": "",
+			"description": "탄식하라,  덧없이 흩어지고 말리라",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": true,
+			"followers_count": 6,
+			"friends_count": 91,
+			"listed_count": 0,
+			"created_at": "Thu Jun 25 13:48:16 +0000 2015",
+			"favourites_count": 3850,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 14165,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1344647229387071494/FeA3dkeA_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1344647229387071494/FeA3dkeA_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3255599052/1606110388",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "다시 읽어도 아리다. https://t.co/pefVFoqntD"
+	},
+	{
 		"created_at": "Mon Feb 14 16:30:20 +0000 2022",
 		"id": 1493261335534063600,
 		"id_str": "1493261335534063618",
