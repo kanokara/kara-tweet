8a77ef3c0a84f4d679b7b0d7ddb7fd8825701d12
kanokara <emptyknk@gmail.com>
2022-02-15 02:10:58 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 22373,
-	"created_at": "Mon Feb 14 16:10:57 +0000 2022",
+	"tweets": 22374,
+	"created_at": "Mon Feb 14 17:10:57 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_02.js
+++ data/js/tweets/2022_02.js
@@ -1,6 +1,207 @@
 Grailbird.data.tweets_2022_02 = 
 [
 	{
+		"created_at": "Mon Feb 14 16:30:20 +0000 2022",
+		"id": 1493261335534063600,
+		"id_str": "1493261335534063618",
+		"full_text": "명확하게 설명할 수 있는 문제.\nhttps://t.co/Oiehdnq12q",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			41
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/Oiehdnq12q",
+					"expanded_url": "https://twitter.com/ppalgaengi/status/1493132953764843524?t=XQ0i9btfkTmU7PrEeXnBsw&s=19",
+					"display_url": "twitter.com/ppalgaengi/sta…",
+					"indices": [
+						18,
+						41
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3255599052,
+			"id_str": "3255599052",
+			"name": "Solitude [동결:11/20~]",
+			"screen_name": "EmptyKnK",
+			"location": "",
+			"description": "탄식하라,  덧없이 흩어지고 말리라",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": true,
+			"followers_count": 6,
+			"friends_count": 91,
+			"listed_count": 0,
+			"created_at": "Thu Jun 25 13:48:16 +0000 2015",
+			"favourites_count": 3850,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 14164,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1344647229387071494/FeA3dkeA_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1344647229387071494/FeA3dkeA_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3255599052/1606110388",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1493132953764843500,
+		"quoted_status_id_str": "1493132953764843524",
+		"quoted_status_permalink": {
+			"url": "https://t.co/Oiehdnq12q",
+			"expanded": "https://twitter.com/ppalgaengi/status/1493132953764843524?t=XQ0i9btfkTmU7PrEeXnBsw&s=19",
+			"display": "twitter.com/ppalgaengi/sta…"
+		},
+		"quoted_status": {
+			"created_at": "Mon Feb 14 08:00:11 +0000 2022",
+			"id": 1493132953764843500,
+			"id_str": "1493132953764843524",
+			"full_text": "한국인 중에 \"영성\"이 무엇인지 이해 못 하거나 피상적으로만 아는 사람 정말 많을 거라는 생각도 든다. 서양인들이 \"효\"나 \"충\"을 온전히 이해하기 어려운 것과도 비슷하다. 그래서 \"영성\"이라는 거 좀 문화적인 것 같아… (이런 말 하면 서양인들이 왠지 좀 언짢아하지만…) https://t.co/CAjYAaIFpc",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				176
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/CAjYAaIFpc",
+						"expanded_url": "https://twitter.com/confusion_LV/status/1493131419744616449",
+						"display_url": "twitter.com/confusion_LV/s…",
+						"indices": [
+							153,
+							176
+						]
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+			"in_reply_to_status_id": 1493130374913818600,
+			"in_reply_to_status_id_str": "1493130374913818628",
+			"in_reply_to_user_id": 1326077694392426500,
+			"in_reply_to_user_id_str": "1326077694392426498",
+			"in_reply_to_screen_name": "ppalgaengi",
+			"user": {
+				"id": 1326077694392426500,
+				"id_str": "1326077694392426498",
+				"name": "공",
+				"screen_name": "ppalgaengi",
+				"location": "지상낙원",
+				"description": "뭐? 멸공?",
+				"url": null,
+				"entities": {
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 1459,
+				"friends_count": 571,
+				"listed_count": 9,
+				"created_at": "Tue Nov 10 08:22:20 +0000 2020",
+				"favourites_count": 4731,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 5413,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "F5F8FA",
+				"profile_background_image_url": null,
+				"profile_background_image_url_https": null,
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1480396505018437636/kZF9Rbk8_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1480396505018437636/kZF9Rbk8_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/1326077694392426498/1616702817",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": true,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none",
+				"withheld_in_countries": []
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": true,
+			"quoted_status_id": 1493131419744616400,
+			"quoted_status_id_str": "1493131419744616449",
+			"quoted_status_permalink": {
+				"url": "https://t.co/CAjYAaIFpc",
+				"expanded": "https://twitter.com/confusion_LV/status/1493131419744616449",
+				"display": "twitter.com/confusion_LV/s…"
+			},
+			"retweet_count": 312,
+			"favorite_count": 165,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "ko"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "명확하게 설명할 수 있는 문제.\nhttps://t.co/Oiehdnq12q"
+	},
+	{
 		"created_at": "Mon Feb 14 15:47:13 +0000 2022",
 		"id": 1493132953764843500,
 		"id_str": "1493250486509895683",
