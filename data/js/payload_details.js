5dedc3d33b5ce1c3c55e9d06e527299a2ba7d443
kanokara <emptyknk@gmail.com>
2022-04-03 16:10:57 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 22454,
-	"created_at": "Sun Apr 03 06:10:56 +0000 2022",
+	"tweets": 22455,
+	"created_at": "Sun Apr 03 07:10:56 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_04.js
+++ data/js/tweets/2022_04.js
@@ -1,6 +1,222 @@
 Grailbird.data.tweets_2022_04 = 
 [
 	{
+		"created_at": "Sun Apr 03 07:03:52 +0000 2022",
+		"id": 1510513396797833200,
+		"id_str": "1510513396797833217",
+		"full_text": "남녀갈등 이전의 세상은 더 이상 없다 #페미니즘\nhttps://t.co/GhuycgVbVz https://t.co/tSzcpq80CY",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			50
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "페미니즘",
+					"indices": [
+						21,
+						26
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/GhuycgVbVz",
+					"expanded_url": "https://pgr21.com/freedom/95171",
+					"display_url": "pgr21.com/freedom/95171",
+					"indices": [
+						27,
+						50
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1510513386085585000,
+					"id_str": "1510513386085584904",
+					"indices": [
+						51,
+						74
+					],
+					"media_url": "http://pbs.twimg.com/media/FPZr6-4akAgQ0U7.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FPZr6-4akAgQ0U7.jpg",
+					"url": "https://t.co/tSzcpq80CY",
+					"display_url": "pic.twitter.com/tSzcpq80CY",
+					"expanded_url": "https://twitter.com/EmptyKnK/status/1510513396797833217/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 773,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 438,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1057,
+							"h": 1640,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1510513386085585000,
+					"id_str": "1510513386085584904",
+					"indices": [
+						51,
+						74
+					],
+					"media_url": "http://pbs.twimg.com/media/FPZr6-4akAgQ0U7.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FPZr6-4akAgQ0U7.jpg",
+					"url": "https://t.co/tSzcpq80CY",
+					"display_url": "pic.twitter.com/tSzcpq80CY",
+					"expanded_url": "https://twitter.com/EmptyKnK/status/1510513396797833217/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 773,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 438,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1057,
+							"h": 1640,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1510513390695121000,
+					"id_str": "1510513390695120896",
+					"indices": [
+						51,
+						74
+					],
+					"media_url": "http://pbs.twimg.com/media/FPZr7QDagAA583X.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FPZr7QDagAA583X.jpg",
+					"url": "https://t.co/tSzcpq80CY",
+					"display_url": "pic.twitter.com/tSzcpq80CY",
+					"expanded_url": "https://twitter.com/EmptyKnK/status/1510513396797833217/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1070,
+							"h": 1174,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1070,
+							"h": 1174,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 620,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3255599052,
+			"id_str": "3255599052",
+			"name": "Solitude [동결:11/20~]",
+			"screen_name": "EmptyKnK",
+			"location": "",
+			"description": "탄식하라,  덧없이 흩어지고 말리라",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": true,
+			"followers_count": 6,
+			"friends_count": 90,
+			"listed_count": 0,
+			"created_at": "Thu Jun 25 13:48:16 +0000 2015",
+			"favourites_count": 3866,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 14197,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1344647229387071494/FeA3dkeA_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1344647229387071494/FeA3dkeA_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3255599052/1606110388",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "남녀갈등 이전의 세상은 더 이상 없다 #페미니즘\nhttps://t.co/GhuycgVbVz https://t.co/tSzcpq80CY"
+	},
+	{
 		"created_at": "Sun Apr 03 06:05:43 +0000 2022",
 		"id": 1510498764796338200,
 		"id_str": "1510498764796338177",
