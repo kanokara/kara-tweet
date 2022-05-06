94eaa9851deb3b69d6f229e3c8482317722f3afb
kanokara <emptyknk@gmail.com>
2022-05-06 16:10:58 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 22538,
-	"created_at": "Thu May 05 11:10:57 +0000 2022",
+	"tweets": 22539,
+	"created_at": "Fri May 06 07:10:57 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_05.js
+++ data/js/tweets/2022_05.js
@@ -1,6 +1,267 @@
 Grailbird.data.tweets_2022_05 = 
 [
 	{
+		"created_at": "Fri May 06 06:34:58 +0000 2022",
+		"id": 1522464923896656000,
+		"id_str": "1522464923896655873",
+		"full_text": "가끔 돌 때마다 눈물 터지네... https://t.co/h0Fzixs9PU",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			18
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/h0Fzixs9PU",
+					"expanded_url": "https://twitter.com/yangd0only/status/1522213527603527680",
+					"display_url": "twitter.com/yangd0only/sta…",
+					"indices": [
+						19,
+						42
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3255599052,
+			"id_str": "3255599052",
+			"name": "Solitude [동결:11/20~]",
+			"screen_name": "EmptyKnK",
+			"location": "",
+			"description": "탄식하라,  덧없이 흩어지고 말리라",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": true,
+			"followers_count": 6,
+			"friends_count": 91,
+			"listed_count": 0,
+			"created_at": "Thu Jun 25 13:48:16 +0000 2015",
+			"favourites_count": 3907,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 14269,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1344647229387071494/FeA3dkeA_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1344647229387071494/FeA3dkeA_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3255599052/1606110388",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1522213527603527700,
+		"quoted_status_id_str": "1522213527603527680",
+		"quoted_status_permalink": {
+			"url": "https://t.co/h0Fzixs9PU",
+			"expanded": "https://twitter.com/yangd0only/status/1522213527603527680",
+			"display": "twitter.com/yangd0only/sta…"
+		},
+		"quoted_status": {
+			"created_at": "Thu May 05 13:56:00 +0000 2022",
+			"id": 1522213527603527700,
+			"id_str": "1522213527603527680",
+			"full_text": "꼬꼬무 대구 지하철 화재사건 피해자들이 죽기 전 보낸 메시지래....보자마자 눈물터짐....😭😭😭 https://t.co/94uqRe4yuD",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				54
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 1522213524441034800,
+						"id_str": "1522213524441034752",
+						"indices": [
+							55,
+							78
+						],
+						"media_url": "http://pbs.twimg.com/media/FR_9IqyacAApvAX.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/FR_9IqyacAApvAX.jpg",
+						"url": "https://t.co/94uqRe4yuD",
+						"display_url": "pic.twitter.com/94uqRe4yuD",
+						"expanded_url": "https://twitter.com/yangd0only/status/1522213527603527680/photo/1",
+						"type": "photo",
+						"sizes": {
+							"small": {
+								"w": 610,
+								"h": 512,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 610,
+								"h": 512,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 610,
+								"h": 512,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1522213524441034800,
+						"id_str": "1522213524441034752",
+						"indices": [
+							55,
+							78
+						],
+						"media_url": "http://pbs.twimg.com/media/FR_9IqyacAApvAX.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/FR_9IqyacAApvAX.jpg",
+						"url": "https://t.co/94uqRe4yuD",
+						"display_url": "pic.twitter.com/94uqRe4yuD",
+						"expanded_url": "https://twitter.com/yangd0only/status/1522213527603527680/photo/1",
+						"type": "photo",
+						"sizes": {
+							"small": {
+								"w": 610,
+								"h": 512,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 610,
+								"h": 512,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 610,
+								"h": 512,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 1518458140056256500,
+				"id_str": "1518458140056256513",
+				"name": "거래계",
+				"screen_name": "yangd0only",
+				"location": "",
+				"description": "거래 구독 잡담 다함",
+				"url": null,
+				"entities": {
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 29,
+				"friends_count": 41,
+				"listed_count": 0,
+				"created_at": "Mon Apr 25 05:13:32 +0000 2022",
+				"favourites_count": 13,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 115,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "F5F8FA",
+				"profile_background_image_url": null,
+				"profile_background_image_url_https": null,
+				"profile_background_tile": false,
+				"profile_image_url": "http://abs.twimg.com/sticky/default_profile_images/default_profile_normal.png",
+				"profile_image_url_https": "https://abs.twimg.com/sticky/default_profile_images/default_profile_normal.png",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": true,
+				"default_profile_image": true,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none",
+				"withheld_in_countries": []
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 19543,
+			"favorite_count": 11962,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "ko"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "가끔 돌 때마다 눈물 터지네... https://t.co/h0Fzixs9PU"
+	},
+	{
 		"created_at": "Thu May 05 10:12:02 +0000 2022",
 		"id": 1522157163170648000,
 		"id_str": "1522157163170648064",
