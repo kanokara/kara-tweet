296c6e0e4e23a0f0196fddd27617f86a0d3d2aa3
kanokara <emptyknk@gmail.com>
2022-10-28 08:11:04 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 22961,
-	"created_at": "Thu Oct 27 11:10:57 +0000 2022",
+	"tweets": 22962,
+	"created_at": "Thu Oct 27 23:11:03 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_10.js
+++ data/js/tweets/2022_10.js
@@ -1,6 +1,123 @@
 Grailbird.data.tweets_2022_10 = 
 [
 	{
+		"created_at": "Thu Oct 27 23:08:35 +0000 2022",
+		"id": 1490942823977603000,
+		"id_str": "1585770462252265472",
+		"full_text": "@suhhee2 @schmeichel80 원인 바이러스의 감염성 발암 환자와, 해당 바이러스들(물론 지금까지 알고 있는 바이러스만 대상으로 하였으니, 미지의 바이러스에 대한 감염은 아예 모르는거죠. 즉 감염 자체가 없는 것이라고 볼수는 없습니다)의 미감염 환자는 그 예후가 많이 차이납니다. 저는 이렇게 오래 잘 지내고 있잖아요",
+		"truncated": false,
+		"display_text_range": [
+			23,
+			183
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "suhhee2",
+					"name": "차별금지",
+					"id": 46946401,
+					"id_str": "46946401",
+					"indices": [
+						0,
+						8
+					]
+				},
+				{
+					"screen_name": "schmeichel80",
+					"name": "General Weakness",
+					"id": 235142576,
+					"id_str": "235142576",
+					"indices": [
+						9,
+						22
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1490942528493097000,
+		"in_reply_to_status_id_str": "1490942528493096960",
+		"in_reply_to_user_id": 461263284,
+		"in_reply_to_user_id_str": "461263284",
+		"in_reply_to_screen_name": "operationoxygen",
+		"user": {
+			"id": 461263284,
+			"id_str": "461263284",
+			"name": "GG🌱",
+			"screen_name": "operationoxygen",
+			"location": "대한민국",
+			"description": "Lab of Axon Regeneration & Degeneration. Studying the epigenetic mechanisms of neuronal injury responses.",
+			"url": "https://t.co/0nEVRdCnsf",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/0nEVRdCnsf",
+							"expanded_url": "http://axonlab.kr",
+							"display_url": "axonlab.kr",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 4883,
+			"friends_count": 5371,
+			"listed_count": 55,
+			"created_at": "Wed Jan 11 16:30:21 +0000 2012",
+			"favourites_count": 214756,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 163000,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1470569044222697474/ag5_IqE9_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1470569044222697474/ag5_IqE9_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/461263284/1650770111",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "ko",
+		"text": "@suhhee2 @schmeichel80 원인 바이러스의 감염성 발암 환자와, 해당 바이러스들(물론 지금까지 알고 있는 바이러스만 대상으로 하였으니, 미지의 바이러스에 대한 감염은 아예 모르는거죠. 즉 감염 자체가 없는 것이라고 볼수는 없습니다)의 미감염 환자는 그 예후가 많이 차이납니다. 저는 이렇게 오래 잘 지내고 있잖아요"
+	},
+	{
 		"created_at": "Thu Oct 27 10:49:37 +0000 2022",
 		"id": 1585584497906839600,
 		"id_str": "1585584497906839552",
