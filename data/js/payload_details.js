349b68e4c78b3253b8890d534c2e2d79cb72c828
kanokara <emptyknk@gmail.com>
2023-01-08 13:11:03 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 23003,
-	"created_at": "Thu Jan 05 21:11:02 +0000 2023",
+	"tweets": 23004,
+	"created_at": "Sun Jan 08 04:11:02 +0000 2023",
 	"lang": "ko"
 }
--- data/js/tweets/2023_01.js
+++ data/js/tweets/2023_01.js
@@ -1,6 +1,220 @@
 Grailbird.data.tweets_2023_01 = 
 [
 	{
+		"created_at": "Sun Jan 08 03:51:14 +0000 2023",
+		"id": 1611933522096451600,
+		"id_str": "1611933522096451585",
+		"full_text": "글쎄... 진정성을 파는 장르가 인디음악 아니던가. https://t.co/w74a8CNeu8",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			28
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/w74a8CNeu8",
+					"expanded_url": "https://twitter.com/eSd6dLt91oOybWJ/status/1607637427811942401",
+					"display_url": "twitter.com/eSd6dLt91oOybW…",
+					"indices": [
+						29,
+						52
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3255599052,
+			"id_str": "3255599052",
+			"name": "Solitude [동결:11/20~]",
+			"screen_name": "EmptyKnK",
+			"location": "",
+			"description": "탄식하라,  덧없이 흩어지고 말리라",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": true,
+			"followers_count": 4,
+			"friends_count": 91,
+			"listed_count": 0,
+			"created_at": "Thu Jun 25 13:48:16 +0000 2015",
+			"favourites_count": 4137,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 14756,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1344647229387071494/FeA3dkeA_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1344647229387071494/FeA3dkeA_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3255599052/1606110388",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1607637427811942400,
+		"quoted_status_id_str": "1607637427811942401",
+		"quoted_status_permalink": {
+			"url": "https://t.co/w74a8CNeu8",
+			"expanded": "https://twitter.com/eSd6dLt91oOybWJ/status/1607637427811942401",
+			"display": "twitter.com/eSd6dLt91oOybW…"
+		},
+		"quoted_status": {
+			"created_at": "Tue Dec 27 07:20:05 +0000 2022",
+			"id": 1607637427811942400,
+			"id_str": "1607637427811942401",
+			"full_text": "이런 수준까지 창작에 당사자성을 요구한다면 우리에게는 사소설 밖에 남지 않아요 봉준호가 살인하고 살인의 추억 만들고 박찬욱이 15년 감금당하고 올드보이 찍지는 않았잖아요? https://t.co/pa52iqXChm",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				95
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/pa52iqXChm",
+						"expanded_url": "https://twitter.com/Cyriakakim/status/1607561555234918400",
+						"display_url": "twitter.com/Cyriakakim/sta…",
+						"indices": [
+							96,
+							119
+						]
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 1009326345615097900,
+				"id_str": "1009326345615097856",
+				"name": "Ex_Libris",
+				"screen_name": "eSd6dLt91oOybWJ",
+				"location": "",
+				"description": "Quantity has a quality all its own.-iosebstalin\n \nI beg to differ with you.",
+				"url": "https://t.co/VLhkJa6RKN",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/VLhkJa6RKN",
+								"expanded_url": "https://m.blog.naver.com/mroyster0503",
+								"display_url": "m.blog.naver.com/mroyster0503",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 89,
+				"friends_count": 294,
+				"listed_count": 1,
+				"created_at": "Wed Jun 20 06:45:27 +0000 2018",
+				"favourites_count": 14001,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 1305,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "F5F8FA",
+				"profile_background_image_url": null,
+				"profile_background_image_url_https": null,
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1302884778446696450/pbDwaDJt_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1302884778446696450/pbDwaDJt_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/1009326345615097856/1599466958",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": true,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none",
+				"withheld_in_countries": []
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": true,
+			"quoted_status_id": 1607561555234918400,
+			"quoted_status_id_str": "1607561555234918400",
+			"quoted_status_permalink": {
+				"url": "https://t.co/pa52iqXChm",
+				"expanded": "https://twitter.com/Cyriakakim/status/1607561555234918400",
+				"display": "twitter.com/Cyriakakim/sta…"
+			},
+			"retweet_count": 2751,
+			"favorite_count": 1167,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": true,
+			"lang": "ko"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "글쎄... 진정성을 파는 장르가 인디음악 아니던가. https://t.co/w74a8CNeu8"
+	},
+	{
 		"created_at": "Thu Jan 05 20:46:13 +0000 2023",
 		"id": 1611101787611857000,
 		"id_str": "1611101787611856896",
