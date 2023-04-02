66ab2d8bc3714219366ee5409965a7f6b8bb1d94
kanokara <emptyknk@gmail.com>
2023-04-03 05:10:58 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 23041,
-	"created_at": "Sun Apr 02 14:10:57 +0000 2023",
+	"tweets": 23042,
+	"created_at": "Sun Apr 02 20:10:57 +0000 2023",
 	"lang": "ko"
 }
--- data/js/tweets/2023_04.js
+++ data/js/tweets/2023_04.js
@@ -1,6 +1,314 @@
 Grailbird.data.tweets_2023_04 = 
 [
 	{
+		"created_at": "Sun Apr 02 19:34:56 +0000 2023",
+		"id": 1641922553907400700,
+		"id_str": "1642611592662777857",
+		"full_text": "What the fuck? AI is posing a massive risk to almost every job in America, people are getting scared of losing their job in the near future, and when asked about it the American government just laughs in Americans face?\n\nGod I fucking hate the American government. https://t.co/s0CkJwKtLE",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			264
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/s0CkJwKtLE",
+					"expanded_url": "https://twitter.com/therecount/status/1641526864626720774",
+					"display_url": "twitter.com/therecount/sta…",
+					"indices": [
+						265,
+						288
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1386530437846028300,
+			"id_str": "1386530437846028292",
+			"name": "Mason",
+			"screen_name": "Mvcvalli",
+			"location": "~/Earth",
+			"description": "Computers • Wrestling • MMA • Bullying of Tay Melo (Tay Conti)  •",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 14,
+			"friends_count": 11,
+			"listed_count": 1,
+			"created_at": "Mon Apr 26 04:03:44 +0000 2021",
+			"favourites_count": 1088,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 1145,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1386553334677446661/34nu9pm1_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1386553334677446661/34nu9pm1_normal.jpg",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1641526864626720800,
+		"quoted_status_id_str": "1641526864626720774",
+		"quoted_status_permalink": {
+			"url": "https://t.co/s0CkJwKtLE",
+			"expanded": "https://twitter.com/therecount/status/1641526864626720774",
+			"display": "twitter.com/therecount/sta…"
+		},
+		"quoted_status": {
+			"created_at": "Thu Mar 30 19:44:37 +0000 2023",
+			"id": 1641526864626720800,
+			"id_str": "1641526864626720774",
+			"full_text": "Fox News’ Peter Doocy uses all his time at the White House press briefing to ask about an assessment that “literally everyone on Earth will die” because of artificial intelligence:\n\n“It sounds crazy, but is it?” https://t.co/CM0C5t79Wo",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				211
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 1641522969254981600,
+						"id_str": "1641522969254981633",
+						"indices": [
+							212,
+							235
+						],
+						"media_url": "http://pbs.twimg.com/media/FsfdawdWIAAJZsd.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/FsfdawdWIAAJZsd.jpg",
+						"url": "https://t.co/CM0C5t79Wo",
+						"display_url": "pic.twitter.com/CM0C5t79Wo",
+						"expanded_url": "https://twitter.com/therecount/status/1641526864626720774/video/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 640,
+								"h": 360,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 640,
+								"h": 360,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 640,
+								"h": 360,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1641522969254981600,
+						"id_str": "1641522969254981633",
+						"indices": [
+							212,
+							235
+						],
+						"media_url": "http://pbs.twimg.com/media/FsfdawdWIAAJZsd.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/FsfdawdWIAAJZsd.jpg",
+						"url": "https://t.co/CM0C5t79Wo",
+						"display_url": "pic.twitter.com/CM0C5t79Wo",
+						"expanded_url": "https://twitter.com/therecount/status/1641526864626720774/video/1",
+						"type": "video",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 640,
+								"h": 360,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 640,
+								"h": 360,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 640,
+								"h": 360,
+								"resize": "fit"
+							}
+						},
+						"video_info": {
+							"aspect_ratio": [
+								153,
+								86
+							],
+							"duration_millis": 89302,
+							"variants": [
+								{
+									"bitrate": 832000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/amplify_video/1641522969254981633/vid/640x360/_f_ZX7QZ8j-VBwsz.mp4?tag=16"
+								},
+								{
+									"bitrate": 288000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/amplify_video/1641522969254981633/vid/480x270/Q6ZgL_Dmci9Bc1OQ.mp4?tag=16"
+								},
+								{
+									"content_type": "application/x-mpegURL",
+									"url": "https://video.twimg.com/amplify_video/1641522969254981633/pl/i8Gqw8-clHCMAG58.m3u8?tag=16&container=fmp4"
+								},
+								{
+									"bitrate": 2176000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/amplify_video/1641522969254981633/vid/1280x720/EoxkFbBNlIjtzQct.mp4?tag=16"
+								}
+							]
+						},
+						"additional_media_info": {
+							"title": "Peter Doocy Asks Doomsday AI Question at White House Press Briefing",
+							"description": "",
+							"embeddable": true,
+							"monetizable": true
+						}
+					}
+				]
+			},
+			"source": "<a href=\"https://studio.twitter.com\" rel=\"nofollow\">Twitter Media Studio</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 1087380412089987100,
+				"id_str": "1087380412089987077",
+				"name": "The Recount",
+				"screen_name": "therecount",
+				"location": "New York, NY",
+				"description": "Everything is politics. Download our app & subscribe to our newsletter. 📩 info@therecount.com",
+				"url": "https://t.co/rdEMixv3PU",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/rdEMixv3PU",
+								"expanded_url": "http://recount.co/comehang",
+								"display_url": "recount.co/comehang",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 363523,
+				"friends_count": 703,
+				"listed_count": 2504,
+				"created_at": "Mon Jan 21 16:04:27 +0000 2019",
+				"favourites_count": 21545,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": true,
+				"statuses_count": 53229,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "F5F8FA",
+				"profile_background_image_url": null,
+				"profile_background_image_url_https": null,
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1155905677094731777/19WoFsvO_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1155905677094731777/19WoFsvO_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/1087380412089987077/1671117998",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": true,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none",
+				"withheld_in_countries": []
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 242,
+			"favorite_count": 1255,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 1,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": true,
+		"lang": "en",
+		"text": "What the fuck? AI is posing a massive risk to almost every job in America, people are getting scared of losing their job in the near future, and when asked about it the American government just laughs in Americans face?\n\nGod I fucking hate the American government. https://t.co/s0CkJwKtLE"
+	},
+	{
 		"created_at": "Sun Apr 02 13:35:08 +0000 2023",
 		"id": 1642521044119621600,
 		"id_str": "1642521044119621632",
